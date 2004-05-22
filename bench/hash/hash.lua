3a141b34badac861aaacd481c8dceb8b60058f67
bfulgham <bfulgham>
2004-05-22 04:31:50 +0000

    Correct for more modern versions of the two languages.

--- bench/hash/hash.lua
+++ bench/hash/hash.lua
@@ -1,4 +1,4 @@
--- $Id: hash.lua,v 1.1 2004-05-19 18:09:55 bfulgham Exp $
+-- $Id: hash.lua,v 1.2 2004-05-22 04:31:50 bfulgham Exp $
 -- http://www.bagley.org/~doug/shootout/
 -- Author: Roberto Ierusalimschy
 
@@ -6,7 +6,7 @@ local n = tonumber((arg and arg[1]) or 1)
 
 local X={}
 for i=1,n do
-  X[format("%x", i)] = i
+  X[string.format("%x", i)] = i
 end
 
 local c = 0
