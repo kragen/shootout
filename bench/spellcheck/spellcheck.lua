0b48d9997bc351edd45da8798ca9c27374f2fb5a
bfulgham <bfulgham>
2004-05-25 02:26:33 +0000

    Correctedfor current versions of MLton, Lua, and C++ STL

--- bench/spellcheck/spellcheck.lua
+++ bench/spellcheck/spellcheck.lua
@@ -1,18 +1,14 @@
--- $Id: spellcheck.lua,v 1.1 2004-05-19 18:13:26 bfulgham Exp $
+-- $Id: spellcheck.lua,v 1.2 2004-05-25 02:26:33 bfulgham Exp $
 -- http://www.bagley.org/~doug/shootout/
 -- based on code from Roberto Ierusalimschy
 
-assert(readfrom("Usr.Dict.Words"))
+assert(io.open("Usr.Dict.Words"))
 local dict = {}
-while 1 do
-  local line = read()
-  if line == nil then break end
-  dict[line] = 1
+for line in io.lines("Usr.Dict.Words") do
+  dict[line] = true
 end
-readfrom()    -- closes dictionary
 
-while 1 do
-  local word = read()
-  if word == nil then break end
+for word in io.lines() do
   if not dict[word] then print(word) end
 end
+
