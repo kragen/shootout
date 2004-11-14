7729e9dba4ec9e4c64ae1c03f16684d78765c092
bfulgham <bfulgham>
2004-11-14 08:49:50 +0000

    Cleanups.

---
+++ website/about/psyco-about.tpl.php
@@ -0,0 +1,3 @@
+<p>Psyco 1.2</p>
+<p>Home Page: <a href="http://psyco.sourceforge.net/">http://psyco.sourceforge.net/</a></p>
+<p>Download: <a href="http://psyco.sourceforge.net/download.html">http://psyco.sourceforge.net/download.html</a></p>
