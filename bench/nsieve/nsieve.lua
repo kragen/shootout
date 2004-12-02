206fb8edd834def0a12775a98c842cfc79b6543e
igouy-guest <igouy-guest>
2004-12-02 07:46:46 +0000

    nsieve

---
+++ bench/nsieve/nsieve.lua
@@ -0,0 +1,35 @@
+-- The Great Computer Language Shootout
+-- http://shootout.alioth.debian.org/
+-- contributed by Isaac Gouy
+
+
+function nsieve(m,isPrime)
+   for i=2,m do
+      isPrime[i] = true
+   end
+   count = 0
+
+   for i=2,m do
+      if flags[i] then
+         for k=i+i, m, i do
+            isPrime[k] = false
+         end
+         count = count + 1
+      end
+   end
+   return count
+end
+ 
+
+n = tonumber(arg and arg[1]) or 1
+if n < 2 then n = 2 end
+flags = {}
+
+m = (2^n)*10000 
+print( string.format("Primes up to %8d%8d", m, nsieve(m,flags)) )
+
+m = (2^(n-1))*10000
+print( string.format("Primes up to %8d%8d", m, nsieve(m,flags)) )
+
+m = (2^(n-2))*10000 
+print( string.format("Primes up to %8d%8d", m, nsieve(m,flags)) )
---
+++ website/about/nsieve-about.tpl.php
@@ -0,0 +1,32 @@
+<p>Each program should be implemented the <a href="faq.php?sort=<?=$Sort;?>#sameway"><b>same&nbsp;way</b></a> - the same way as this <a href="benchmark.php?test=nsieve&lang=lua&sort=<?=$Sort;?>">Lua program</a>.
+
+<p>Count the prime numbers from 2 to M, finding each prime number with a na&#239ve implementation of the Sieve of Eratosthenes:
+<ul>
+  <li>create an array of M boolean flags</li>
+  <li>for each index number
+     <ul>
+     <li>if the flag value at that index is true</li>
+     <ul>
+     <li>set all the flag values at multiples of that index false</li>
+     <li>increment the count</li>
+     </ul>
+     </li>
+     </ul>
+   </li>
+</ul>
+Compute 3 prime counts, for M = 2<sup>N</sup> &#215; 10000, 2<sup>N-1</sup> &#215; 10000, and 2<sup>N-2</sup> &#215; 10000. 
+</p>
+
+<p>Correct output N = 2 is:
+<pre>
+Primes up to    40000    4203
+Primes up to    20000    2262
+Primes up to    10000    1229
+</pre>
+</p>
+<br/>
+
+
+<p>The basic benchmark was described in "A High-Level Language Benchmark." BYTE, September 1981, p. 180, Jim Gilbreath.</p>
+
+<p>For more information see Eric W. Weisstein, "Sieve of Eratosthenes." From <a href="http://mathworld.wolfram.com"><i>MathWorld</i></a>--A Wolfram Web Resource.<br/><a href="http://mathworld.wolfram.com/SieveofEratosthenes.html">http://mathworld.wolfram.com/SieveofEratosthenes.html</a> and <a href="http://mathworld.wolfram.com/PrimeCountingFunction.html">http://mathworld.wolfram.com/PrimeCountingFunction.html</a></p>
