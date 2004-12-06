0e3d4b76ef9b279c72eaa9f6003cb83d7a466c2a
igouy-guest <igouy-guest>
2004-12-06 18:00:38 +0000

    *** empty log message ***

--- website/about/oberon2-about.tpl.php
+++ website/about/oberon2-about.tpl.php
@@ -1,5 +1,5 @@
 <p>online book <a href="http://www.oberon.ethz.ch/wirthPiO/">Programming in Oberon - Niklaus Wirth</a></p>
 <p>OO2C translates Oberon-2 source-code to C source-code, and compiles with GCC.</p>
 <?=$Version;?>
-<p>Home Page: <a href="http://oo2c.sourceforge.net/">http://oo2c.sourceforge.net/</a></p>
-<p>Download: <a href="http://oo2c.sourceforge.net/files">http://oo2c.sourceforge.net/files</a></p>
+<p>Home Page: <a href="http://sourceforge.net/projects/ooc">http://sourceforge.net/projects/ooc</a></p>
+<p>Download: <a href="http://sourceforge.net/project/showfiles.php?group_id=3539&package_id=8330">http://sourceforge.net/projects/ooc</a></p>
