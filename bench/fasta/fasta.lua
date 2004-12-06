23726a86c799c927f31adbaa2d8adb7ab67cc3f1
igouy-guest <igouy-guest>
2004-12-06 17:54:37 +0000

    lua fasta; C# nsieve takfp

--- bench/fasta/fasta.lua
+++ bench/fasta/fasta.lua
@@ -20,13 +20,16 @@ end
 
 local function mkCumulative(a)
    local cp = 0;
-   for k,v in pairs(a) do cp = cp + v; a[k] = cp end
+   for i,v in ipairs(a) do 
+      cp = cp + v.p; 
+      a[i].p = cp 
+   end
 end
 
 local function selectRandom(a)
    local r = random(1)
-   for k,v in pairs(a) do 
-      if r < v then return k end 
+   for i,v in ipairs(a) do 
+      if r < v.p then return v.c end 
    end
 end
 
@@ -50,38 +53,42 @@ end
 -- Main -- define alphabets, make 5 fragments
 
 dna1 = { 
-   a = 0.3029549426680
-  ,c = 0.1979883004921
-  ,g = 0.1975473066391
-  ,t = 0.3015094502008 
+    { c= "a", p= 0.3029549426680 }
+   ,{ c= "c", p= 0.1979883004921 }
+   ,{ c= "g", p= 0.1975473066391 }
+   ,{ c= "t", p= 0.3015094502008 }
   }
 mkCumulative(dna1)
 
+
 dna2 = { 
-    a = 0.25
-   ,c = 0.125
-   ,g = 0.125
-   ,t = 0.25
-   ,U = 0.019230769231
-   ,R = 0.019230769231
-   ,Y = 0.019230769231
-   ,K = 0.019230769231
-   ,M = 0.019230769231
-   ,S = 0.019230769231
-   ,W = 0.019230769231
-   ,B = 0.019230769231
-   ,D = 0.019230769231
-   ,H = 0.019230769231
-   ,V = 0.019230769231
-   ,N = 0.019230769231
-   ,["-"] = 0.019230769231
-   }          
-mkCumulative(dna2)          
+    { c= "a", p= 0.250 }
+   ,{ c= "c", p= 0.125 }
+   ,{ c= "g", p= 0.125 }
+   ,{ c= "t", p= 0.250 }
+
+   ,{ c= "U", p= 0.019230769231 }
+   ,{ c= "R", p= 0.019230769231 }
+   ,{ c= "Y", p= 0.019230769231 }
+   ,{ c= "K", p= 0.019230769231 }
+   ,{ c= "M", p= 0.019230769231 }
+   ,{ c= "S", p= 0.019230769231 }
+   ,{ c= "W", p= 0.019230769231 }
+   ,{ c= "B", p= 0.019230769231 }
+   ,{ c= "D", p= 0.019230769231 }
+   ,{ c= "H", p= 0.019230769231 }
+   ,{ c= "V", p= 0.019230769231 }
+   ,{ c= "N", p= 0.019230769231 }
+   ,{ c= "-", p= 0.019230769231 }
+  }       
+mkCumulative(dna2)        
+
+
 
 local n = tonumber(arg and arg[1]) or 1000
 
-mkFastaFragment('TestOne','test fragment',dna2,n)
-mkFastaFragment('TestTwo','test fragment',dna1,n)
-mkFastaFragment('TestThree','test fragment',dna2,n)
-mkFastaFragment('TestFour','test fragment',dna1,n)
-mkFastaFragment('TestFive','test fragment',dna2,n)
+mkFastaFragment('TestOne','test fragment',dna2,n*1)
+mkFastaFragment('TestTwo','test fragment',dna1,n*2)
+mkFastaFragment('TestThree','test fragment',dna2,n*3)
+mkFastaFragment('TestFour','test fragment',dna1,n*4)
+mkFastaFragment('TestFive','test fragment',dna2,n*5)
