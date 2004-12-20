71e36d7c9d5cda7a2e97c1aee739461167dfe0d4
igouy-guest <igouy-guest>
2004-12-20 22:45:52 +0000

    *** empty log message ***

---
+++ website/about/mandelbrot-about.tpl.php
@@ -0,0 +1,4 @@
+<p>Each program should plot the Mandelbrot set [-1.5-i,0.5+i] on an N-by-N bitmap. Write output byte-by-byte in <a href="http://www-info2.informatik.uni-wuerzburg.de/mitarbeiter/wolfram/lehre/bildformate.html#pbm">portable bitmap format</a>.</p>
+
+<p>For more information see Eric W. Weisstein, "Mandelbrot Set." From <a href="http://mathworld.wolfram.com"><i>MathWorld</i></a>--A Wolfram Web Resource.<br/><a href="http://mathworld.wolfram.com/MandelbrotSet.html">http://mathworld.wolfram.com/MandelbrotSet.html</a></p>
+
