ed4e0f40773d4c6cd4391413a9013d43fa4313c6
igouy-guest <igouy-guest>
2004-12-20 23:04:37 +0000

    *** empty log message ***

--- bench/mandelbrot/mandelbrot.lua
+++ bench/mandelbrot/mandelbrot.lua
@@ -6,6 +6,7 @@
 local width = tonumber(arg and arg[1]) or 100
 local height = width
 local m, byte, bitnum, limit2 = 50, 0, 7, 4.0
+local isOverLimit = false
 local Zr, Zi, Cr, Ci, Tr, Ti
 
 io.write("P4\n", width, " ", height, "\n")
@@ -17,14 +18,16 @@ for y=0,height-1 do
       Cr = 2*x / width - 1.5
       Ci = 2*y / height - 1
 
-      for i=1,m do
+      i = 1
+      repeat
          Tr = Zr*Zr - Zi*Zi + Cr
          Ti = 2*Zr*Zi + Ci
          Zr = Tr; Zi = Ti
-         if (Zr*Zr + Zi*Zi > limit2) then break end
-      end
+         i = i + 1
+         isOverLimit = Zr*Zr + Zi*Zi > limit2
+      until isOverLimit or (i > m) 
 
-      if (Zr*Zr + Zi*Zi <= limit2) then 
+      if not isOverLimit then 
          byte = byte + math.floor(2^bitnum)     
       end
       bitnum = bitnum - 1
