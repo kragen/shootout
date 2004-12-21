fe218cbadc976ac3d7f943aaa914b0ab4d077ef2
igouy-guest <igouy-guest>
2004-12-21 06:11:22 +0000

    *** empty log message ***

--- bench/mandelbrot/mandelbrot.lua
+++ bench/mandelbrot/mandelbrot.lua
@@ -5,7 +5,7 @@
 
 local width = tonumber(arg and arg[1]) or 100
 local height = width
-local m, byte, bitnum, limit2 = 50, 0, 7, 4.0
+local m, bits, bitnum, limit2 = 50, 0, 0, 4.0
 local isOverLimit = false
 local Zr, Zi, Cr, Ci, Tr, Ti
 
@@ -18,22 +18,29 @@ for y=0,height-1 do
       Cr = 2*x / width - 1.5
       Ci = 2*y / height - 1
 
-      i = 1
+      i = 0
       repeat
          Tr = Zr*Zr - Zi*Zi + Cr
          Ti = 2*Zr*Zi + Ci
          Zr = Tr; Zi = Ti
          i = i + 1
          isOverLimit = Zr*Zr + Zi*Zi > limit2
-      until isOverLimit or (i > m) 
+      until isOverLimit or (i == m) 
 
-      if not isOverLimit then 
-         byte = byte + math.floor(2^bitnum)     
+
+      -- encode as bit value
+
+      bits = bits * 2
+      if not isOverLimit then bits = bits + 1 end
+      bitnum = bitnum + 1
+
+      if x == width - 1 then 
+         for i = bitnum,7 do bits = bits * 2 end; bitnum = 8
       end
-      bitnum = bitnum - 1
 
-      if (bitnum < 0) or (x == (width-1)) then 
-         io.write(string.char(byte)); byte = 0; bitnum = 7            
+
+      if bitnum == 8 then
+         io.write(string.char(bits)); bits = 0; bitnum = 0          
       end
       
    end
