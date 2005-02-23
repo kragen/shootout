545b0cb093d47f3d5d7e7334beff19e3715207fd
bfulgham <bfulgham>
2005-02-23 01:00:18 +0000

    Let's remove unused stuff.

--- html/nav.php
+++
@@ -1,4 +0,0 @@
-<div id="banner"><center>
-<a href="http://www.bagley.org/~doug/shootout/">[The Original Shootout]</a> &nbsp; 
-<a href="http://dada.perl.it/shootout/">[The Win32 Shootout]</a> &nbsp; 
-</center></div>
