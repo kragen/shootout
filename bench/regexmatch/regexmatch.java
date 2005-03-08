da194d1722f6821725643222a04782c275babb8e
igouy-guest <igouy-guest>
2005-03-08 21:31:01 +0000

    *** empty log message ***

--- bench/regexmatch/regexmatch.java
+++ bench/regexmatch/regexmatch.java
@@ -1,78 +1,55 @@
-// $Id: regexmatch.java,v 1.1 2004-05-23 07:14:28 bfulgham Exp $
-// http://www.bagley.org/~doug/shootout/
+/* The Great Computer Language Shootout
+   http://shootout.alioth.debian.org/
 
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.lang.reflect.*;
-import com.oroinc.text.regex.*;
+   contributed by Paul Lofte
+*/
 
-public class regexmatch {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+ 
+public class regexmatch 
+   {
+   public static void main(String args[]) {
+      try {
+         String regex = "(^|^\\D*[^\\(\\d])"    // must be preceeded by non-digit
+            + "((\\(\\d\\d\\d\\))|(\\d\\d\\d))" // match 2: Area Code inner match 3: area with perens, 
+                                                //inner match 4: without perens 
+            + "[ ]"                             // area code followed by one space
+            + "(\\d\\d\\d)"                     //match 5: prefix of 3 digits
+            + "[ -]"                            // prefix followed by space or dash
+            + "(\\d\\d\\d\\d)"                  // match 6: last 4 digits
+            + "(\\D*$)";                        // followed by non numeric chars 
 
-    public static void main(String args[])
-	throws IOException, MalformedPatternException {
+         Pattern phonePattern = Pattern.compile(regex);
+         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+         String line;
+         int count = 0;
+         while ((line = in.readLine()) != null) {
+            Matcher match = phonePattern.matcher(line);
+            if (match.matches()) {
+               count++;
+               if (match.group(3) != null) {
+                     System.out.println(count + ": " 
+                                       + match.group(3) + " " 
+                                       + match.group(5) + "-" 
+                                       + match.group(6));
+               } else {
+                     System.out.println(count + ": (" 
+                                       + match.group(4) + ") " 
+                                       + match.group(5) + "-" 
+                                       + match.group(6));
+               }
+            }
 
-	int n = Integer.parseInt(args[0]);
-	LinkedList lines = new LinkedList();
-	try {
-            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-	    String line;
-	    while ((line = in.readLine()) != null) {
-		lines.addLast(line);
-	    }
-	    in.close();
-        } catch (IOException e) {
-            System.err.println(e);
-            return;
-        }
-
-	String regex = 
-	    "(?: ^ | [^\\d\\(])		# must be preceeded by non-digit\n" +
-	    "(?:			# area code is 3 digits\n" +
-	    " \\((\\d\\d\\d)\\)		# match 1: with parens\n" +
-	    "|				# or\n" +
-	    " (\\d\\d\\d)		# match 2: without parens\n" +
-	    ")				# if match1 then match right paren\n" +
-	    "[ ]			# area code followed by one space\n" +
-	    "(\\d\\d\\d)		# match 3: prefix of 3 digits\n" +
-	    "[ -]			# separator is either space or dash\n" +
-	    "(\\d\\d\\d\\d)		# match 4: last 4 digits\n" +
-	    "(?:\\D|$)			# must be followed by a non-digit\n";
-
-	PatternMatcher matcher = new Perl5Matcher();
-	PatternCompiler compiler = new Perl5Compiler();
-	Pattern pattern = null;
-
-	try {
-	    pattern = compiler.compile(regex, Perl5Compiler.EXTENDED_MASK);
-	} catch(MalformedPatternException e) {
-	    System.err.println("Bad pattern.");
-	    System.err.println(e.getMessage());
-	    System.exit(1);
-	}
-
-	for (int i = 0; i < n; i++) {
-	    LinkedList phones = new LinkedList();
-	    for (ListIterator li = lines.listIterator(); li.hasNext();) {
-		String line = (String)li.next();
-		if (matcher.contains(line, pattern)) {
-		    MatchResult match = matcher.getMatch();
-		    String num = "";
-		    if (match.group(1) != null) {
-			num = "(" + match.group(1) + ") " + match.group(3) + "-" + match.group(4);
-		    } else {
-			num = "(" + match.group(2) + ") " + match.group(3) + "-" + match.group(4);
-		    }
-		    phones.addLast(num);
-		}
-	    }
-	    if (i == (n - 1)) {
-		int count = 1;
-		for (ListIterator li = phones.listIterator(); li.hasNext();) {
-		    String num = (String)li.next();
-		    System.out.println(count++ + ": " + num);
-		}
-	    }
-	}
-    }
+         }
+         in.close();
+            
+      } catch (IOException e) {
+         System.err.println(e);
+      }
+      System.exit(0);
+   }
 }
