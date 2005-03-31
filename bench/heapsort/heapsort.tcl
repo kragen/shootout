0635374cbc064d3c2a46df781cca203d8c342bda
sgeard-guest <sgeard-guest>
2005-03-31 14:31:50 +0000

    301337 - updates from Daniel South

--- bench/heapsort/heapsort.tcl
+++ bench/heapsort/heapsort.tcl
@@ -1,73 +1,56 @@
 #!/usr/bin/tclsh
-# $Id: heapsort.tcl,v 1.1 2004-05-19 18:10:11 bfulgham Exp $
+# $Id: heapsort.tcl,v 1.2 2005-03-31 14:31:50 sgeard-guest Exp $
 # http://www.bagley.org/~doug/shootout/
 # sped up by Miguel Sofer's function generator
 
-set IM 139968
-set IA   3877
-set IC  29573
-
-set last 42
+foreach {IM IA IC last} {139968 3877 29573 42} break
 
 proc make_gen_random {} {
-    global IM IA IC
-    set params [list IM $IM IA $IA IC $IC]
+    set params [list IM $::IM IA $::IA IC $::IC]
     set body [string map $params {
-        global last
-        expr {($max * [set last [expr {($last * IA + IC) % IM}]]) / IM}
-    }]
+	expr {($max * [set ::last [expr {($::last * IA + IC) % IM}]]) / IM}}]
     proc gen_random {max} $body
 }
 
 proc heapsort {n ra_name} {
-    upvar $ra_name ra
+    upvar 1 $ra_name ra
 
-    set j 0
-    set i 0
-    set rra 0.0
     set l [expr {($n >> 1) + 1}]
-    set ir $n
     while 1 {
         if {$l > 1} {
-	    incr l -1
-            set rra $ra($l)
+            set rra [lindex $ra [incr l -1]]
         } else {
-	    set rra $ra($ir)
-	    set ra($ir) $ra(1)
-	    incr ir -1
-	    if {$ir == 1} {
-                set ra(1) $rra
-                return
-            }
+	    set rra [lindex $ra $n]
+	    lset ra $n [lindex $ra 1]
+	    if {[incr n -1] == 1} {return [lset ra 1 $rra]}
         }
 	set i $l
 	set j [expr {$l << 1}]
-        while {$j <= $ir} {
-	    if {($j < $ir) && ($ra($j) < $ra([expr {$j + 1}]))} {
+        while {$j <= $n} {
+	    if {$j < $n && [lindex $ra $j] < [lindex $ra [expr {$j + 1}]]} {
 		incr j
 	    }
-            if {$rra < $ra($j)} {
-		set ra($i) $ra($j)
+            if {$rra < [lindex $ra $j]} {
+		lset ra $i [lindex $ra $j]
 		set i $j
-                set j [expr {$j + $i}]
+                incr j $j
             } else {
-		set j [expr {$ir + 1}]
+		set j $n
+		incr j
             }
         }
-        set ra($i) $rra
+        lset ra $i $rra
     }
 }
 
-proc main {} {
-    global argv
-    set n [lindex $argv 0]
+proc main {n} {
     make_gen_random
-
-    for {set i 1} {$i <= $n} {incr i} {
-	set ary($i) [gen_random 1.0]
-    }
-    heapsort $n ary
-    puts [format "%.10f" $ary($n)]
+    for {set i 1} {$i <= $n} {incr i} {lappend data [gen_random 1.0]}
+    incr n -1
+    heapsort $n data
+    puts [format "%.10f" [lindex $data $n]]
 }
 
-main
+set N [lindex $argv 0]
+if {$N < 1} {set N 1}
+main $N
--- bench/knucleotide/knucleotide.tcl
+++ bench/knucleotide/knucleotide.tcl
@@ -1,35 +1,32 @@
 #!/usr/bin/tclsh
 
-# The Great Computer Language Shootout
-# http://shootout.alioth.debian.org/
-#
-# contributed by Daniel South
-
 proc kFrequency {s k framesize} {
-   global freq
-   set n [string length $s]
-   incr k -1
-   incr n -$k
-   for {set i [incr framesize -1]} {$i < $n} {incr i} {
-      set c [string range $s $i [incr i $k]]
-      if {[catch {incr freq($c)}]} {set freq($c) 1}
-   }
+    global freq
+
+    set n [string length $s]
+    incr k -1
+    incr n -$k
+    for {set i $framesize} {$i < $n} {incr i} {
+	set c [string range $s $i [incr i $k]]
+	if {[catch {incr freq($c)}]} {set freq($c) 1}
+    }
 }
 
-proc frequency {s k} {
-   array unset ::freq
-   set sum 0
 
-   for {set i 0} {$i < $k} {} {kFrequency $s $k [incr i]}
-   foreach {fragment count} [array get ::freq] {
-      lappend sortheap [list $fragment $count]
-      incr sum $count
-   }
-   foreach item [lsort -integer -index 1 -decreasing [lsort $sortheap]] {
-      set percent [expr {double([lindex $item 1]) / $sum * 100}]
-      puts [format "%s %0.2f" [lindex $item 0] $percent]
-   }
-   puts ""
+proc frequency {s k} {
+    array unset ::freq
+    set sum 0
+
+    for {set i 0} {$i < $k} {} {kFrequency $s $k [incr i]}
+    foreach {fragment count} [array get ::freq] {
+	lappend sortheap [list $fragment $count]
+	incr sum $count
+    }
+    foreach item [lsort -integer -index 1 -decreasing $sortheap] {
+	set percent [expr {double([lindex $item 1]) / $sum * 100}]
+	puts [format "%s %0.2f" [lindex $item 0] $percent]
+    }
+    puts ""
 }
 
 proc count {s fragment} {
@@ -46,7 +43,7 @@ proc main {} {
     while {[gets stdin line] != -1} {if {[string match ">THREE*" $line]} break}
     while {[gets stdin line] != -1} {append sequence $line}
     set sequence [string toupper $sequence]
-    
+
     frequency $sequence 1
     frequency $sequence 2
 
