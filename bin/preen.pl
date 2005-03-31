bcfa06b07ae7cb673dc582c8a88ea5078c7a52c9
bfulgham <bfulgham>
2005-03-31 01:05:57 +0000

    Add utility to verify file permissions on scripts.

---
+++ bin/preen.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl -w
+
+my @scripts = ( "gawk", "mawk", "bash", "rep", "lua", "newlisp",
+	     "php", "perl", "python", "psyco", "pike", "ruby",
+	     "guile", "mzscheme", "slang", "tcl", "xemacs" );
+ 
+my @files;
+
+foreach $lang (@scripts)
+{
+	#print "Checking $lang\n";
+	push @files, `find . -name *.$lang`;
+}
+
+print "I found:\n";
+foreach $i (@files)
+{
+	chomp($i);
+	if (! -x $i)
+	{
+		print "$i\n";
+		chmod 0755, $i or die "Could not chmod $file: $!\n";
+		system ("touch -f $i") == 0 or die "Could not touch $file: $!!\n";
+	}
+}
+print "... done.\n";
