821849823e0147b41483f34d8196d9a0049db0ef
igouy-guest <igouy-guest>
2005-04-16 17:00:45 +0000

    website - revert to old  fibonacci definition and only show on The Doug Bagley

--- website/about/fibo-about.tpl.php
+++ website/about/fibo-about.tpl.php
@@ -1,7 +1,7 @@
-<p>Each program should calculate the Fibonacci function using the same na&#239ve recursive-algorithm</p>
+<p>Each program should calculate a version of the Fibonacci function using the same na&#239ve recursive-algorithm (Mathematical Handbook 2nd ed., G. Korn, T. Korn, 1968. paragraph 8.7-2).</p>
 <pre>
 F(n)
-  n = 0     = 0
+  n = 0     = 1
   n = 1     = 1
   otherwise = F(n-2) + F(n-1)
 </pre>
@@ -9,8 +9,9 @@ F(n)
 
 <p>Calculate F(N). Correct output N = 32 is:</p>
 <pre>
-2178309
+3524578
 </pre>
 <br />
 
-<p>For more information see Eric W. Weisstein, "Fibonacci Number." From <a href="http://mathworld.wolfram.com"><i>MathWorld</i></a>--A Wolfram Web Resource.<br/><a href="http://mathworld.wolfram.com/FibonacciNumber.html">http://mathworld.wolfram.com/FibonacciNumber.html</a></p>
+
+<p>For a different version of the Fibonacci function see Eric W. Weisstein, "Fibonacci Number." From <a href="http://mathworld.wolfram.com"><i>MathWorld</i></a>--A Wolfram Web Resource.<br/><a href="http://mathworld.wolfram.com/FibonacciNumber.html">http://mathworld.wolfram.com/FibonacciNumber.html</a></p>
