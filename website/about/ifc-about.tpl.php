a4bbf7cf903a17075023c8be5864d4aea5006203
igouy-guest <igouy-guest>
2005-04-27 19:03:53 +0000

    *** empty log message ***

--- website/about/ifc-about.tpl.php
+++ website/about/ifc-about.tpl.php
@@ -1,3 +1,3 @@
 <?=$Version;?>
-<p>Home Page: <a href="http://www.intel.com/software/products/compilers/linux">http://www.intel.com/software/products/compilers/linux</a></p>
-<p>Download: <a href="http://www.intel.com/software/products/compilers/flin/">http://www.intel.com/software/products/compilers/flin/</a></p>
+<p>Home Page: <a href="http://www.intel.com/software/products/noncom/">http://www.intel.com/software/products/noncom/</a></p>
+<p>Download: <a href="http://www.intel.com/software/products/noncom/">http://www.intel.com/software/products/noncom/</a></p>
