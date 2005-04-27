0b43384e055e2b90b3e9dad27c2db898747d14ac
igouy-guest <igouy-guest>
2005-04-27 23:51:18 +0000

    *** empty log message ***

--- website/about/icpp-about.tpl.php
+++ website/about/icpp-about.tpl.php
@@ -1,3 +1,3 @@
 <?=$Version;?>
-<p>Home Page: <a href="http://www.intel.com/software/products/compilers/linux">http://www.intel.com/software/products/compilers/linux</a></p>
-<p>Download: <a href="http://www.intel.com/software/products/compilers/clin/">http://www.intel.com/software/products/compilers/clin/</a></p>
+<p>Home Page: <a href="http://www.intel.com/software/products/noncom/">http://www.intel.com/software/products/noncom/</a></p>
+<p>Download: <a href="http://www.intel.com/software/products/noncom/">http://www.intel.com/software/products/noncom/</a></p>
