82428ffbc74484529c70a3b0abb66455122cf095
igouy-guest <igouy-guest>
2005-05-08 21:47:28 +0000

    website xhtml fixes

--- website/misc/benchmarking.php
+++ website/misc/benchmarking.php
@@ -18,7 +18,7 @@
   <li><p><a href="http://openmap.bbn.com/~kanderso/performance/postscript/fannkuch.ps">"Performing Lisp Analysis of the FANNKUCH Benchmark"</a> (55KB postscript)</p></li>
   <li><p><a href="http://www-128.ibm.com/developerworks/java/library/j-jtp02225.html?ca=drs-j0805#4.0">Java theory and practice: Anatomy of a flawed microbenchmark. Is there any other kind?</a></p></li>
   <li><p><a href="http://www.dreamsongs.com/NewFiles/Timrep.pdf">Performance and Evaluation of Lisp Systems</a>, Richard P. Gabriel, 1985 (1.1MB pdf)</p></li>
-  <li><p><a href="http://h21007.www2.hp.com/dspp/tech/tech_TechDocumentDetailPage_IDX/1,1701,2155,00.html">Writing micro-benchmarks for Java™ HotSpot JVM</a></p></li>
+  <li><p><a href="http://h21007.www2.hp.com/dspp/tech/tech_TechDocumentDetailPage_IDX/1,1701,2155,00.html">Writing micro-benchmarks for Java<sup>&#174;</sup> HotSpot JVM</a></p></li>
 </ul>
 <p>Many benchmark suites are designed to help language implementors optimize compiler designs:</p>
 <ul>  
@@ -30,14 +30,15 @@
 
 <tr class="b"><td><a class="ab" href="#app" name="app"><strong>"your application is the ultimate benchmark"</strong></a></td></tr>
 <tr><td>
-<blockquote>"In order to find the optimal cost/benefit ratio, Wirth used a highly intuitive metric, 
+<blockquote><p>"In order to find the optimal cost/benefit ratio, Wirth used a highly intuitive metric, 
 the origin of which is unknown to me but that may very well be Wirth's own invention. He used <strong>the 
 compiler's self-compilation speed</strong> as a measure of the compiler's quality. Considering that Wirth's 
 compilers were written in the languages they compiled, and that compilers are substantial and non-trivial 
 pieces of software in their own right, this introduced a highly practical benchmark that directly
 contested a compiler's complexity against its performance. Under the self compilation speed benchmark,
 only those optimizations were allowed to be incorporated into a compiler that accelerated it by so much
-that the intrinsic cost of the new code addition was fully compensated." <a href="http://www.ics.uci.edu/~franz/pubs-pdf/BC03.pdf"><br/>Oberon: The Overlooked Jewel</a> page 4. (73KB pdf)</blockquote>
+that the intrinsic cost of the new code addition was fully compensated." <a href="http://www.ics.uci.edu/~franz/pubs-pdf/BC03.pdf"><br/>Oberon: The Overlooked Jewel</a> page 4. (73KB pdf)
+</p></blockquote>
 </td></tr>
 
 
