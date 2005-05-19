bc7c3c11b3e25f172eba58c1a9d2c1b029da3c4d
igouy-guest <igouy-guest>
2005-05-19 17:54:42 +0000

    binarytrees

--- website/about/binarytrees-about.tpl.php
+++ website/about/binarytrees-about.tpl.php
@@ -3,7 +3,7 @@
    <li>define a tree node class and methods, or a tree node record and functions</li>
    <li>allocate a binary tree to 'stretch' memory, check it exists, and deallocate it</li>
    <li>allocate a long-lived binary tree which will live-on while other trees are allocated and deallocated</li>   
-   <li>allocate, walk, and deallocate many top-down and bottom-up binary trees
+   <li>allocate, walk, and deallocate many bottom-up binary trees
       <ul>
       <li>allocate a tree</li>
       <li>walk the tree nodes, sum and cancel node items (and maybe deallocate the node)</li>
@@ -14,13 +14,14 @@
 </ul>
 <br />
 
-<p>There are reference implementations in Oberon-2, C#, and PHP.</p>
+<p>There are reference implementations in OCaml, C#, and PHP.</p>
 <p>Correct output N = 10 is:</p>
-<pre>2048     trees of depth 4        check: 15392
-512      trees of depth 6        check: 776
-128      trees of depth 8        check: 2
+<pre>stretch tree of depth 11         check: -4095
+2048     trees of depth 4        check: 31776
+512      trees of depth 6        check: 1800
+128      trees of depth 8        check: 66
 32       trees of depth 10       check: 0
-OK
+long lived tree of depth 10      check: -2047
 </pre>
 <br/>
 
