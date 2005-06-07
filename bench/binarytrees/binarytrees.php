942f87925cf6fbb3cdbd2c4eb3de11825e4c00bc
igouy-guest <igouy-guest>
2005-06-07 20:47:20 +0000

    *** empty log message ***

--- bench/binarytrees/binarytrees.php
+++ bench/binarytrees/binarytrees.php
@@ -1,7 +1,7 @@
 <? /* The Great Computer Language Shootout 
    http://shootout.alioth.debian.org/
    
-   contributed by Isaac Gouy (PHP novice)
+   contributed by Isaac Gouy (PHP novice) 
    
    php -q binarytrees.php 12
 */
