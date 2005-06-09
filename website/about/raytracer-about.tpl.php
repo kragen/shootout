59188b8e4ae633a2d40530aec3a1bf31bfa60e6d
igouy-guest <igouy-guest>
2005-06-09 23:35:48 +0000

    website ray-tracer

---
+++ website/about/raytracer-about.tpl.php
@@ -0,0 +1,5 @@
+<p>Correct output N = 32 is in this 1KB <a href="iofile.php?test=<?=$SelectedTest;?>&amp;lang=<?=$SelectedLang;?>&amp;sort=<?=$Sort;?>&amp;file=output">output file</a>.</p>
+
+<img src="<?=IMAGE_PATH;?>raytracer200.png" alt="Ray-tracer output N=200,converted to PNG" height="200" width="200" />
+
+<p>Thanks to Jon Harrop for this benchmark.</p>
