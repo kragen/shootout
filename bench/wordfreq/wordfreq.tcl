0ffa6cbb83d5c4de7dac436e881af8c60a59fb7d
sgeard-guest <sgeard-guest>
2005-06-10 21:22:02 +0000

    added speed improvement by Roy Terry

--- bench/wordfreq/wordfreq.tcl
+++ bench/wordfreq/wordfreq.tcl
@@ -1,20 +1,27 @@
 #!/usr/bin/tclsh
-# $Id: wordfreq.tcl,v 1.3 2005-03-31 15:47:06 sgeard-guest Exp $
+#
+# $Id: wordfreq.tcl,v 1.4 2005-06-10 21:22:02 sgeard-guest Exp $
+#
 # http://shootout.alioth.debian.org/
 # with help from: Tom Wilkason and Branko Vesligaj
-
+#
+# Speed increase by Roy Terry
 proc main {} {
+
+    set punc {\{\}"'\\!@#$%^&*()-_+=|[]:;,.~`?0123456789}
+    foreach c [split $punc ""] {lappend map $c " "}
     while {[set data [read stdin 4096]] != {}} {
-	if {[gets stdin extra] != -1} {append data $extra}
-	regsub -all {[^[:alpha:]]+} $data { } line
-	foreach word [string tolower $line] {
-	    if {[catch {incr count($word)}]} {set count($word) 1}
-	}
+        if {[gets stdin extra] != -1} {append data $extra}
+        set line [string map $map $data]
+        foreach word [string tolower $line] {
+            if {[catch {incr count($word)}]} {set count($word) 1}
+        }
     }
     foreach {word cnt}  [array get count] {
-	lappend lines [format "%7d %s" $cnt $word]
+        lappend lines [format "%7d %s" $cnt $word]
     }
     puts [join [lsort -decreasing $lines] "\n"]
 }
+
 
 main
