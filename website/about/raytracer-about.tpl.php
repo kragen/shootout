04da36c0f3b210fa12ae9a76523ac29cf4510156
igouy-guest <igouy-guest>
2005-06-14 02:05:54 +0000

    website

--- website/about/raytracer-about.tpl.php
+++ website/about/raytracer-about.tpl.php
@@ -4,5 +4,5 @@
 <img src="<?=IMAGE_PATH;?>raytracer200.png" alt="Ray-tracer output N=200,converted to PNG" height="200" width="200" />
 
 
-<p><br />Correct output N=32 is in this 1KB <a href="iofile.php?test=<?=$SelectedTest;?>&amp;lang=<?=$SelectedLang;?>&amp;sort=<?=$Sort;?>&amp;file=output">output file</a>.</p>
+<p><br />Correct output N=32 is in this 1KB <a href="iofile.php?test=<?=$SelectedTest;?>&amp;lang=<?=$SelectedLang;?>&amp;sort=<?=$Sort;?>&amp;file=output&amp;ext=pgm">output file</a>.</p>
 <p>Thanks to Jon Harrop for this benchmark.</p>
--- website/lib/iofile.php
+++ website/lib/iofile.php
@@ -20,6 +20,9 @@ else { $S = 'cpu'; }
 if (isset($HTTP_GET_VARS['file'])){ $F = $HTTP_GET_VARS['file']; } 
 else { $F = 'input'; }
 
+if (isset($HTTP_GET_VARS['ext'])){ $X = $HTTP_GET_VARS['ext']; } 
+else { $X = 'txt'; }
+
 $TestName = $Tests[$T][TEST_NAME];
 
 if ($F == 'input'){ $Title = $TestName.' input file'; } 
@@ -38,8 +41,8 @@ $Page->set('Sort', $S);
 
 $Body = & new Template(LIB_PATH); 
 $Body->set('Title', $Title);
-$Body->set('Download', DOWNLOAD_PATH.$T.SEPARATOR.$F.'.txt');
-$Body->set('Text', HtmlFragment( DOWNLOAD_PATH.$T.SEPARATOR.$F.'.txt' ));
+$Body->set('Download', DOWNLOAD_PATH.$T.SEPARATOR.$F.'.'.$X);
+$Body->set('Text', HtmlFragment( DOWNLOAD_PATH.$T.SEPARATOR.$F.'.'.$X ));
 
 $Page->set('PageBody', $Body->fetch('iofile.tpl.php'));
 $Page->set('Robots', '<meta name="robots" content="noindex,nofollow" />');
