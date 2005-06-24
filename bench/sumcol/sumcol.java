fa27fb0623ab17f8336ac4ad74dba8c40fd78fe4
igouy-guest <igouy-guest>
2005-06-24 16:12:05 +0000

    bad log files 6 experiment

--- bench/sumcol/sumcol.java
+++ bench/sumcol/sumcol.java
@@ -1,23 +1,23 @@
-// $Id: sumcol.java,v 1.3 2005-06-21 05:36:55 igouy-guest Exp $
-// http://www.bagley.org/~doug/shootout/
+// $Id: sumcol.java,v 1.4 2005-06-24 16:12:05 igouy-guest Exp $
+// http://www.bagley.org/~doug/shootout/ 
 
 import java.io.*;
 import java.util.*;
 import java.text.*;
 
 public class sumcol {
-    public static void main(String[] args) {
-	int sum = 0;
-	String line;
-        try {
-            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-            while ((line = in.readLine()) != null) {
-		sum = sum + Integer.parseInt(line);
-            }
-        } catch (IOException e) {
-            System.err.println(e);
-            return;
-        }
-	System.out.println(Integer.toString(sum));
-    }
+   public static void main(String[] args) {
+      int sum = 0;
+      String line;
+      try {
+         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+         while ((line = in.readLine()) != null) {
+            sum = sum + Integer.parseInt(line);
+         }
+      } catch (IOException e) {
+         System.err.println(e);
+         return;
+      }
+      System.out.println(Integer.toString(sum));
+   }
 }
