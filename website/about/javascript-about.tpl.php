da88417dab8d170a6ea5021c307b9922da80ef1b
igouy-guest <igouy-guest>
2005-06-25 15:11:13 +0000

    *** empty log message ***

---
+++ website/about/javascript-about.tpl.php
@@ -0,0 +1,7 @@
+<p>ECMAScript standard <a href="http://www.mozilla.org/js/language/E262-3.pdf">"ECMA-262 Edition 3"</a> 1.2MB pdf</p>
+
+<?=$Version;?>
+
+<p>Home Page: <a href="http://www.mozilla.org/js/language/">http://www.mozilla.org/js/language/</a></p>
+
+<p>Download: <a href="http://www.mozilla.org/js/spidermonkey/">http://www.mozilla.org/js/spidermonkey/</a></p>
