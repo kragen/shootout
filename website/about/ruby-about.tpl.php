207d89554c5a79a9c4dcb0dc9d2b0995eab0262e
igouy-guest <igouy-guest>
2005-08-17 17:17:38 +0000

    *** empty log message ***

--- website/about/ruby-about.tpl.php
+++ website/about/ruby-about.tpl.php
@@ -1,4 +1,7 @@
 <p>online book <a href="http://www.rubycentral.com/book/">"Programming Ruby: The Pragmatic Programmer's Guide</a></p>
+
 <?=$Version;?>
+
 <p>Home Page: <a href="http://www.ruby-lang.org/">http://www.ruby-lang.org/</a></p>
-<p>Download: <a href="ftp://ftp.netlab.co.jp/pub/lang/ruby/">ftp://ftp.netlab.co.jp/pub/lang/ruby/</a></p>
+
+<p>Download: <a href="http://www.ruby-lang.org/en/20020102.html">http://www.ruby-lang.org/en/20020102.html</a></p>
