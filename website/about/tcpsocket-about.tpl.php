d6b479fdf86c8aec8e37ad7359b0d7712743b99f
igouy-guest <igouy-guest>
2005-08-24 19:25:17 +0000

    *** empty log message ***

---
+++ website/about/tcpsocket-about.tpl.php
@@ -0,0 +1,49 @@
+<p>Each program will be started first with argument N negated (reply to requests), and then once more with argument N and a lower priority (make requests).</p>
+<ul>
+   <li>N&#60;=0, start a TCP server that will reply to requests
+      <ul>
+      <li>make 10*N replies    
+      <ul>       
+         <li>read a request (of known size) from the socket</li>
+         <li>write a reply to the socket</li>   
+         <li>after M<sub>i</sub>*N requests change to a new reply size</li>    
+      </ul>           
+      </li>  
+      <li>stop the server</li>                    
+      </ul>         
+   </li>
+   
+   <li>N&#62;0, start a TCP client that will make requests
+      <ul>
+      <li>make 10*N +3 requests    
+      <ul>              
+         <li>write a request to the socket</li>
+         <li>read a reply (of known size) from the socket</li>
+         <li>after M<sub>i</sub>*N requests expect a new reply size</li>   
+      </ul>           
+      </li>                    
+      <li>count the replies, and sum the bytes in the replies</li>
+      <li>close the socket</li>
+      <li>print the count and sum</li>      
+      </ul>       
+   </li>   
+   
+   <li>Buffer size = 1024 bytes</li>    
+   <li>Request size = 64 bytes</li>    
+   <li>Reply sizes
+      <ul>   
+      <li>The first 2*N replies should be 64 bytes</li>
+      <li>The next 7*N replies should be 4096 bytes</li>
+      <li>The last 1*N replies should be 409600 bytes</li>                     
+      </ul>       
+   </li>       
+  
+</ul>    
+<br/>
+<p>Each program should leave the sockets available for immediate reuse.</p>
+
+<p>Correct output N = 10 is:</p>
+<pre>
+replies: 100    bytes: 4384000
+</pre>
+<br/>
