68959f6b1522d66f541e40e23be434504767d1fc
igouy-guest <igouy-guest>
2005-08-24 17:38:06 +0000

    *** empty log message ***

--- website/about/discovery-about.tpl.php
+++ website/about/discovery-about.tpl.php
@@ -13,15 +13,15 @@
 
 <p>Correct output for this 100KB <a href="iofile.php?test=<?=$SelectedTest;?>&amp;lang=<?=$SelectedLang;?>&amp;sort=<?=$Sort;?>&amp;file=input">input file</a> (generated with the fasta program N = 10000), is</p>
 <pre>
-(agggtaaa)|(tttaccct) 0
-([cgt]gggtaaa)|(tttaccc[acg]) 3
-(a[act]ggtaaa)|(tttacc[agt]t) 8
-(ag[act]gtaaa)|(tttac[agt]ct) 8
-(agg[act]taaa)|(ttta[agt]cct) 7
-(aggg[acg]aaa)|(ttt[cgt]ccct) 3
-(agggt[cgt]aa)|(tt[acg]accct) 4
-(agggta[cgt]a)|(t[acg]taccct) 3
-(agggtaa[cgt])|([acg]ttaccct) 5
+agggtaaa|(tttaccct) 0
+[cgt]gggtaaa|tttaccc[acg] 3
+a[act]ggtaaa|tttacc[agt]t 8
+ag[act]gtaaa|tttac[agt]ct 8
+agg[act]taaa|ttta[agt]cct 7
+aggg[acg]aaa|ttt[cgt]ccct 3
+agggt[cgt]aa|tt[acg]accct 4
+agggta[cgt]a|t[acg]taccct 3
+agggtaa[cgt]|[acg]ttaccct 5
 </pre>
 <br />
 
