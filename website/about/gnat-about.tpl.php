411fde646f7a0e93cd55e3bc978f87b84d856767
igouy-guest <igouy-guest>
2005-09-06 17:56:49 +0000

    *** empty log message ***

--- website/about/gnat-about.tpl.php
+++ website/about/gnat-about.tpl.php
@@ -1,3 +1,4 @@
+<p>wikibook <a href="http://en.wikibooks.org/wiki/Ada_Programming">Ada Programming</a></p>
 <?=$Version;?>
 <p>Home Page: <a href="http://libre.act-europe.fr/GNAT/">http://libre.act-europe.fr/GNAT/</a></p>
 <p>Download: <a href="ftp://gcc.gnu.org/pub/gcc/releases">ftp://gcc.gnu.org/pub/gcc/releases</a></p>
