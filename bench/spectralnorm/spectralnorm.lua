b086331ae95d6b3051f6d6765e8d2b9cafad1a94
igouy-guest <igouy-guest>
2005-09-08 21:37:17 +0000

    *** empty log message ***

--- bench/spectralnorm/spectralnorm.lua
+++ bench/spectralnorm/spectralnorm.lua
@@ -1,41 +1,46 @@
 -- The Great Computer Language Shootout
 -- http://shootout.alioth.debian.org/
 --
--- contributed by Isaac Gouy
+-- contributed by Isaac Gouy, tuned by Mike Pall
 
-
-function A(i,j) return 1.0/((i+j)*(i+j+1) /2 +i+1) end
-
-function Av(n,v) 
-   local Av = {}
-   for i = 0,n-1 do 
-      Av[i] = 0
-      for j = 0,n-1 do Av[i] = Av[i] + (A(i,j)*v[j]) end     
-   end     
-   return Av
+local function A(i, j)
+  local ij = i+j
+  return 1.0 / (ij * (ij+1) * 0.5 + i+1)
 end
 
-function Atv(n,v) 
-   local Atv = {}
-   for i = 0,n-1 do 
-      Atv[i] = 0
-      for j = 0,n-1 do Atv[i] = Atv[i] + A(j,i)*v[j] end
-   end 
-   return Atv
+local function Av(n, x, y)
+  for i = 0,n-1 do
+    local a = 0
+    for j = 0,n-1 do a = a + A(i,j) * x[j] end
+    y[i] = a
+  end
 end
 
-function AtAv(n,v) 
-   return Atv(n, Av(n,v)) 
+local function Atv(n, x, y)
+  for i = 0,n-1 do
+    local a = 0
+    for j = 0,n-1 do a = a + A(j,i) * x[j] end
+    y[i] = a
+  end
+end
+
+local function AtAv(n, x, y, t)
+  Av(n, x, t)
+  Atv(n, t, y)
 end
 
 
 local n = tonumber(arg and arg[1]) or 100
-local u,v = {},{}
+local u, v, t = {}, {}, {}
 for i = 0,n-1 do u[i] = 1 end
 
-for i = 1,10 do v = AtAv(n,u); u = AtAv(n,v) end
+for i = 1,10 do AtAv(n, u, v, t) AtAv(n, v, u, t) end
 
-local vBv,vv = 0,0
-for i = 0,n-1 do vBv = vBv + u[i]*v[i]; vv = vv + v[i]*v[i] end
+local vBv, vv = 0, 0
+for i = 0,n-1 do
+  local ui, vi = u[i], v[i]
+  vBv = vBv + ui*vi
+  vv = vv + vi*vi
+end
 
-io.write( string.format("%0.9f", math.sqrt(vBv/vv) ), "\n")
+io.write(string.format("%0.9f\n", math.sqrt(vBv / vv)))
