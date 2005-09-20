a4516118f8044e0a2a7b78056d68bf5d8473bd86
igouy-guest <igouy-guest>
2005-09-20 17:12:05 +0000

    reset - nestedloop, random, spellcheck, sumcol

--- bench/sumcol/sumcol.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# $Id: sumcol.bash,v 1.2 2005-05-13 16:24:19 igouy-guest Exp $
-# http://www.bagley.org/~doug/shootout/ 
-
-sum=0;while read num ; do sum=$[ $sum + $num ]; done ; echo $sum
-
