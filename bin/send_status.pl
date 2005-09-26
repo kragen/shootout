9a3ded89cccbe6a780c7fbee5613f4857c3c8158
bfulgham <bfulgham>
2005-09-26 02:21:35 +0000

    Rerun of benchmarks.

---
+++ bin/send_status.pl
@@ -0,0 +1,66 @@
+#!/usr/bin/perl -w
+#
+# Simple program to send an e-mail status message to everyone in
+# the etc/mail.list file.
+
+use Mail::Sendmail;
+
+sub get_mail_list() {
+    my @mailTo;
+
+    if (open MAILLIST, "<./etc/mail.list") {
+        while (<MAILLIST>) {
+	    chomp();
+	    push @mailTo, $_;
+        }
+    }
+
+    return @mailTo;
+}
+
+sub send_a_mail($) {
+    my $recipient = shift;
+
+    my ($sec,$min,$hour,$mday,$month,$centm) = gmtime();
+
+    my $year = $centm + 1900;
+
+    my $date = "$month-$mday-$year at $hour:$min:$sec GMT";
+
+    %mail = ( 
+              from      => 'bfulgham@debian.org',
+              to	=> "$recipient",
+	      subject   => 'Shootout Build Status',
+	    );
+
+    $mail{body} = 
+    "Greetings!\n" .
+    "This is an automated message from the Shootout build machine.\n" .
+    "\n\n" .
+    "Shootout Build Status:\n" .
+    "======================\n" .
+    "A build of the benchmark was successfully completed on $date\n" .
+    "\n\n" .
+    "Sincerely,\n\n" .
+    "The Shootout Autobuilder";
+
+    if (sendmail %mail) {
+        print "Mail send to $recipient SUCCEEDED.\n";
+    } else {
+        print "Error sending mail: $Mail::Sendmail::error \n"
+    }
+}
+
+sub main() {
+    # Set desired mail server
+    unshift @{$Mail::Sendmail::mailcfg{'smtp'}} , 'localhost';
+
+    # Read list of mail recipients
+    my @mailTo = get_mail_list();
+
+    foreach my $d (@mailTo) {
+        send_a_mail( $d );
+    }
+}
+
+main()
