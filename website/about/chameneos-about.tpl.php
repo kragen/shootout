af1ac0b337ecf2361da825de2355972fc3d8658f
igouy-guest <igouy-guest>
2005-10-04 21:21:58 +0000

    *** empty log message ***

--- website/about/chameneos-about.tpl.php
+++ website/about/chameneos-about.tpl.php
@@ -1,6 +1,13 @@
-
+<p>Each program should</p>
+<ul>
+<li>create four differently coloured (blue, red, yellow, blue) concurrent chameneos creatures</li>
+<li>each creature will repeatedly go to the meeting place and meet, or wait to meet, another chameneos</li>
+<li>each creature will change colour to complement the colour of the chameneos that they met</li>
+<li>after N total meetings have taken place, any creature entering the meeting place will take on a faded colour, report the number of creatures it has met, and end</li>
+<li>write the sum of reported creatures met</li>
+</ul>
 
 <p>Correct output N = 100 is:</p> 
 <pre>200</pre><br />
 
-<p>The chameneos benchmark is a simplistic adaptation of <a href="http://cedric.cnam.fr/PUBLIS/RC474.pdf">"Chameneos, a Concurrency Game for Java, Ada and Others"</a> 100KB pdf</p>
+<p>The chameneos benchmark is a simplistic adaptation of <a href="http://cedric.cnam.fr/PUBLIS/RC474.pdf">"Chameneos, a Concurrency Game for Java, Ada and Others"</a> 100KB pdf (which includes example implementations in Java, Ada and C).</p>
