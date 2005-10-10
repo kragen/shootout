b1a61f27e45ff7cea22d36c9c4dc6cee0ac0ad1b
igouy-guest <igouy-guest>
2005-10-10 17:52:28 +0000

    *** empty log message ***

--- website/about/sbcl-about.tpl.php
+++ website/about/sbcl-about.tpl.php
@@ -1,4 +1,1 @@
-<p><a href="http://www.dreamsongs.com/NewFiles/clos-cacm.pdf">"CLOS: Integrating Object-Oriented and Functional Programming"</a>,  Communications of the ACM, vol. 34, pp. 28-38, 1991. (143KB pdf)</p>
-<?=$Version;?>
-<p>Home Page: <a href="http://sbcl.sourceforge.net/">http://sbcl.sourceforge.net/</a></p>
-<p>Download: <a href="http://sbcl.sourceforge.net/">http://sbcl.sourceforge.net/</a></p>
+<p>online book <a href="http://www.gigamonkeys.com/book/">Practical Common Lisp</a></p><?=$Version;?><p>Home Page: <a href="http://sbcl.sourceforge.net/">http://sbcl.sourceforge.net/</a></p><p>Download: <a href="http://sbcl.sourceforge.net/">http://sbcl.sourceforge.net/</a></p>
