e6e44bf3175274fad95168c47aaf1f776fd76097
igouy-guest <igouy-guest>
2005-11-02 20:20:49 +0000

    GP4 updates, Scala Io -Xint -client

---
+++ website/about/scala-about.tpl.php
@@ -0,0 +1,3 @@
+<?=$Version;?>
+<p>Home Page: <a href="http://scala.epfl.ch/">http://scala.epfl.ch/</a></p>
+<p>Download: <a href="http://scala.epfl.ch/downloads/index.html">http://scala.epfl.ch/downloads/index.html</a></p>
---
+++ website/websites/gp4/version/javaclient-version.php
@@ -0,0 +1,1 @@
+<p>Java HotSpot(TM) Client VM (build 1.5.0_05-b05, mixed mode)</p>
