97b7f24d688eb52840fca995710c85067182c770
igouy-guest <igouy-guest>
2005-11-14 23:03:29 +0000

    *** empty log message ***

---
+++ website/about/swiprolog-about.tpl.php
@@ -0,0 +1,3 @@
+<?=$Version;?>
+<p>Home Page: <a href="http://www.swi-prolog.org/">http://www.swi-prolog.org/</a></p>
+<p>Download: <a href="http://www.swi-prolog.org/dl-stable.html">http://www.swi-prolog.org/dl-stable.html</a></p>
