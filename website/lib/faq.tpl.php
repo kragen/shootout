a531bcd9b41e177b3da8f778a9316c117face879
igouy-guest <igouy-guest>
2005-11-17 06:39:45 +0000

    *** empty log message ***

--- website/lib/faq.tpl.php
+++ website/lib/faq.tpl.php
@@ -281,7 +281,7 @@ The <span><a class="arevGP4" title="Gentoo Linux on Intel Pentium 4" href="<?=GP
 
 <tr class="b"><td><a class="ab" href="#downsource" name="downsource">Where can I download the program sources and build scripts?</a></td></tr>
 <tr><td><p>You can <a href="http://alioth.debian.org/scm/?group_id=30402"  title="Browse the Great Computer Language Shootout CVS tree">browse the CVS tree</a>.</p>
-<p>Build dependencies include - <a href="http://packages.debian.org/stable/admin/gtop">libgtop</a>, <a href="http://search.cpan.org/~stas/GTop/">GTop</a>, libXau, and <a href="http://packages.debian.org/stable/interpreters/libbsd-resource-perl">BSD::Resource</a> </p>
+<p>Build dependencies include - <a href="http://packages.debian.org/stable/libs/libgtop1">libgtop</a>, <a href="http://search.cpan.org/~mjh/GTop-0.16/GTop.pod">GTop</a>, libXau, and <a href="http://packages.debian.org/stable/perl/libbsd-resource-perl">BSD::Resource</a> </p>
 </td></tr>
 
 <tr class="b"><td><a class="ab" href="#findmore" name="findmore">Where can I find out more?</a></td></tr>
