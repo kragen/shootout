4e20d544d3d7e87481fd782dadf58c24f89f5957
igouy-guest <igouy-guest>
2005-11-28 04:19:32 +0000

    *** empty log message ***

---
+++ website/about/vw-about.tpl.php
@@ -0,0 +1,4 @@
+<p><a href="http://users.ipa.net/~dwighth/smalltalk/byte_aug81/design_principles_behind_smalltalk.html">"Design Principles Behind Smalltalk" by Daniel Ingalls</a></p>
+<?=$Version;?>
+<p>Home Page: <a href="http://smalltalk.cincom.com/prodinformation/index.ssp?content=vwfactsheet">Cincom Smalltalk™ VisualWorks® Environment Data Sheet</a></p>
+<p>Download: <a href="http://smalltalk.cincom.com/downloads/index.ssp?content=visualworks">VisualWorks® Non-Commercial</a></p>
