c32545240a3809ae3502652cd83136bb9173d550
igouy-guest <igouy-guest>
2005-12-03 08:01:39 +0000

    *** empty log message ***

---
+++ bench/hello/hello.vb
@@ -0,0 +1,11 @@
+' The Computer Language Shootout
+' http://shootout.alioth.debian.org/
+' contributed by Isaac Gouy
+
+Imports System
+
+Public Module hello
+   Sub Main() 
+      Console.WriteLine("hello world") 
+   End Sub
+End Module
