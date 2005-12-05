5b3ce3b354e13bc78bb846d2e4e58b59b695d16e
igouy-guest <igouy-guest>
2005-12-05 19:37:33 +0000

    *** empty log message ***

--- bench/harmonic/harmonic.vb
+++ bench/harmonic/harmonic.vb
@@ -5,12 +5,12 @@
 Imports System
 
 Public Module Harmonic
-   Sub Main(ByVal CmdArgs() As String)
-      Dim N As Integer = Int32.Parse(CmdArgs(0)) 
-      Dim PartialSum As Double = 0.0
-      For I As Integer = 1 To N 
-         PartialSum += 1.0/I
+   Sub Main(ByVal cmdArgs() As String)
+      Dim n As Integer = Int32.Parse(cmdArgs(0)) 
+      Dim partialSum As Double = 0.0
+      For i As Integer = 1 To n 
+         partialSum += 1.0/i
       Next
-      Console.WriteLine("{0:f9}", PartialSum)
+      Console.WriteLine("{0:f9}", partialSum)
    End Sub
 End Module
