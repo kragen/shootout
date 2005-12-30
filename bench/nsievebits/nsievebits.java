eff0e99a0f79b3af30a03e29a901cee8c4039a68
igouy-guest <igouy-guest>
2005-12-30 01:50:47 +0000

    *** empty log message ***

--- bench/nsievebits/nsievebits.java
+++ bench/nsievebits/nsievebits.java
@@ -22,6 +22,20 @@ public class nsievebits
       return count;
    }
 
+   public static String padNumber(int number, int fieldLen)
+   {
+      StringBuffer sb = new StringBuffer();
+      String bareNumber = "" + number;
+      int numSpaces = fieldLen - bareNumber.length();
+
+      for (int i = 0; i < numSpaces; i++)
+         sb.append(" ");
+
+      sb.append(bareNumber);
+
+      return sb.toString();
+   }
+
    public static void main(String[] args)
    {
       int n = 2;
@@ -32,12 +46,12 @@ public class nsievebits
 
       int m = (1 << n) * 10000;
       BitSet bits = new BitSet(m+1);
-      System.out.println("Primes up to " + m + " " + nsieve(m, bits));
+      System.out.println("Primes up to " + padNumber(m, 8) + " " + padNumber(nsieve(m,bits), 8));
 
       m = (1 << n-1) * 10000;
-      System.out.println("Primes up to " + m + " " + nsieve(m, bits));
+      System.out.println("Primes up to " + padNumber(m, 8) + " " + padNumber(nsieve(m,bits), 8));
 
       m = (1 << n-2) * 10000;
-      System.out.println("Primes up to " + m + " " + nsieve(m, bits));
+      System.out.println("Primes up to " + padNumber(m, 8) + " " + padNumber(nsieve(m,bits), 8));
    }
 }
