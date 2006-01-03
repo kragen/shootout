7a48c563bf3c8a4361acbe36092a50ff2f1cbcf4
igouy-guest <igouy-guest>
2006-01-03 03:48:32 +0000

    *** empty log message ***

---
+++ bench/sumcol/sumcol.scala
@@ -0,0 +1,24 @@
+/*
+ * The Computer Language Shootout
+ * http://shootout.alioth.debian.org/
+ *
+ * contributed by Andrei Formiga
+ */
+
+/* imperative version */
+object sumcol
+{
+  def main(args: Array[String]) = 
+  {
+    var sum = 0;
+    var line = Console.readLine;
+
+    while (line != null)
+    {
+      sum = sum + Integer.parseInt(line);
+      line = Console.readLine;
+    }
+
+    Console.println(sum.toString());
+  }
+}
---
+++ bench/sumcol/sumcol.scala-2.scala
@@ -0,0 +1,21 @@
+/*
+ * The Computer Language Shootout
+ * http://shootout.alioth.debian.org/
+ *
+ * contributed by Andrei Formiga
+ */
+
+/* functional version */
+object sumcol
+{
+  def sumFile(res: int): int = 
+  {
+    val line = Console.readLine;
+    if (line == null) res else sumFile(res + Integer.parseInt(line));
+  }
+
+  def main(args: Array[String]) = 
+  {
+    Console.println(sumFile(0).toString());
+  }
+}
