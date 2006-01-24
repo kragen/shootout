adc318b4cb7c32dad6b98b6afdf796fde1785bcf
igouy-guest <igouy-guest>
2006-01-24 00:38:20 +0000

    *** empty log message ***

--- website/about/recursive-about.tpl.php
+++ website/about/recursive-about.tpl.php
@@ -1,10 +1,6 @@
-<p>"(alpha)" benchmarks are just experiments - we'll probably change them or decide not to use them at all.</p>
-<p><strong>Please don't waste your valuable time implementing alpha benchmarks</strong>.</p>
+<p><strong>diff</strong> program output N = 3 with this <a href="iofile.php?test=<?=$SelectedTest;?>&amp;lang=<?=$SelectedLang;?>&amp;file=output">output file</a> to check your program is correct before contributing.</p>
 
-<p><strong>diff</strong> program output N = 1 with this <a href="iofile.php?test=<?=$SelectedTest;?>&amp;lang=<?=$SelectedLang;?>&amp;file=output">output file</a> to check your program is correct before contributing.</p>
-
-<p>Each program should use the same na&#239;ve recursive-algorithms to calculate 3 simple numeric functions: 
-ackermann, fibonnaci and tak. The fibonnaci and tak implementations should either provide separate functions - one for integer calculation and one for double calculation - or provide a function that uses integer calculation with integer parameters and double calculation with double parameters.</p>
+<p>Each program should use the same na&#239;ve recursive-algorithms to calculate 3 simple numeric functions: ackermann, fibonnaci and tak.</p>
 <pre>
 Ack(x,y)
   x = 0     = y+1
@@ -12,15 +8,17 @@ Ack(x,y)
   otherwise = Ack(x-1, Ack(x,y-1))
 
 Fib(n)
-  n = 0     = 1
-  n = 1     = 1
+  n < 2     = 1
   otherwise = Fib(n-2) + Fib(n-1)
 
 Tak(x,y,z)
-  y < x   = Tak(Tak(x-1.0,y,z),Tak(y-1.0,z,x),Tak(z-1.0,x,y))
-  y >= x  = z
+  y < x     = Tak(Tak(x-1.0,y,z),Tak(y-1.0,z,x),Tak(z-1.0,x,y))
+  otherwise = z
 </pre>
 <br />
+
+<p>For this benchmark, the fibonnaci and tak implementations should either provide separate functions - one for integer calculation and one for double calculation - or provide a function that uses integer calculation with integer parameters and double calculation with double parameters.</p><br />
+
 
 <p>The Ackermann benchmark is described in <a href="http://cm.bell-labs.com/cm/cs/who/bwk/interps/pap.html">Timing Trials, or, the Trials of Timing: Experiments with Scripting and User-Interface Languages</a>.</p>
 <p>For more information see Eric W. Weisstein, "Ackermann Function." From <a href="http://mathworld.wolfram.com"><i>MathWorld</i></a>--A Wolfram Web Resource.<br/><a href="http://mathworld.wolfram.com/AckermannFunction.html">http://mathworld.wolfram.com/AckermannFunction.html</a></p>
