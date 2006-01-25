33a61bd1c062c9c1bf5dd726effe446d9fd7eb5c
igouy-guest <igouy-guest>
2006-01-25 07:21:08 +0000

    *** empty log message ***

--- website/about/partialsums-about.tpl.php
+++ website/about/partialsums-about.tpl.php
@@ -1,1 +1,5 @@
+<p><strong>diff</strong> program output N = 25000 with this <a href="iofile.php?test=<?=$SelectedTest;?>&amp;lang=<?=$SelectedLang;?>&amp;file=output">output file</a> to check your program is correct before contributing.</p>
 
+<p>Each program should use the same na&#239;ve iterative double-precision algorithms to calculate partial-sums of the series.</p>
+
+<p>For more information see "General Series." From <a href="http://mathworld.wolfram.com"><i>MathWorld</i></a>--A Wolfram Web Resource.<br/><a href="http://mathworld.wolfram.com/topics/GeneralSeries.html">http://mathworld.wolfram.com/topics/GeneralSeries.html</a></p>
