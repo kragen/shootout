99a2463630ee1c002c1350ebdaef46e4b6723b7a
igouy-guest <igouy-guest>
2006-01-29 22:49:59 +0000

    *** empty log message ***

--- website/about/partialsums-about.tpl.php
+++ website/about/partialsums-about.tpl.php
@@ -1,5 +1,18 @@
 <p><strong>diff</strong> program output N = 25000 with this <a href="iofile.php?test=<?=$SelectedTest;?>&amp;lang=<?=$SelectedLang;?>&amp;file=output">output file</a> to check your program is correct before contributing.</p>
 
-<p>Each program should use the same na&#239;ve iterative double-precision algorithms to calculate partial-sums of the series.</p>
+<p>(Programs may use a single-loop or several-loops; programs may cache recomputed values in local variables.)
+
+<p>Each program should use the same na&#239;ve iterative double-precision algorithms to calculate partial-sums of the series:</p>
+<div>
+<p>&#8721; (&#178;&#8260;<sub>3</sub>)&#8319; <sub>n=0, &#8230;</sub></p>
+<p>&#8721; n<sup>&#8722;0.5</sup> <sub>n=1, &#8230;</sub></p>
+<p>&#8721; &#185;&#8260;<sub>n(n+1)</sub></p>
+<p>&#8721; &#185;&#8260;<sub>n&#179;(sin n)&#178;</sub></p>
+<p>&#8721; &#185;&#8260;<sub>n&#179;(cos n)&#178;</sub></p>
+<p>&#8721; &#185;&#8260;<sub>n</sub></p>
+<p>&#8721; &#185;&#8260;<sub>n&#178;</sub></p>
+<p>&#8721; <sup>&#8722;1&#8319;</sup>&#8260;<sub>n</sub></p>
+<p>&#8721; <sup>&#8722;1&#8319;</sup>&#8260;<sub>2n &#8722;1</sub></p>
+</div>
 
 <p>For more information see "General Series." From <a href="http://mathworld.wolfram.com"><i>MathWorld</i></a>--A Wolfram Web Resource.<br/><a href="http://mathworld.wolfram.com/topics/GeneralSeries.html">http://mathworld.wolfram.com/topics/GeneralSeries.html</a></p>
