345cdabb982b72c4dfd874a0282b8b8a03870047
igouy-guest <igouy-guest>
2006-01-29 22:56:39 +0000

    *** empty log message ***

--- website/about/partialsums-about.tpl.php
+++ website/about/partialsums-about.tpl.php
@@ -4,15 +4,15 @@
 
 <p>Each program should use the same na&#239;ve iterative double-precision algorithms to calculate partial-sums of the series:</p>
 <div>
-<p>&#8721; (&#178;&#8260;<sub>3</sub>)&#8319; <sub>n=0, &#8230;</sub></p>
-<p>&#8721; n<sup>&#8722;0.5</sup> <sub>n=1, &#8230;</sub></p>
-<p>&#8721; &#185;&#8260;<sub>n(n+1)</sub></p>
-<p>&#8721; &#185;&#8260;<sub>n&#179;(sin n)&#178;</sub></p>
-<p>&#8721; &#185;&#8260;<sub>n&#179;(cos n)&#178;</sub></p>
-<p>&#8721; &#185;&#8260;<sub>n</sub></p>
-<p>&#8721; &#185;&#8260;<sub>n&#178;</sub></p>
-<p>&#8721; <sup>&#8722;1&#8319;</sup>&#8260;<sub>n</sub></p>
-<p>&#8721; <sup>&#8722;1&#8319;</sup>&#8260;<sub>2n &#8722;1</sub></p>
+<p>&#8721; (&#178;&#8260;<sub>3</sub>)<sup>k</sup> <sub>k=0, &#8230;</sub></p>
+<p>&#8721; k<sup>&#8722;0.5</sup> <sub>k=1, &#8230;</sub></p>
+<p>&#8721; &#185;&#8260;<sub>k(k+1)</sub></p>
+<p>&#8721; &#185;&#8260;<sub>k&#179;(sin k)&#178;</sub></p>
+<p>&#8721; &#185;&#8260;<sub>k&#179;(cos k)&#178;</sub></p>
+<p>&#8721; &#185;&#8260;<sub>k</sub></p>
+<p>&#8721; &#185;&#8260;<sub>k&#178;</sub></p>
+<p>&#8721; <sup>&#8722;1<sup>k+1</sup></sup>&#8260;<sub>k</sub></p>
+<p>&#8721; <sup>&#8722;1<sup>k+1</sup></sup>&#8260;<sub>2k &#8722;1</sub></p>
 </div>
 
 <p>For more information see "General Series." From <a href="http://mathworld.wolfram.com"><i>MathWorld</i></a>--A Wolfram Web Resource.<br/><a href="http://mathworld.wolfram.com/topics/GeneralSeries.html">http://mathworld.wolfram.com/topics/GeneralSeries.html</a></p>
