ee6d6e47363168c41ca75235c927e0d92771f75e
igouy-guest <igouy-guest>
2006-02-02 21:57:57 +0000

    *** empty log message ***

--- bench/partialsums/partialsums.scala
+++ bench/partialsums/partialsums.scala
@@ -8,10 +8,11 @@
 object partialsums
 {
   val twodiv3 = 2.0 / 3.0;
+  var n: double = _;
 
   def calculate(s1:double, s2:double, s3:double, s4:double, s5:double, s6:double,
-		s7:double, s8:double, s9:double, sign: double, k: int): unit = {
-    if (k == 0) {
+		s7:double, s8:double, s9:double, sign: double, kd: double): unit = {
+    if (kd > n) {
       Console.printf("{0,number,0.000000000}\t(2/3)^k\n")(s1);
       Console.printf("{0,number,0.000000000}\tk^-0.5\n")(s2);
       Console.printf("{0,number,0.000000000}\t1/k(k+1)\n")(s3);
@@ -23,19 +24,19 @@ object partialsums
       Console.printf("{0,number,0.000000000}\tGregory\n")(s9);
     }
     else {
-      val k2 = k * k.toDouble;
-      val k3 = k2 * k;
-      val sin = Math.sin(k);
-      val cos = Math.cos(k);
-      calculate(s1 + Math.pow(twodiv3, k - 1.0), s2 + Math.pow(k, -0.5),
-		s3 + 1.0 / (k * (k + 1.0)), s4 + 1.0 / (k3 * sin*sin),
-		s5 + 1.0 / (k3 * cos*cos), s6 + 1.0 / k, s7 + 1.0 / k2,
-		s8 + sign / k, s9 + sign / (2 * k - 1), -sign, k - 1);
+      val k2 = kd * kd;
+      val k3 = k2 * kd;
+      val sin = Math.sin(kd);
+      val cos = Math.cos(kd);
+      calculate(s1 + Math.pow(twodiv3, kd - 1.0), s2 + Math.pow(kd, -0.5),
+		s3 + 1.0 / (kd * (kd + 1.0)), s4 + 1.0 / (k3 * sin*sin),
+		s5 + 1.0 / (k3 * cos*cos), s6 + 1.0 / kd, s7 + 1.0 / k2,
+		s8 + sign / kd, s9 + sign / (2.0 * kd - 1.0), -sign, kd + 1.0);
     }
   }
 
   def main(args: Array[String]) = {
-    val n = Integer.parseInt(args(0));
-    calculate(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0, n);
+    n = Integer.parseInt(args(0)).toDouble;
+    calculate(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0, 1.0);
   }
 }
