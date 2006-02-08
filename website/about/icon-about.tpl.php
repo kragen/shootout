fb7af9e791f7dfa965016a1bae9c20a482873123
igouy-guest <igouy-guest>
2006-02-08 21:48:11 +0000

    *** empty log message ***

--- website/about/icon-about.tpl.php
+++ website/about/icon-about.tpl.php
@@ -1,3 +1,4 @@
+<p>online book <a href="http://www.tools-of-computing.com/tc/CS/iconprog.pdf">"Icon Programming Language Handbook"</a> (pdf) by Thomas W Christopher</p>
 <?=$Version;?>
 <p>Home Page: <a href="http://www.cs.arizona.edu/icon/">http://www.cs.arizona.edu/icon/</a></p>
-<p>Download: <a href="http://www.cs.arizona.edu/icon/v93.htm">http://www.cs.arizona.edu/icon/v93.htm</a></p>
+<p>Download: <a href="http://www.cs.arizona.edu/icon/v943/">http://www.cs.arizona.edu/icon/v943/</a></p>
