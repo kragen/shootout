a8dc64ef6ea3676eba6573c938eb26d08cdb289a
igouy-guest <igouy-guest>
2006-02-25 07:56:52 +0000

    *** empty log message ***

---
+++ website/about/cint-about.tpl.php
@@ -0,0 +1,3 @@
+<?=$Version;?>
+<p>Home Page: <a href="http://root.cern.ch/root/Cint.html">http://root.cern.ch/root/Cint.html</a></p>
+<p>Download: <a href="ftp://root.cern.ch/root/cint5.16.tar.gz">ftp://root.cern.ch/root/cint5.16.tar.gz</a></p>
