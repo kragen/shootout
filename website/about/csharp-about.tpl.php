283e175dc13fff540bce50a44de98873e6d984d9
igouy-guest <igouy-guest>
2006-02-28 19:47:55 +0000

    *** empty log message ***

--- website/about/csharp-about.tpl.php
+++ website/about/csharp-about.tpl.php
@@ -4,5 +4,5 @@
 
 <p><a href="http://www.ecma-international.org/publications/standards/Ecma-335.htm"> Common Language Infrastructure (CLI) ECMA-335</a></p>
 
-<p>Home Page: <a href="http://www.go-mono.com/">http://www.go-mono.com/</a></p>
-<p>Download: <a href="http://www.go-mono.com/">http://www.go-mono.com/</a></p>
+<p>Home Page: <a href="http://www.mono-project.com/">http://www.mono-project.com/</a></p>
+<p>Download: <a href="http://www.mono-project.com/Downloads">http://www.mono-project.com/Downloads</a></p>
--- website/websites/gp4/version/csharp-version.php
+++ website/websites/gp4/version/csharp-version.php
@@ -1,1 +1,1 @@
-<p>Mono JIT compiler version 1.1.9.2</p>
+<p>Mono JIT compiler version 1.1.12.1</p>
