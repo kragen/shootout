b1748fc5b705ea18d9f79ffc8d973cff5d80375f
igouy-guest <igouy-guest>
2006-03-17 21:24:40 +0000

    *** empty log message ***

---
+++ website/about/luajit-about.tpl.php
@@ -0,0 +1,3 @@
+<?=$Version;?>
+<p>Home Page: <a href="http://luajit.luaforge.net/">http://luajit.luaforge.net/</a></p>
+<p>Download: <a href="http://luaforge.net/frs/download.php/1482/LuaJIT-1.1.0.tar.gz">http://luaforge.net/frs/download.php/1482/LuaJIT-1.1.0.tar.gz</a></p>
