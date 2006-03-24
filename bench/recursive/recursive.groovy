b20e62ca207e130bbefb4d1fbaf260c24b39c365
ajborla-guest <ajborla-guest>
2006-03-24 06:07:52 +0000

    *** empty log message ***

--- bench/recursive/recursive.groovy
+++ bench/recursive/recursive.groovy
@@ -2,28 +2,21 @@
 // The Great Computer Language Shootout
 // http://shootout.alioth.debian.org/
 // 
-// Use JRE 1.4 features only [JRE 1.5 'printf' would have helped better
-// streamline code]
-//
 // Contributed by Anthony Borla
 // ---------------------------------------------------------------------
 
-import java.text.DecimalFormat
-
 def main()
 {
-  n = Integer.parseInt(args[0]) ; fmt = new DecimalFormat(".#")
+  n = Integer.parseInt(args[0])
 
-  result = ack(3, n) ; println "Ack(3,${n}): ${result}" 
+  printf("Ack(3,%d): %d\n", n, ack(3, n))
+  printf("Fib(%.1f): %.1f\n", 27.0D + n, fib(27.0D + n))
 
-  arg = fmt.format(27.0 + n) ; result = fmt.format(fib(27.0 + n))
-  println "Fib(${arg}): ${result}"
+  n -= 1
+  printf("Tak(%d,%d,%d): %d\n", n * 3, n * 2, n, tak(n * 3, n * 2, n))
 
-  n -= 1 ; arg1 = n * 3 ; arg2 = n * 2 ; result = tak(n * 3, n * 2, n)
-  println "Tak(${arg1},${arg2},${n}): ${result}"
-
-  result = fib(3) ; println "Fib(3): ${result}" 
-  result = fmt.format(tak(3.0, 2.0, 1.0)) ; println "Tak(3.0,2.0,1.0): ${result}"
+  printf("Fib(3): %d\n", fib(3)) 
+  printf("Tak(3.0,2.0,1.0): %.1f\n", tak(3.0D, 2.0D, 1.0D))
 }
 
 // --------------------------------
@@ -37,17 +30,29 @@ def ack(x, y)
 
 // --------------
 
-def fib(n)
+def fib(int n)
 {
-  if (n < 2) return 1
-  return fib(n - 2) + fib(n - 1)
+  if (n < 2I) return 1I
+  return fib(n - 2I) + fib(n - 1I)
+}
+
+def fib(double n)
+{
+  if (n < 2.0D) return 1.0D
+  return fib(n - 2.0D) + fib(n - 1.0D)
 }
 
 // --------------
 
-def tak(x, y, z)
+def tak(int x, int y, int z)
 {
-  if (y < x) return tak(tak(x - 1, y, z), tak(y - 1, z, x), tak(z - 1, x, y))
+  if (y < x) return tak(tak(x - 1I, y, z), tak(y - 1I, z, x), tak(z - 1I, x, y))
+  return z
+}
+
+def tak(double x, double y, double z)
+{
+  if (y < x) return tak(tak(x - 1.0D, y, z), tak(y - 1.0D, z, x), tak(z - 1.0D, x, y))
   return z
 }
 
