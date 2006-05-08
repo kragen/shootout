69f07fe8968803f053c115031b4c6705bbed380b
ajborla-guest <ajborla-guest>
2006-05-08 09:11:50 +0000

    *** empty log message ***

---
+++ bench/nsieve/nsieve.scala
@@ -0,0 +1,61 @@
+/* ------------------------------------------------------------------ */
+/* The Great Computer Language Shootout                               */
+/* http://shootout.alioth.debian.org/                                 */
+/*                                                                    */
+/* Adapted from Pike version by Adam Montague                         */
+/*                                                                    */
+/* Contributed by Anthony Borla                                       */
+/* ------------------------------------------------------------------ */
+
+object nsieve
+{
+  def main(args: Array[String]): unit =
+  {
+    val N = Integer.parseInt(args(0)); var M: int = 0;
+
+    M = (1 << N) * 10000;
+    Console.printf("Primes up to {0} {1}\n")(padr(M, 8, ' '), padr(nsieve(M), 8, ' '));
+
+    M = (1 << N - 1) * 10000;
+    Console.printf("Primes up to {0} {1}\n")(padr(M, 8, ' '), padr(nsieve(M), 8, ' '));
+
+    M = (1 << N - 2) * 10000;
+    Console.printf("Primes up to {0} {1}\n")(padr(M, 8, ' '), padr(nsieve(M), 8, ' '));
+  } 
+
+  def nsieve(m: int): int =
+  {
+    val sieve: Array[byte] = new Array[byte](m);
+
+    var i:int = 0; while (i < m) { sieve(i) = 1; i = i + 1; }
+
+    var count: int = 0; i = 2; 
+
+    while (i < m)
+    {
+      if (sieve(i) != 0)
+      {
+        var j: int = i + i; while (j < m) { sieve(j) = 0; j = j + i; }
+        count = count + 1;
+      }
+
+      i = i + 1;
+    }
+
+    return count;
+  }
+
+  def padr(v: Any, padlen: int, padchar: char): String =
+  {
+    val s: String = "" + v; val reqlen: int = s.length() - padlen;
+    return if (reqlen < 0) makeString(Math.abs(reqlen), padchar) + s else s; 
+  }
+
+  def makeString(len: int, fillchar: char): String =
+  {
+    val sb: StringBuffer = new StringBuffer(); var i: int = 0;
+    while (i < len) { sb.append(fillchar); i = i + 1; }
+    return sb.toString();
+  }
+}
+
