81ba66a65e2c49ad23a892c6404bad3fd8ffbe8d
igouy-guest <igouy-guest>
2006-07-30 02:43:59 +0000

    *** empty log message ***

--- website/about/programs/regexdna-perl-about.tpl.php
+++
@@ -1,1 +0,0 @@
-<p><strong>NOT ACCEPTED:</strong> Program should use repeated regex match-replace rather than table based substitution</p>
--- website/about/programs/regexdna-php-about.tpl.php
+++
@@ -1,1 +0,0 @@
-<p><strong>NOT ACCEPTED:</strong> Program should use repeated regex match-replace rather than string map</p>
