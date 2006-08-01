3190b90a51cee25fd5e9c355a869246c95460e3e
igouy-guest <igouy-guest>
2006-08-01 15:13:24 +0000

    *** empty log message ***

--- website/about/iron-about.tpl.php
+++ website/about/iron-about.tpl.php
@@ -1,3 +1,3 @@
 <?=$Version;?>
-<p>Home Page: <a href="http://www.gotdotnet.com/workspaces/workspace.aspx?id=ad7acff7-ab1e-4bcb-99c0-57ac5a3a9742">IronPython: Workspace Home</a></p>
-<p>Download: <a href="http://www.gotdotnet.com/workspaces/releases/viewuploads.aspx?id=ad7acff7-ab1e-4bcb-99c0-57ac5a3a9742">IronPython: Releases: Home</a></p>
+<p>Home Page: <a href="http://www.codeplex.com/Wiki/View.aspx?ProjectName=IronPython">CodePlex IronPython</a></p>
+<p>Download: <a href="http://www.codeplex.com/Release/ProjectReleases.aspx?ProjectName=IronPython">CodePlex IronPython Releases</a></p>
--- website/websites/gp4/version/iron-version.php
+++ website/websites/gp4/version/iron-version.php
@@ -1,1 +1,1 @@
-<p>IronPython 1.0.60523 (Beta) on .NET 2.0.50727.42</p>
+<p>IronPython 1.0.60725 on .NET 2.0.50727.42</p>
