fc12c6e586c46f490600f9ec15e865d726b5b30c
igouy-guest <igouy-guest>
2006-10-15 23:51:51 +0000

    *** empty log message ***

--- website/about/scala-about.tpl.php
+++ website/about/scala-about.tpl.php
@@ -1,3 +1,5 @@
 <?=$Version;?>
+<p><a href="http://scala.epfl.ch/docu/files/ScalaTutorial.pdf">A Scala Tutorial for Java programmers</a> pdf</p>
+<p><a href="http://scala.epfl.ch/docu/files/ScalaByExample.pdf"> PDF   	 Scala By Example</a> pdf</p>
 <p>Home Page: <a href="http://scala.epfl.ch/">http://scala.epfl.ch/</a></p>
 <p>Download: <a href="http://scala.epfl.ch/downloads/index.html">http://scala.epfl.ch/downloads/index.html</a></p>
