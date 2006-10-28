0cbfc5b855ddacc42ec96d459401f4ef80b48514
igouy-guest <igouy-guest>
2006-10-28 19:59:47 +0000

    *** empty log message ***

--- website/about/csharpaot-about.tpl.php
+++ website/about/csharpaot-about.tpl.php
@@ -1,8 +1,6 @@
 <?=$Version;?>
 
-<p><a href="http://www.ecma-international.org/publications/standards/Ecma-334.htm">C# Language Specification ECMA-334</a></p>
-
-<p><a href="http://www.ecma-international.org/publications/standards/Ecma-335.htm"> Common Language Infrastructure (CLI) ECMA-335</a></p>
+<p><a href="http://tirania.org/blog/archive/2006/Aug-17.html">Ahead of Time Compilation in Mono</a></p>
 
 <p>Home Page: <a href="http://www.mono-project.com/">http://www.mono-project.com/</a></p>
 <p>Download: <a href="http://www.mono-project.com/Downloads">http://www.mono-project.com/Downloads</a></p>
---
+++ website/websites/gp4/version/csharpaot-version.php
@@ -0,0 +1,1 @@
+<p>Mono JIT compiler version 1.1.18</p>
