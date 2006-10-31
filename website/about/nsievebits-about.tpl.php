7e302a1e23e576b8c93ebb5f0755c2cc6a61e86b
igouy-guest <igouy-guest>
2006-10-31 00:48:57 +0000

    *** empty log message ***

--- website/about/nsievebits-about.tpl.php
+++ website/about/nsievebits-about.tpl.php
@@ -1,7 +1,6 @@
 <p><strong>diff</strong> program output N = 2 with this <a href="iofile.php?test=<?=$SelectedTest;?>&amp;lang=<?=$SelectedLang;?>&amp;file=output">output file</a> to check your program is correct before contributing.
 </p>
 
-
 <p>Each program should count the prime numbers from 2 to M, using the same na&#239;ve Sieve of Eratosthenes algorithm:</p>
 <ul>
   <li>create an array of M <strong>bit flags</strong> which represent either prime or not-prime</li>
@@ -9,7 +8,7 @@
      <ul>
      <li>test the flag value at that index
       <ul>
-      <li>set or clear all the flag values at multiples of that index</li>
+      <li>set or clear the flag values at multiples of that index as necessary</li>
       <li>if necessary keep and increment a separate count</li>
       </ul>
      </li>
@@ -19,4 +18,4 @@
 
 <p>Calculate 3 prime counts, for M = 2<sup>N</sup> &#215; 10000, 2<sup>N-1</sup> &#215; 10000, and 2<sup>N-2</sup> &#215; 10000.</p>
 
-<p>A variation on the <a href="benchmark.php?test=nsieve&amp;lang=all&amp;sort=<?=$Sort;?>">nsieve benchmark</a>.</p>
+<p>A variation on the <a href="benchmark.php?test=nsieve&amp;lang=all&amp;sort=<?=$Sort;?>">nsieve benchmark</a> with 2 differences - bit values are used and it's acceptable to test the bit value and only flip values that need to be changed.</p>
