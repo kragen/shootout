d880a1ff02bbf25353f0f26ff64b98932ea4da34
igouy-guest <igouy-guest>
2006-11-08 03:43:28 +0000

    *** empty log message ***

--- website/about/meteor-about.tpl.php
+++ website/about/meteor-about.tpl.php
@@ -1,5 +1,7 @@
 <p>This is a <strong>contest</strong> - different algorithms may be used.</p>
 
+<p>You are expected to use your program to find all 2098 solutions of the puzzle and <strong>diff that output against our </strong><a href="http://shootout.alioth.debian.org/download/meteor-output.txt"><strong>237KB output file</strong> (Save Target As… Save Link As…)</a> <em>before</em> you contribute your program.</p>
+
 <p>Each program should find and accumulate all the solutions for the Meteor Puzzle, print how many solutions were found, and sort and print all the solutions.</p>
 
 <p>The Meteor Puzzle and 3 Java puzzle solvers are described in <a href="http://www-128.ibm.com/developerworks/java/library/j-javaopt/">"Optimize your Java application's performance"</a>.</p>
