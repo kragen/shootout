4fef631cd8e53ec8ff7714d24a35d8c896951b4e
igouy-guest <igouy-guest>
2006-11-08 04:21:54 +0000

    *** empty log message ***

--- website/about/meteor-about.tpl.php
+++ website/about/meteor-about.tpl.php
@@ -1,6 +1,6 @@
 <p>This is a <strong>contest</strong> - different algorithms may be used.</p>
 
-<p>You are expected to use your program to find all 2098 solutions of the puzzle and <strong>diff that output against our </strong><a href="http://shootout.alioth.debian.org/download/meteor-output.txt"><strong>237KB output file</strong> (Save Target As… Save Link As…)</a> <em>before</em> you contribute your program.</p>
+<p>You are expected to use your program to find all 2098 solutions of the puzzle and <strong>diff that output against our </strong><a href="http://shootout.alioth.debian.org/download/meteor-output.txt"><strong>237KB output file</strong> (Save Target As… Save Link As…)</a> <strong><em>before</em> you contribute your program.</strong></p>
 
 <p>Each program should find and accumulate all the solutions for the Meteor Puzzle, and then print how many solutions were found, and sort and print all the solutions.</p>
 
