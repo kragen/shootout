4903790ec3a27088b62b21793cd56c4cb975fcb3
igouy-guest <igouy-guest>
2006-11-08 02:43:43 +0000

    *** empty log message ***

---
+++ website/about/meteor-about.tpl.php
@@ -0,0 +1,5 @@
+<p>This is a <strong>contest</strong> - different algorithms may be used.</p>
+
+<p>Each program should find and accumulate all the solutions for the Meteor Puzzle, print how many solutions were found, and sort and print all the solutions.</p>
+
+<p>The Meteor Puzzle and 3 Java puzzle solvers are described in <a href="http://www-128.ibm.com/developerworks/java/library/j-javaopt/">"Optimize your Java application's performance"</a>.</p>
