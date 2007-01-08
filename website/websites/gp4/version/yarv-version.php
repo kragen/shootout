6e2c89279456c68d39c9be9f23e1e07907a1ef33
igouy-guest <igouy-guest>
2007-01-08 22:51:52 +0000

    *** empty log message ***

--- website/websites/gp4/version/yarv-version.php
+++ website/websites/gp4/version/yarv-version.php
@@ -1,2 +1,2 @@
-<p>ruby 1.9.0 (Base: Ruby 1.9.0 2006-11-06) [i686-linux]
-YARVCore 0.4.1 Rev: 581 (2006-11-10) [opts: [direct threaded code] [inline method cache] ]</p>
+<p>ruby 1.9.0 (Base: Ruby 1.9.0 2006-12-06) [i686-linux]
+YARVCore 0.4.1 Rev: 590 (2006-12-31) [opts: [direct threaded code] [inline method cache] ]</p>
