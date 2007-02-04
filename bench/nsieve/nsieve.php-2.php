55077ce2845ced5a907b094016bf5a0b2c9922be
igouy-guest <igouy-guest>
2007-02-04 03:11:58 +0000

    *** empty log message ***

--- bench/nsieve/nsieve.php-2.php
+++ bench/nsieve/nsieve.php-2.php
@@ -4,29 +4,27 @@
  * http://shootout.alioth.debian.org/
  *
  * contributed by Charles G.
- * fixed by Isaac Gouy
+ * modified by Isaac Gouy
  */
 
 function nsieve($m)
 {
     $flags = ' ';
-    $flags[$m-1] = ' ';
+    $flags[$m] = ' ';
     
     $count = 0;
-    for ($i = 2; $i <= $m; ++$i)
-        if ($flags[$i-1] == ' ') {
+    for ($i = 2; $i < $m; ++$i)
+        if ($flags[$i] == ' ') {
             ++$count;
             for ($j = $i << 1; $j < $m; $j += $i)
-                $flags[$j-1] = 'x';
+                $flags[$j] = 'x';
         }
-
-//    echo "Primes up to $m $count\n";
 
     printf("Primes up to %8d %8d\n", $m, $count);
 
 }
 
 $m = $argv[1];
-for ($i = 0; $i < 3; ++$i)
+for ($i = 0; $i < 3; $i++)
     nsieve(10000 << ($m-$i));
 ?>
