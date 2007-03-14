93e17674de2e4cfef866d05e3f9f4d9432af8909
igouy-guest <igouy-guest>
2007-03-14 00:07:41 +0000

    *** empty log message ***

--- website/websites/basic.php
+++ website/websites/basic.php
@@ -1,8 +1,13 @@
-<?php 
-// Copyright (c) Isaac Gouy 2005
-
-require_once('config.php'); 
-$T = 'all'; $L = 'vb'; $metaRobots = '';
-require_once(LIB_PATH.'benchmark.php'); 
-?>
-
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" >
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta name="robots" content="noindex,noarchive" />
+<title>The Computer Language Shootout Benchmarks</title>
+<meta http-equiv="refresh" content="3;url=http://shootout.alioth.debian.org/gp4/fbasic.php">
+</head>
+<body>
+<p>Redirecting in 3 seconds</p> 
+<h3><a href="http://shootout.alioth.debian.org/gp4/fbasic.php">http://shootout.alioth.debian.org/debian/pascal.php</a></h3>
+</body>
+</html>
