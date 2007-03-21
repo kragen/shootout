53b412085eb5ef068a2cc9572fe7b06302151720
igouy-guest <igouy-guest>
2007-03-21 02:07:54 +0000

    *** empty log message ***

--- website/about/smlnj-about.tpl.php
+++ website/about/smlnj-about.tpl.php
@@ -1,3 +1,3 @@
 <?=$Version;?>
-<p>Home Page: <a href="http://cm.bell-labs.com/cm/cs/what/smlnj/">http://cm.bell-labs.com/cm/cs/what/smlnj/</a></p>
-<p>Download: <a href="http://cm.bell-labs.com/cm/cs/what/smlnj/software.html">http://cm.bell-labs.com/cm/cs/what/smlnj/software.html</a></p>
+<p>Home Page: <a href="http://www.smlnj.org/">http://www.smlnj.org/</a></p>
+<p>Download: <a href="http://www.smlnj.org//dist/working/index.html">http://www.smlnj.org//dist/working/index.html</a></p>
