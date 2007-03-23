82aa3e007da0feeb4f2de38c9eb10f0922958462
igouy-guest <igouy-guest>
2007-03-23 05:26:31 +0000

    *** empty log message ***

--- website/lib/faq.tpl.php
+++ website/lib/faq.tpl.php
@@ -197,7 +197,7 @@ Bash time builtin command
 </dd>
 
 <dt><a href="#os" name="os">What OS are you using on the test machine?</a></dt>
-<dd><p>We use <strong>Debian Linux&#8482;</strong> 'unstable', Kernel 2.6.18-3-k7 and <strong>Gentoo Linux&#8482;</strong> gentoo-sources-2.6.16-r13</p>
+<dd><p>We use <strong>Debian Linux&#8482;</strong> 'unstable', Kernel 2.6.18-3-k7 and <strong>Gentoo Linux&#8482;</strong> gentoo-sources-2.6.18-r4</p>
 </dd>
 
 
