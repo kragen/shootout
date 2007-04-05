d6cfc08fdcbe0c523f1f8ffbe88dc2e45ae25079
igouy-guest <igouy-guest>
2007-04-05 06:24:04 +0000

    *** empty log message ***

--- website/about/vw-about.tpl.php
+++ website/about/vw-about.tpl.php
@@ -1,5 +1,5 @@
 <p><strong>Code Lines</strong> is even more misleading for Smalltalk than for other languages - we show Smalltalk source code in a verbose <em>chunk file</em> format used to archive or transfer source code between Smalltalk <em>images</em>.</p>
 <p><a href="http://users.ipa.net/~dwighth/smalltalk/byte_aug81/design_principles_behind_smalltalk.html">"Design Principles Behind Smalltalk" by Daniel Ingalls</a></p>
 <?=$Version;?>
-<p>Home Page: <a href="http://smalltalk.cincom.com/prodinformation/index.ssp?content=vwfactsheet">Cincom Smalltalk™ VisualWorks® Environment Data Sheet</a></p>
-<p>Download: <a href="http://smalltalk.cincom.com/downloads/index.ssp?content=visualworks">VisualWorks® Non-Commercial</a></p>
+<p>Home Page: <a href="http://smalltalk.cincom.com/prodinformation/index.ssp?content=vwfactsheet">Cincom Smalltalk&#8482; VisualWorksl&#174; Environment Data Sheet</a></p>
+<p>Download: <a href="http://www.cincomsmalltalk.com/userblogs/cincom/blogView?content=smalltalk">VisualWorksl&#174; Non-Commercial</a></p>
