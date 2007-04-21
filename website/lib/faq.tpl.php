b41e09c1ffe5426ca8bc2ee555e0d78993ccc508
igouy-guest <igouy-guest>
2007-04-21 05:32:02 +0000

    *** empty log message ***

--- website/lib/faq.tpl.php
+++ website/lib/faq.tpl.php
@@ -30,7 +30,7 @@
 
 <dt><a href="#winning" name="winning"><strong>Who's winning?</strong></a></dt>
 <dd>
-<p>It varies from week to week. It varies from benchmark to benchmark. It depends which language implementations are compared. It depends which measures are compared.</p>
+<p>It varies from week to week. It varies from benchmark to benchmark. It depends which language implementations are compared. It depends which measures are compared. Be curious - look and learn.</p>
 </dd>
 
 <dt><a href="#end" name="end"><strong>When does the game end?</strong></a></dt>
@@ -128,7 +128,7 @@
 <p>As part of performance analysis, those differences hint at how much is not accounted for by JVM startup or dynamic compilation, and how little or how much we might still be able to achieve by contributing better programs.</p>
 </dd>
 
-<dt><a href="#whatlanguage" name="whatlanguage">What language was used to write the benchmark programs?</a></dt>
+<dt><a href="#whatlanguage" name="whatlanguage">What language was used to write each initial benchmark program?</a></dt>
 <dd><p>Different benchmark programs, different authors - different languages.</p>
 <p>The benchmark descriptions sometimes refer to an article which included program source: Lisp and C for fannkuch; Java for binary-trees and meteor and chameneos; Haskell for pidigits; Erlang for cheap-concurrency. And others as the author provided: C for mandelbrot and spectral-norm; Java for n-body. And others in Nice or C# or Lua or &#8230; as the mood would have it.</p>
 </dd>
