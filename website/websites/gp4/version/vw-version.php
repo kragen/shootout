1b70ca3e0887c6a8bfd65d58d2f478fdc43088e9
igouy-guest <igouy-guest>
2007-04-25 01:28:43 +0000

    *** empty log message ***

--- website/websites/gp4/version/vw-version.php
+++ website/websites/gp4/version/vw-version.php
@@ -1,1 +1,1 @@
-<p>VisualWorks(R) 7.4.1 Non-Commercial May 30, 2006</p>
+<p>Non-Commercial Pre-Release 7.5 of April 17, 2007</p>
--- website/websites/gp4/version/yarv-version.php
+++ website/websites/gp4/version/yarv-version.php
@@ -1,1 +1,1 @@
-<p>ruby 1.9.0 (2007-03-22 patchlevel 0) [i686-linux]</p>
+<p>ruby 1.9.0 (2007-04-24 patchlevel 0) [i686-linux]</p>
