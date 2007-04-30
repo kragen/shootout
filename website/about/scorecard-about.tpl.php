956f6d3bcb95635cc12f4c5d633c2cc28d5c19ad
igouy-guest <igouy-guest>
2007-04-30 22:41:28 +0000

    *** empty log message ***

--- website/about/scorecard-about.tpl.php
+++ website/about/scorecard-about.tpl.php
@@ -1,13 +1,8 @@
-<h5>The Basic Score</h5>
-<p>For each benchmark, the <em>best measurement</em> <strong>B</strong> is the lowest non-zero measurement.</p>
-<p>For each language implementation, the measurement <strong>L</strong> for the language implementation is converted to a basic score:</p><pre>basic score = B/L &#215; 100</pre><br/>
-<p>The highest possible unweighted score is 100. Missing programs and programs which did not complete the benchmark score 0.</p>
-
-<h5>The Mean Score</h5>
-<p>The basic score can be adjusted by a measurement multiplier (make CPU time or memory use or 
+<h5>The Weighted Geometric Mean</h5>
+<p>For each benchmark, the <em>best measurement</em> <strong>B</strong> is the lowest non-zero measurement. For each language implementation, the measurement <strong>L</strong> for the language implementation is converted to the <em>ratio</em> <strong>L/B</strong></p>
+<p>The ratio can be adjusted by a measurement multiplier (make CPU time or memory use or
 lines-of-code more or less important) and by a benchmark weight (make particular benchmarks more or less important).</p>
-<p>For each language implementation, the weighted scores for every benchmark are summed. The CPU time score, memory use score, and lines-of-code score are summed to give a total score.</p>
-<p>The total score is divided by the number of non-zero weighted benchmarks to give the mean score.</p>
+<p>For each language implementation, we report the weighted geometric mean of those <strong>L/B</strong> ratios.</p>
 
 
 
