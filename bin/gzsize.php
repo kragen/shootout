0c4103c6bbb496cf4e6c84b22a02e0247d52cf1f
igouy-guest <igouy-guest>
2007-06-11 16:22:51 +0000

    *** empty log message ***

--- bin/gzsize.php
+++ bin/gzsize.php
@@ -3,8 +3,14 @@ $stuff = file_get_contents('php://stdin');
 
 $stuff = preg_replace('/<span class="com">.*<\/span>/m', '', $stuff);
 $stuff = preg_replace('/<span class="slc">.*<\/span>/m', '', $stuff);
-$stuff = preg_replace('/<\/span>/m', '', $stuff);
 $stuff = preg_replace('/<span class="[a-z]{3}">/m', '', $stuff);
+
+$stuff = preg_replace('/<span class="hl com">.*<\/span>/m', '', $stuff);
+$stuff = preg_replace('/<span class="hl slc">.*<\/span>/m', '', $stuff);
+$stuff = preg_replace('/<span class="hl [a-z]{3}">/m', '', $stuff);
+
+$stuff = preg_replace('/<\/span>/m', '', $stuff);
+
 $stuff = preg_replace('/\s+/m', ' ', $stuff);
 $stuff = preg_replace('/&quot;/m', '"', $stuff);
 $stuff = preg_replace('/&lt;/m', '<', $stuff);
