a0caeec1142b7e471e62b3b5f9510b3ddb6782f5
igouy-guest <igouy-guest>
2007-06-11 05:19:01 +0000

    *** empty log message ***

---
+++ website/about/jruby-about.tpl.php
@@ -0,0 +1,7 @@
+<p>online book <a href="http://www.rubycentral.com/book/">"Programming Ruby: The Pragmatic Programmer's Guide</a></p>
+
+<?=$Version;?>
+
+<p>Home Page: <a href="http://jruby.codehaus.org/">Java powered Ruby implementation</a></p>
+
+<p>Download: <a href="http://dist.codehaus.org/jruby/">jruby-bin</a></p>
