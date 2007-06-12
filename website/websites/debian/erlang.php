0cb4af248ba0e8cd624efa7f05bbb61ab0d068f9
igouy-guest <igouy-guest>
2007-06-12 19:08:36 +0000

    *** empty log message ***

--- website/websites/debian/erlang.php
+++ website/websites/debian/erlang.php
@@ -2,7 +2,7 @@
 // Copyright (c) Isaac Gouy 2005
 
 require_once('config.php'); 
-$T = 'all'; $L = 'erlang'; $metaRobots = '';
+$T = 'all'; $L = 'hipe'; $metaRobots = '';
 require_once(LIB_PATH.'benchmark.php'); 
 ?>
 
