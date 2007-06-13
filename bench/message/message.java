a8f8960e0c9c5e90522d548e5212e5a1fd0a81cd
igouy-guest <igouy-guest>
2007-06-13 00:34:43 +0000

    *** empty log message ***

--- bench/message/message.java
+++ bench/message/message.java
@@ -1,59 +1,100 @@
-/* The Computer Language Shootout
-   http://shootout.alioth.debian.org/
+/* The Computer Language Benchmarks Game
+ http://shootout.alioth.debian.org/
 
-   contributed by James McIlree
-   modified by Dimitar Dimitrov
-*/
+ contributed by Mattias Bergander
+ */
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ArrayBlockingQueue;
+import java.util.LinkedList;
+import java.util.List;
 
+public class message {
+    public static final int numberOfThreads = 500;
 
-public class message implements Runnable {
-    private static final int NUMBER_OF_THREADS = 500;
-    private static final double QUEUE_BUFFER_RATIO = Integer.getInteger("bufferPct", 100) / 100.0;
+    public static int numberOfMessagesToSend;
 
-    private final BlockingQueue<Integer> queue;
-    private final message next;
+    public static void main(String args[]) {
+        numberOfMessagesToSend = Integer.parseInt(args[0]);
 
-    message(message next, int queueSize) {
-        this.queue = new ArrayBlockingQueue<Integer>(queueSize);
-        this.next = next;
+        MessageThread chain = null;
+        for (int i = 0; i < numberOfThreads; i++) {
+            chain = new MessageThread(chain);
+            new Thread(chain).start();
+        }
+
+        for (int i = 0; i < numberOfMessagesToSend; i++) {
+            chain.enqueue(new MutableInteger(0));
+        }
+
+    }
+}
+
+class MutableInteger {
+    int value;
+
+    public MutableInteger() {
+        this(0);
+    }
+
+    public MutableInteger(int value) {
+        this.value = value;
+    }
+
+    public MutableInteger increment() {
+        value++;
+        return this;
+    }
+
+    public int intValue() {
+        return value;
+    }
+}
+
+class MessageThread implements Runnable {
+    MessageThread nextThread;
+
+    List<MutableInteger> list = new LinkedList<MutableInteger>();
+
+    MessageThread(MessageThread nextThread) {
+        this.nextThread = nextThread;
     }
 
     public void run() {
-        if (next==null)  return;
-        try {
+        if (nextThread != null) {
             while (true) {
-                next.queue.put(this.queue.take() + 1);
+                nextThread.enqueue(dequeue());
             }
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        } else {
+            int sum = 0;
+            int finalSum = message.numberOfThreads * message.numberOfMessagesToSend;
+            while (sum < finalSum) {
+                sum += dequeue().intValue();
+            }
+            System.out.println(sum);
+            System.exit(0);
         }
     }
 
-    public static void main(String args[]) throws InterruptedException {
-        int messagesCount = Integer.parseInt(args[0]);
-        int queueSize = (int) (messagesCount * QUEUE_BUFFER_RATIO);
-
-        message first = new message(null, queueSize);
-        message last = first;
-        for (int i = 0; i < NUMBER_OF_THREADS-1; i++) {
-            last = new message(last, queueSize);
-            Thread thread = new Thread(last, "Worker-" + i);
-            thread.setDaemon(true);
-            thread.start();
+    /**
+     * @param message
+     */
+    public void enqueue(MutableInteger message) {
+        synchronized (list) {
+            list.add(message);
+            if (list.size() == 1) {
+                list.notify();
+            }
         }
+    }
 
-        for (int j = 0; j < messagesCount; j++) {
-            last.queue.put(1);
+    public MutableInteger dequeue() {
+        synchronized (list) {
+            while (list.size() == 0) {
+                try {
+                    list.wait();
+                } catch (InterruptedException e) {
+                }
+            }
+            return list.remove(0).increment();
         }
-
-        int sum = 0;
-        while (sum < NUMBER_OF_THREADS * messagesCount) {
-            sum += first.queue.take();
-        }
-
-        System.out.println(sum);
     }
 }
--- bench/message/message.java-4.java
+++ bench/message/message.java-4.java
@@ -1,97 +1,130 @@
-/* 
- The Computer Language Shootout
- http://shootout.alioth.debian.org/
- #303302
- contributed by tt@kyon.de
- */
-public final class message extends Thread {
+/* The Computer Language Benchmarks Game
+   http://shootout.alioth.debian.org/
+   contributed by Graham Miller 
+*/
 
-	private static final int THREADS = 500;
-	private static int msgCount;
-	private static int max;
-	private final message nextThread;
-	private int[] messages = new int[1024]; // reasonably sized buffer
-	private int todo;
-	boolean waiting;
+public class message {
+   public static final int numberOfThreads = 500;
 
-	public static void main(String args[]) {
-		msgCount = Integer.parseInt(args[0]);
-		max = msgCount * THREADS;
-		message thread = null;
-		for (int i = THREADS; --i >= 0;) {
-			(thread = new message(thread)).start();
-		}
-		for (int i = msgCount; --i >= 0;) {
-			thread.send(0);
-		}
-	}
-	private message(message next) {
-		nextThread = next;
-	}
-	public synchronized void run() {
-		try {
-			if (todo == 0) {
-				waiting = true;
-				wait();
-			}
-			if (nextThread != null) {
-				pass();
-			} else {
-				add();
-			}
-		} catch (InterruptedException e) {
-			// will not be thrown under any normal circumstances
-			e.printStackTrace();
-		}
-	}
-	private void pass() throws InterruptedException {
-		for (;;) {
-			int done = todo;
-			int[] m = messages;
-			do {
-				nextThread.send(m[--done] + 1);
-			} while (done != 0);
-			todo = 0;
-			do {
-				// no unsynchronized todos left
-				waiting = true;
-				wait();
-			} while (todo == 0);
-		}
-	}
-	private void add() throws InterruptedException {
-		int sum = 0;
-		for (;;) {
-			int done = todo;
-			int[] m = messages;
-			do {
-				sum += m[--done] + 1;
-			} while (done != 0);
-			todo = 0;
-			do {
-				// no unsynchronized todos left
-				if (sum == max) {
-					System.out.println(sum);
-					System.exit(0);
-				}
-				waiting = true;
-				wait();
-			} while (todo == 0);
-		}
-	}
-	private synchronized void send(int message) {
-		int[] m = messages;
-		int l = m.length;
-		if (todo == l) {
-			int[] n = new int[l << 2];
-			System.arraycopy(m, 0, n, 0, l);
-			messages = m = n;
-		}
-		m[todo++] = message;
-		if (waiting) {
-			// otherwise already notified
-			waiting = false;
-			notify();
-		}
-	}
+   public static int numberOfMessagesToSend;
+
+   public static void main(String args[]) {
+      numberOfMessagesToSend = Integer.parseInt(args[0]);
+
+      RingBufferThread chain = null;
+      for (int i = 0; i < numberOfThreads; i++) {
+         chain = new RingBufferThread(chain, numberOfMessagesToSend*(numberOfThreads));
+         chain.start();
+       }
+
+
+      for (int i = 0; i < numberOfMessagesToSend; i++) {
+         chain.enqueue(0);
+      }
+      chain.signalDoneSendingMessages();
+    }
+
+   public static class RingBufferThread extends Thread {
+
+      private static final int RING_BUFFER_CAPACITY = 100;
+         
+      private volatile RingNode loadNode;
+      private volatile RingNode consumeNode;
+
+      RingBufferThread nextThread;
+      private volatile boolean done = false;
+      private final int finalSum;
+
+      
+      RingBufferThread(RingBufferThread nextThread, int finalSum) {
+         RingNode node = new RingNode();
+         RingNode tail = node;
+         for (int i = 0; i < RING_BUFFER_CAPACITY-1; i ++){
+            RingNode newNode = new RingNode();
+            newNode.next = node;
+            node = newNode;
+         }
+         // complete the ring
+         tail.next = node;
+         
+         // both load and consume start at the same node
+         loadNode = node;
+         consumeNode = node;
+         
+         this.nextThread = nextThread;
+         this.finalSum = finalSum;
+      }
+
+      public void run() {
+         if (nextThread != null) {
+            while (!done || !isEmpty()) {
+               nextThread.enqueue(dequeue());
+            }
+            nextThread.signalDoneSendingMessages();
+         } else {
+            int sum = 0;
+            while (sum < finalSum) {
+               int message = dequeue();
+               sum += message;
+            }
+            System.out.println(sum);
+            System.exit(0);
+         }
+      }
+
+
+      private boolean isEmpty() {
+         return consumeNode == loadNode;
+      }
+
+      /**
+       * @param message
+       */
+      public final void enqueue(int message) {
+         // after this test becomes false, and the loop exits
+         // the removal of an element by the "other" thread
+         // cannot make it true again, so therefore it is invariant
+         // for the rest of the execution of this method.
+         // that is once we have some free space, we will always
+         // have free space until the thread calling this method
+         // adds an element.
+         do { /* nothing */ } while (loadNode.next == consumeNode && trueYield());
+
+         loadNode.message = message;
+         loadNode = loadNode.next;
+      }
+
+      public final int dequeue() {
+         // after this test becomes false, and the loop exits
+         // the addition of an element by the "other" thread
+         // cannot make it true again, therefore it is invariant
+         // for the rest of the execution of this method
+         // that is once we have at least one element, we will always
+         // have at least one element until the thread calling this
+         // method removes one.
+         do { /* nothing */ } while (loadNode == consumeNode && trueYield());
+
+         int message = 1 + consumeNode.message;
+         consumeNode = consumeNode.next;
+         
+          return message;
+      }
+
+      public final void signalDoneSendingMessages() {
+         // once done is true, I am the only
+         // thread accessing any of my variables, so we have no
+         // more threading issues
+         done = true;
+      }
+      
+      private final boolean trueYield() {
+         Thread.yield();
+         return true;
+      }
+   }
+   
+   static class RingNode {
+      public volatile int message;
+      public RingNode next;
+   }
 }
