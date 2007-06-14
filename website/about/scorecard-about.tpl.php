ecf530150a59c0b69abbe936b94b9842cfa0f35b
igouy-guest <igouy-guest>
2007-06-14 21:13:54 +0000

    *** empty log message ***

--- website/about/scorecard-about.tpl.php
+++ website/about/scorecard-about.tpl.php
@@ -1,4 +1,4 @@
-<p>(Language implementations with more than a couple of Timeouts distort the average too much, so they have been excluded from this ranking. They can still be <a href="benchmark.php?test=all&lang=oz&lang2=hipe"><strong>compared directly</strong></a> against another language implementation.)</p>
+<p>(Language implementations with more than a couple of Timeouts distort the ranking - so they have been excluded. They can still be <a href="benchmark.php?test=all&lang=oz&lang2=hipe"><strong>compared directly</strong></a> against another language implementation.)</p>
 
 <p>For each benchmark, <em>the best measurement</em> <strong>B</strong> is the lowest non-zero measurement. For each language implementation, the measurement <strong>L</strong> for the language implementation is converted to the <em>ratio</em> <strong>L/B</strong></p>
 <p>The ratio can be adjusted by a measurement multiplier (make CPU time or memory use or
