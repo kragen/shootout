1516567d32a23f72d6059d37169e8e82e781284a
igouy-guest <igouy-guest>
2007-06-15 01:14:50 +0000

    *** empty log message ***

--- bench/nsieve/nsieve.groovy
+++ bench/nsieve/nsieve.groovy
@@ -1,33 +1,30 @@
-/*
-	The Computer Language Shootout
-	http://shootout.alioth.debian.org/
-
-	contributed by Jochen Hinrichsen
-
-        The NSieveBits sample is more groovy style than this fixed boolean array
+/* The Computer Language Benchmarks Game
+   http://shootout.alioth.debian.org/
+   contributed by Jochen Hinrichsen
+   The NSieveBits sample is more groovy style than this fixed boolean array
 */
 
 def nsieve(isPrime, m) {
-        isPrime.each() {
-                it = true
-        }
-        def count = 0
-	for (i in 2..m) {
-		if (isPrime[i]) {
-                        (i+i..m).step(i) { j ->
-				isPrime[j] = false
-			}
+   isPrime.each() {
+      it = true
+   }
 
-                        count++
-		}
-	}
-	count
+def count = 0
+   for (i in 2..m) {
+      if (isPrime[i]) {
+         (i+i..m).step(i) { j ->
+            isPrime[j] = false
+         }
+         count++
+      }
+   }
+   count
 }
 
 def run(isPrime, n) {
-        def m = 2**n*10000
-	print("Primes up to ${m.toString().padLeft(8)}")
-	println(nsieve(isPrime, m).toString().padLeft(8))
+   def m = 2**n*10000
+   print("Primes up to ${m.toString().padLeft(8)}")
+   println(nsieve(isPrime, m).toString().padLeft(9))
 }
 
 def n = args.length == 0 ? 2 : args[0].toInteger()
@@ -40,5 +37,4 @@ run(isPrime, n)
 run(isPrime, n-1)
 run(isPrime, n-2)
 
-// EOF
 
--- bench/nsievebits/nsievebits.groovy
+++ bench/nsievebits/nsievebits.groovy
@@ -1,28 +1,26 @@
-/*
-	The Computer Language Shootout
-	http://shootout.alioth.debian.org/
+/* The Computer Language Benchmarks Game
+   http://shootout.alioth.debian.org/
+   contributed by Jochen Hinrichsen
 
-	contributed by Jochen Hinrichsen
 */
 
 def nsieve(m) {
-	def bits = new java.util.BitSet(m)
-	bits.set(2, m, true)
-	for (i in 2..m) {
-		if (bits.get(i)) {
-                        (i+i..m).step(i) { j ->
-				bits.clear(j)
-			}
-
-		}
-	}
-	bits.cardinality()	
+   def bits = new java.util.BitSet(m)
+   bits.set(2, m, true)
+   for (i in 2..m) {
+      if (bits.get(i)) {
+         (i+i..m).step(i) { j ->
+            bits.clear(j)
+         }
+      }
+   }
+   bits.cardinality()	
 }
 
 def run(n) {
-	int m = 2**n*10000
-	print("Primes up to ${m.toString().padLeft(8)}")
-	println(nsieve(m).toString().padLeft(8))
+   int m = 2**n*10000
+   print("Primes up to ${m.toString().padLeft(8)}")
+   println(nsieve(m).toString().padLeft(9))
 }
 
 def n = args.length == 0 ? 2 : args[0].toInteger()
@@ -31,6 +29,4 @@ n = (int) Math.max(n, 2)
 run(n)
 run(n-1)
 run(n-2)
-
-// EOF
 
