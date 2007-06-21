5fba07012b77537bd104c8bdf66e2a956bc7029f
igouy-guest <igouy-guest>
2007-06-21 04:19:48 +0000

    *** empty log message ***

---
+++ website/about/znn-about.tpl.php
@@ -0,0 +1,27 @@
+<?=$Version;?>
+<p>Home Page: <a href="http://www.zonnon.ethz.ch/">zonnon programming language & compiler</a></p>
+<p>Download: <a href="http://www.zonnon.ethz.ch/compiler/download.html">compiler for Mono/Rotor (Eclipse plugin)</a></p>
+
+<pre>
+module BenchmarksGame;
+import System;
+
+procedure {public} argi(): integer;
+var 
+   objArray : System.Array;
+   obj : System.Object;
+   n : integer;
+begin
+   objArray := System.Environment.GetCommandLineArgs();
+   obj := objArray.GetValue(1);
+   return integer(System.Int32.Parse(obj.ToString()));
+end argi;
+
+
+procedure {public} writex(x:real); 
+begin
+   System.Console.Write("{0:f9}",x); 
+end writex;
+
+end BenchmarksGame.
+</pre>
---
+++ website/websites/gp4/version/znn-version.php
@@ -0,0 +1,1 @@
+<p>Zonnon Compiler, Version 1.0.0.57 of Sunday, 08 April 2007, 10:24:06</p>
