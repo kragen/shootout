ee949f7da3383530ede77bc073730e5c8514a8a6
igouy-guest <igouy-guest>
2007-08-31 17:06:42 +0000

    *** empty log message ***

--- website/about/partialsums-about.tpl.php
+++ website/about/partialsums-about.tpl.php
@@ -1,6 +1,6 @@
 <p><strong>diff</strong> program output N = 25000 with this <a href="iofile.php?test=<?=$SelectedTest;?>&amp;lang=<?=$SelectedLang;?>&amp;file=output">output file</a> to check your program is correct before contributing.</p>
 
-<p>(Programs may use a single-loop or several-loops; programs may cache recomputed values in local variables)
+<p>(Programs may use a single-loop or several-loops; programs may cache <tt>&#178;&#8260;<sub>3</sub></tt>, <tt>k&#178;</tt>, <tt>k&#179;</tt>, <tt>sin k</tt>, <tt>cos k</tt> in local variables)
 
 <p>Each program should use the same <strong>na&#239;ve</strong> iterative double-precision algorithms to calculate partial-sums of the series:</p>
 <div>
