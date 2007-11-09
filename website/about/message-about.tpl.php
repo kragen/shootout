7310334e613ade5afdfbe2e2f78a5a94d41687f4
igouy-guest <igouy-guest>
2007-11-09 02:43:36 +0000

    *** empty log message ***

--- website/about/message-about.tpl.php
+++ website/about/message-about.tpl.php
@@ -1,24 +1,24 @@
 <p><strong>diff</strong> program output N = 10 with this <a href="iofile.php?test=<?=$SelectedTest;?>&amp;lang=<?=$SelectedLang;?>&amp;file=output">output file</a> to check your program is correct before contributing.
 </p>
 
-<p>Each program should create, keep alive, and <em>send integer messages</em> between 500 explicitly-linked threads. Programs may use kernel threads, lightweight threads, cooperative threads&#8230;</p>
+<p>Each program should create, keep alive, and <em>send integer values</em> between 500 explicitly-linked threads. Programs may use kernel threads, lightweight threads, cooperative threads&#8230;</p>
 
 <p>Each program should</p>
 <ul>
    <li>create 500 threads - each thread should
       <ul>
       <li>hold and use a reference to the next thread</li>      
-      <li>take, and increment, an integer message</li>
-      <li>put the incremented message on the next thread</li>
+      <li>take, and increment, an integer value</li>
+      <li>put the incremented value on the next thread</li>
       </ul>
    </li>  
    <li>N times
       <ul>
-      <li>put the integer message 0 on the first thread</li>     
-      <li>add the message taken and incremented by the last thread to a sum</li>         
+      <li>put the integer 0 on the first thread</li>     
+      <li>add the value taken and incremented by the last thread to a sum</li>         
       </ul>
    </li>                     
-   <li>print the sum of incremented integer messages - a count of takes</li>
+   <li>print the sum of incremented integer values - a count of takes</li>
 </ul>
 
 <p>Similar benchmarks are described in <a href="http://www.sics.se/~joe/ericsson/du98024.html">Performance Measurements of Threads in Java and Processes in Erlang, 1998;</a> and <a href="http://www.cl.cam.ac.uk/users/mr/Cobench.html">A Benchmark Test for BCPL Style Coroutines, 2004.</a> 
--- website/about/messagering-about.tpl.php
+++ website/about/messagering-about.tpl.php
@@ -1,15 +1,15 @@
 <p><strong>diff</strong> program output N = 1000 with this <a href="iofile.php?test=<?=$SelectedTest;?>&amp;lang=<?=$SelectedLang;?>&amp;file=output">output file</a> to check your program is correct before contributing.
 </p>
 
-<p>Each program should create and keep alive, 503 threads linked in an unbroken ring, and pass a message between one thread and the next thread at least N times. Programs may use kernel threads, lightweight threads, cooperative threads&#8230;</p>
+<p>Each program should create and keep alive, 503 threads linked in an unbroken ring, and pass a value between one thread and the next thread at least N times. Programs may use kernel threads, lightweight threads, cooperative threads&#8230;</p>
 
 <p>Each program should</p>
 <ul>
    <li>create 503 linked threads (named 1 to 503)</li>
    <li>thread 503 should be linked to thread 1, forming an unbroken ring</li>           
-   <li>pass the message to thread 1</li>     
-   <li>pass the message from thread to thread N times</li>        
-   <li>print the name of the last thread (1 to 503) to take the message</li>
+   <li>pass the value to thread 1</li>     
+   <li>pass the value from thread to thread N times</li>        
+   <li>print the name of the last thread (1 to 503) to take the value</li>
 </ul>
 
 <p>Similar benchmarks are described in <a href="http://www.sics.se/~joe/ericsson/du98024.html">Performance Measurements of Threads in Java and Processes in Erlang, 1998;</a> and <a href="http://www.cl.cam.ac.uk/users/mr/Cobench.html">A Benchmark Test for BCPL Style Coroutines, 2004.</a> 
