7b5fec05f407bea18336ed44e620bd6b2841dc73
igouy-guest <igouy-guest>
2007-11-27 22:32:26 +0000

    *** empty log message ***

--- website/about/gnat-about.tpl.php
+++ website/about/gnat-about.tpl.php
@@ -1,5 +1,5 @@
 <p>book <a href="http://www.adacore.com/wp-content/files/attachments/Ada_Distilled.pdf">Ada Distilled: An introduction to Ada Programming for Experienced Computer Programmers, Richard Riehle, 2003</a> (pdf)</p>
 <p>wikibook <a href="http://en.wikibooks.org/wiki/Ada_Programming">Ada Programming</a></p>
 <?=$Version;?>
-<p>Home Page: <a href="https://libre.adacore.com/">https://libre.adacore.com/</a></p>
-<p>Download: <a href="ftp://gcc.gnu.org/pub/gcc/releases">ftp://gcc.gnu.org/pub/gcc/releases</a></p>
+<p>Home Page: <a href="https://libre.adacore.com/">The Libre Site</a></p>
+<p>Download: <a href="https://libre.adacore.com/dynamic/download_page">Download GNAT GPL Edition</a></p>
