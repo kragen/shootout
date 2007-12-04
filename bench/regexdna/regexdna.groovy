2e4e32e6852eebecf1b934ed12c1b0b5217060c4
igouy-guest <igouy-guest>
2007-12-04 18:00:33 +0000

    *** empty log message ***

--- bench/regexdna/regexdna.groovy
+++ bench/regexdna/regexdna.groovy
@@ -3,40 +3,36 @@
 //
 // contributed by James Durbin
 // based very closely on Ruby version by jose fco. gonzalez
-// 
-// Performance probably awful if called repeatedly
-// from command line, but not quite as bad if 
-// run on larger and larger files.  
 
 
-def seq = new StringBuffer();   
-seq = System.in.readLines().join("\n");
+ddef seq = new StringBuffer();   
+seq = System.in.readLines().join("\n")+"\n";
 
 initialLength = seq.length();
 seq = (seq =~">.*\n|\n").replaceAll("");
 codeLength = seq.length();
 
 [
-	"agggtaaa|tttaccct" ,
-	"[cgt]gggtaaa|tttaccc[acg]", 
-	"a[act]ggtaaa|tttacc[agt]t",
-	"ag[act]gtaaa|tttac[agt]ct", 
-	"agg[act]taaa|ttta[agt]cct", 
-	"aggg[acg]aaa|ttt[cgt]ccct",
-	"agggt[cgt]aa|tt[acg]accct", 
-	"agggta[cgt]a|t[acg]taccct", 
-	"agggtaa[cgt]|[acg]ttaccct"
+   "agggtaaa|tttaccct" ,
+   "[cgt]gggtaaa|tttaccc[acg]", 
+   "a[act]ggtaaa|tttacc[agt]t",
+   "ag[act]gtaaa|tttac[agt]ct", 
+   "agg[act]taaa|ttta[agt]cct", 
+   "aggg[acg]aaa|ttt[cgt]ccct",
+   "agggt[cgt]aa|tt[acg]accct", 
+   "agggta[cgt]a|t[acg]taccct", 
+   "agggtaa[cgt]|[acg]ttaccct"
 ].each{
-	println "$it "+(seq =~it).getCount();
+   println "$it "+(seq =~it).getCount();
 };
 
 
 [
-	'B':'(c|g|t)', 'D':'(a|g|t)', 'H':'(a|c|t)', 'K':'(g|t)',
-	'M':'(a|c)', 'N':'(a|c|g|t)', 'R':'(a|g)', 'S':'(c|t)',
-	'V':'(a|c|g)', 'W':'(a|t)', 'Y':'(c|t)'
+   'B':'(c|g|t)', 'D':'(a|g|t)', 'H':'(a|c|t)', 'K':'(g|t)',
+   'M':'(a|c)', 'N':'(a|c|g|t)', 'R':'(a|g)', 'S':'(c|t)',
+   'V':'(a|c|g)', 'W':'(a|t)', 'Y':'(c|t)'
 ].each { f,r -> 
-	seq = (seq =~ f).replaceAll(r);
+   seq = (seq =~ f).replaceAll(r);
 }
 
 println "";
