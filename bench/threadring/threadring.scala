f06b7fb5d61112b906367660e6b9e6e52bd19e81
igouy-guest <igouy-guest>
2007-12-14 18:48:01 +0000

    *** empty log message ***

---
+++ bench/threadring/threadring.scala
@@ -0,0 +1,43 @@
+/* The Computer Language Benchmarks Game
+   http://shootout.alioth.debian.org/
+
+   Contributed by Vincent Kraeutler
+   modified by Isaac Gouy
+*/
+
+import scala.actors.Actor
+import scala.actors.Actor._
+
+object threadring {
+        
+    class Thread(_label: int) extends Actor {
+        
+        val label = _label
+        var next : Thread = null
+                
+        def act() { loop { react {
+                        case 0 => {
+                                println("Terminating at thread: " + label)
+                                System.exit(0)
+                        }
+                        case (n: int) => {
+                                next ! n - 1
+                        }
+                }}}  
+    }
+  
+    // create the threads
+    var ring = List.range(0, 503).map { i => new Thread(i + 1) }.toArray
+
+    // hook them up
+    ring.foreach{t => {
+                    val nextIndex = (t.label) % ring.length
+                    t.next = ring(nextIndex)
+                    t.start
+                 }}
+            
+    def main(args : Array[String]) : Unit = {
+           val nHops = Integer.parseInt(args(0))
+           ring(0) ! nHops
+    }
+}
