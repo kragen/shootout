088a5db0594b69a348396e72961945304f1c4361
igouy-guest <igouy-guest>
2008-01-19 00:32:33 +0000

    *** empty log message ***

--- website/websites/gp4/version/ghc-version.php
+++ website/websites/gp4/version/ghc-version.php
@@ -1,1 +1,1 @@
-<p>The Glorious Glasgow Haskell Compilation System, version 6.6</p>
+<p>Glasgow Haskell Compiler, Version 6.8.2, for Haskell 98, stage 2 booted by GHC version 6.8.2</p>
--- website/websites/gp4/version/ocaml-version.php
+++ website/websites/gp4/version/ocaml-version.php
@@ -1,1 +1,1 @@
-<p>The Objective Caml native-code compiler, version 3.09.3</p>
+<p>The Objective Caml toplevel, version 3.10.0</p>
