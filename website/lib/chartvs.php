dc9c7e151471756a9ae44a95bf7322e2ec680e0e
igouy-guest <igouy-guest>
2008-02-01 05:14:43 +0000

    *** empty log message ***

--- website/lib/chartvs.php
+++ website/lib/chartvs.php
@@ -159,7 +159,7 @@ ImageString($im, 2, $o-$v1*9+8+5+110, $b+50, 'Memory Use', $black);
 
 } else {
 
-$box = imagettftext($im,20,0,0,5,$black,'/usr/share/fonts/truetype/ttf-bitstream-vera/Vera.ttf','abcdef');
+$box = imagettftext($im,20,0,16,5,$black,'/usr/share/fonts/truetype/ttf-bitstream-vera/Vera.ttf','abcdef');
 
 }
 
