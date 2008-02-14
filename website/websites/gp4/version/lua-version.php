bc0465faff9fbfb5e0d872ddc3263ad90cb12e66
igouy-guest <igouy-guest>
2008-02-14 02:28:49 +0000

    *** empty log message ***

--- website/websites/gp4/version/lua-version.php
+++ website/websites/gp4/version/lua-version.php
@@ -1,1 +1,1 @@
-<p>Lua 5.1.2  Copyright (C) 1994-2007 Lua.org, PUC-Rio</p>
+<p>Lua 5.1.3  Copyright (C) 1994-2008 Lua.org, PUC-Rio</p>
--- website/websites/gp4/version/luajit-version.php
+++ website/websites/gp4/version/luajit-version.php
@@ -1,1 +1,2 @@
-<p>LuaJIT 1.1.0</p>
+<p>Lua 5.1.3  Copyright (C) 1994-2008 Lua.org, PUC-Rio<br/>
+LuaJIT 1.1.4  Copyright (C) 2005-2008 Mike Pall</p>
