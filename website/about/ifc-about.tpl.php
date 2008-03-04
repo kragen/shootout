b094bca0556799d91558981714a24a3886accdff
igouy-guest <igouy-guest>
2008-03-04 03:15:21 +0000

    *** empty log message ***

--- website/about/ifc-about.tpl.php
+++ website/about/ifc-about.tpl.php
@@ -1,3 +1,3 @@
 <?=$Version;?>
-<p>Home Page: <a href="http://www.intel.com/software/products/noncom/">http://www.intel.com/software/products/noncom/</a></p>
-<p>Download: <a href="http://www.intel.com/software/products/noncom/">http://www.intel.com/software/products/noncom/</a></p>
+<p>Home Page: <a href="http://www.intel.com/cd/software/products/asmo-na/eng/219771.htm">Non-Commercial Software Development</a></p>
+
