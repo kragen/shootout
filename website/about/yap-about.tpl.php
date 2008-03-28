fa836ff5f737426070b48901d573ff0b48987f9e
igouy-guest <igouy-guest>
2008-03-28 23:00:18 +0000

    *** empty log message ***

---
+++ website/about/yap-about.tpl.php
@@ -0,0 +1,4 @@
+<?=$Version;?>
+<p>Online book: <a href="http://www.coli.uni-saarland.de/~kris/learn-prolog-now/lpnpage.php?pageid=online">Learn Prolog Now!</a></p>
+<p>Home Page: <a href="http://www.ncc.up.pt/~vsc/Yap/">:- Yet Another Prolog</a></p>
+<p>Download: <a href="http://www.ncc.up.pt/~vsc/Yap/downloads.html">:- Stable Version</a></p>
---
+++ website/websites/gp4/version/yap-version.php
@@ -0,0 +1,1 @@
+<p>YAP version Yap-5.1.1</p>
