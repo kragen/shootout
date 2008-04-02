56a01d7ffeb0a5239b3f1d86c3abd60cb5efdd2f
igouy-guest <igouy-guest>
2008-04-02 15:59:49 +0000

    *** empty log message ***

--- website/websites/gp4/version/erlang-version.php
+++ website/websites/gp4/version/erlang-version.php
@@ -1,1 +1,2 @@
-<p>Erlang/OTP R12B-0</p>
+<p>Erlang (ASYNC_THREADS,HIPE) (BEAM) emulator version 5.5.3<br/>
+erlang-12.2.1</p>
--- website/websites/gp4/version/scala-version.php
+++ website/websites/gp4/version/scala-version.php
@@ -1,1 +1,1 @@
-<p>Scala compiler version 2.6.1-final -- (c) 2002-2007 LAMP/EPFL</p>
+<p>Scala compiler version 2.7.0-final -- (c) 2002-2008 LAMP/EPFL</p>
