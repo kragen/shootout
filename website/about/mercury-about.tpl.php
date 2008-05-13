b12e5c229dfdff53721a1bd09d7e5133ed8f063c
igouy-guest <igouy-guest>
2008-05-13 20:15:53 +0000

    *** empty log message ***

--- website/about/mercury-about.tpl.php
+++ website/about/mercury-about.tpl.php
@@ -1,3 +1,3 @@
 <?=$Version;?>
-<p>Home Page: <a href="http://www.cs.mu.oz.au/mercury/">http://www.cs.mu.oz.au/mercury/</a></p>
-<p>Download: <a href="http://www.cs.mu.oz.au/mercury/">http://www.cs.mu.oz.au/mercury/</a></p>
+<p>Home Page: <a href="http://www.cs.mu.oz.au/research/mercury/index.html">The Mercury Project</a></p>
+<p>Download: <a href="http://www.cs.mu.oz.au/research/mercury/download.html">The Mercury Project Download</a></p>
