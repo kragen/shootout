fdeb7a94860c087fdb69eb7a5ab7c85bb1be65d3
igouy-guest <igouy-guest>
2008-05-21 01:31:24 +0000

    *** empty log message ***

--- bench/sumcol/sumcol.java-3.java
+++ bench/sumcol/sumcol.java-3.java
@@ -6,51 +6,35 @@
 import java.io.*;
 
 public final class sumcol {
-   
-   static final byte[] buf = new byte [32768];
-   static final DataInputStream in = new DataInputStream (System.in);
-   static int count = 0;
-   static int token = 0;
-   static int index = 0;
-      
+
+   static final byte[] buf = new byte [18432];
+   final static InputStream in = System.in;
+
    public static void main(String[] args) throws Exception {
-      int sum = 0;
-      while (count >= 0){
-      	  readToken();
-          sum += token; 
-      }    	
-      System.out.println(sum);
+
+      System.out.println(sum());
    }
-   
-   private static void readToken() throws Exception
-   {   	    
-   	    int c = read();
-	    boolean neg = false;
-	    if (c == '-') {
-	    	neg = true;
-	    	c = read();
-	    }
-	    token = 0;
-	    while (true) {
-	      if ('0' <= c && c <= '9') {
-		  token = token * 10 + (c - '0');
-		  } else
-		      break;
-		  c = read();
-	    }
-	    token = neg ? -token : token;
-	    return;
-   }
-   private static int read() throws Exception
+
+   private static int sum() throws Exception
    {
-   	   if (count == 0)
-   	   {
-   	   	count = in.read(buf); 
-   	   	if (count == -1)
-   	   	   return '\0';
-   	   	index = 0;  
-   	   }
-   	  count--; 
-      return buf [index++];
+   	  int total = 0, num=0, j, neg = 1;
+   	  while ((j = in.read(buf)) > 0)
+   	  {
+   	  	for (int i = 0; i < j; i++)
+   	  	{
+   	  		int c = buf[i];
+            if (c >= '0' && c <= '9')
+                num =  num * 10 + c - '0';
+            else if (c == '-')
+                neg = -1;
+            else {
+                total += (num * neg);
+                num = 0;
+                neg = 1;
+            }	
+   	  	}
+   	  	
+   	  }
+      return total;
    }
 }
