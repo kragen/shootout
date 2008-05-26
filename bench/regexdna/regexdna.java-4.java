02a276489a1216b3035cc80fddd8cad12c9b8d95
igouy-guest <igouy-guest>
2008-05-26 00:48:28 +0000

    *** empty log message ***

--- bench/regexdna/regexdna.java-4.java
+++ bench/regexdna/regexdna.java-4.java
@@ -1,78 +1,115 @@
 /*
-  The Computer Language Benchmarks Game
-  http://shootout.alioth.debian.org/
-  Modifications to Josh Goldfoot version by Byron Foster
-  which was modified originally from the Nice entry by Isaac Guoy
-  modified to not split at "|" and increased StringBuilder initial size by Amir K aka Razzi.
+   The Computer Language Benchmarks Game
+   http://shootout.alioth.debian.org/
+   contributed by Razii, idea taken from Elliott Hughes and Roger Millington
 */
 
 import java.io.*;
 import java.util.regex.*;
+import java.util.*;
 
-public final class regexdna
-{
+public final class regexdna {
 
-  public static void main(String[] args)
-    throws IOException
-  {
-    BufferedReader br = new BufferedReader (new InputStreamReader(System.in), 65536);
-    StringBuilder sb = new StringBuilder(10240 * 8);
-    int initialLength = 0;
-    for (String line = ""; line != null; line = br.readLine())
-    {
-      initialLength += line.length() + 1;
-      if (line.startsWith(">")) continue;
-      sb.append(line);
-    }
+   private static final Map<String, String> replacements = new HashMap<String, String>();
 
-    String sequence = sb.toString();
-    int codeLength = sequence.length();
+   static {
 
-    String[] variants =
-      {
-        "agggtaaa|tttaccct" ,"[cgt]gggtaaa|tttaccc[acg]", 
-        "a[act]ggtaaa|tttacc[agt]t", "ag[act]gtaaa|tttac[agt]ct", 
-        "agg[act]taaa|ttta[agt]cct", "aggg[acg]aaa|ttt[cgt]ccct",
-        "agggt[cgt]aa|tt[acg]accct", "agggta[cgt]a|t[acg]taccct", 
-        "agggtaa[cgt]|[acg]ttaccct"
-      };
+      replacements.put("W", "(a|t)");
+      replacements.put("Y", "(c|t)");
+      replacements.put("K", "(g|t)");
+      replacements.put("M", "(a|c)");
+      replacements.put("S", "(c|g)");
+      replacements.put("R", "(a|g)");
+      replacements.put("B", "(c|g|t)");
+      replacements.put("D", "(a|g|t)");
+      replacements.put("V", "(a|c|g)");
+      replacements.put("H", "(a|c|t)");
+      replacements.put("N", "(a|c|g|t)");
+   }
 
+   static abstract class Rewriter {
+      private Pattern pattern;
+      private Matcher matcher;
 
-    for (String variant : variants)
-    {
-      int count = 0;
-      Matcher m = Pattern.compile(variant).matcher(sequence);
-      while (m.find())
-        count++;
-      System.out.println(variant + " " + count);
-    }
+      public Rewriter(String regularExpression) {
 
-    sb = new StringBuilder(10240 * 8);
-    for (int i=0; i<sequence.length(); i++)
-    {
-      char c = sequence.charAt(i);
-      switch (c)
-      {
-      case 'B': sb.append("(c|g|t)"); break;
-      case 'D': sb.append("(a|g|t)"); break;
-      case 'H': sb.append("(a|c|t)"); break;
-      case 'K': sb.append("(g|t)"); break;
-      case 'M': sb.append("(a|c)"); break;
-      case 'N': sb.append("(a|c|g|t)"); break;
-      case 'R': sb.append("(a|g)"); break;
-      case 'S': sb.append("(c|g)"); break;
-      case 'V': sb.append("(a|c|g)"); break;
-      case 'W': sb.append("(a|t)"); break;
-      case 'Y': sb.append("(c|t)"); break;
-      default: sb.append(c); break;
+         this.pattern = Pattern.compile(regularExpression);
       }
-    }
 
-    System.out.println();
-    System.out.println(initialLength-1); // Assume file does not end
-                                         // with \n
-    System.out.println(codeLength);
-    System.out.println(sb.length());
-  }
+      public String group(int i) {
+         return matcher.group(i);
+      }
+
+      public abstract String replacement();
+
+      public String rewrite(CharSequence original) {
+         return rewrite(original, new StringBuffer(original.length())).toString();
+      }
+
+      public StringBuffer rewrite(CharSequence original, StringBuffer destination) {
+         this.matcher = pattern.matcher(original);
+         while (matcher.find()) {
+            matcher.appendReplacement(destination, "");
+            destination.append(replacement());
+         }
+         matcher.appendTail(destination);
+         return destination;
+      }
+   }
+
+   public static void main(String[] args)
+   throws IOException {
+
+      Reader r = new InputStreamReader(System.in, "ISO-8859-1");
+      StringBuilder sb = new StringBuilder(5100000);
+      char[] cbuf = new char[16384];
+      int charsRead;
+      while ((charsRead = r.read(cbuf)) != -1)
+         sb.append(cbuf, 0, charsRead);
+
+      int initialLength = sb.length();
+
+      String sequence = new Rewriter(">.*\n|\n") {
+
+         public String replacement() {
+            return "";
+         }
+      }.rewrite(sb);
+
+
+      int codeLength = sequence.length();
+
+      String[] variants = { "agggtaaa|tttaccct" ,
+                       "[cgt]gggtaaa|tttaccc[acg]",
+                       "a[act]ggtaaa|tttacc[agt]t",
+                       "ag[act]gtaaa|tttac[agt]ct",
+                       "agg[act]taaa|ttta[agt]cct",
+                       "aggg[acg]aaa|ttt[cgt]ccct",
+                       "agggt[cgt]aa|tt[acg]accct",
+                       "agggta[cgt]a|t[acg]taccct",
+                       "agggtaa[cgt]|[acg]ttaccct"
+                     };
+
+      for (String variant : variants) {
+
+         int count = 0;
+         Matcher m = Pattern.compile(variant).matcher(sequence);
+         while (m.find())
+            count++;
+         System.out.println(variant + " " + count);
+      }
+
+      sequence = new Rewriter("[WYKMSRBDVHN]") {
+
+         public String replacement() {
+            return replacements.get(group(0));
+         }
+      }.rewrite(sequence);
+
+      System.out.println();
+      System.out.println(initialLength);
+      System.out.println(codeLength);
+      System.out.println(sequence.length());
+
+   }
 }
-
