17cfdb3d6cee46805880f786e1870cdd1c7e0923
igouy-guest <igouy-guest>
2008-07-11 03:45:09 +0000

    *** empty log message ***

--- website/websites/gp4/version/javaclient-version.php
+++ website/websites/gp4/version/javaclient-version.php
@@ -1,4 +1,4 @@
-<p>java version "1.6.0_04"<br/>
-Java(TM) SE Runtime Environment (build 1.6.0_04-b12)<br/>
-Java HotSpot(TM) Client VM (build 10.0-b19, mixed mode)
+<p>java version "1.6.0_07"<br/>
+Java(TM) SE Runtime Environment (build 1.6.0_07-b06)<br/>
+Java HotSpot(TM) Client VM (build 10.0-b23, mixed mode, sharing)
 </p>
--- website/websites/gp4/version/javaxint-version.php
+++ website/websites/gp4/version/javaxint-version.php
@@ -1,4 +1,4 @@
-<p>java version "1.6.0_04"<br/>
-Java(TM) SE Runtime Environment (build 1.6.0_04-b12)<br/>
-Java HotSpot(TM) Server VM (build 10.0-b19, mixed mode)
+<p>java version "1.6.0_07"<br/>
+Java(TM) SE Runtime Environment (build 1.6.0_07-b06)<br/>
+Java HotSpot(TM) Server VM (build 10.0-b23, interpreted mode, sharing)
 </p>
