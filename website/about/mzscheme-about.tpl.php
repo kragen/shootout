096bc2827dedde572dac89f5ae9d3186cfc95d92
igouy-guest <igouy-guest>
2008-07-20 05:08:12 +0000

    *** empty log message ***

--- website/about/mzscheme-about.tpl.php
+++ website/about/mzscheme-about.tpl.php
@@ -1,3 +1,3 @@
 <?=$Version;?>
-<p>Home Page: <a href="http://www.plt-scheme.org">http://www.plt-scheme.org</a></p>
-<p>Download: <a href="http://download.plt-scheme.org/drscheme/">http://download.plt-scheme.org/drscheme/</a></p>
+<p>Home Page: <a href="http://www.plt-scheme.org">PLT Scheme</a></p>
+<p>Download: <a href="http://download.plt-scheme.org/mzscheme/">Download MzScheme</a></p>
