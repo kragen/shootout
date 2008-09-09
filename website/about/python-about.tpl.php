93da86c588f01bf23d3cdd460dee376e2033366d
igouy-guest <igouy-guest>
2008-09-09 05:28:17 +0000

    *** empty log message ***

--- website/about/python-about.tpl.php
+++ website/about/python-about.tpl.php
@@ -1,4 +1,4 @@
 <?=$Version;?>
-<p>Paul Graham's <a href="http://paulgraham.com/pypar.html">Essay</a></p>
+<p>Online book: <a href="http://www.diveintopython.org/">Dive Into Python</a></p>
 <p>Home Page: <a href="http://www.python.org/">http://www.python.org/</a></p>
 <p>Download: <a href="http://www.python.org/download/">http://www.python.org/download/</a></p>
