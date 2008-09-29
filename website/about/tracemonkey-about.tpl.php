b4deb14e01579b6bc15f3f186c8b99c4e25b30b0
igouy-guest <igouy-guest>
2008-09-29 02:58:16 +0000

    *** empty log message ***

---
+++ website/about/tracemonkey-about.tpl.php
@@ -0,0 +1,5 @@
+<p>ECMAScript standard <a href="http://www.mozilla.org/js/language/E262-3.pdf">"ECMA-262 Edition 3"</a> 1.2MB pdf</p>
+
+<?=$Version;?>
+
+<p>Home Page: <a href="https://wiki.mozilla.org/JavaScript:TraceMonkey">mozilla wiki JavaScript:TraceMonkey</a></p>
