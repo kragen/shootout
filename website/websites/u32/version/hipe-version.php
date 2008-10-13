cde8643a8fb865cc4ba5cfeec7d9eac2d46adf17
igouy-guest <igouy-guest>
2008-10-13 03:58:45 +0000

    *** empty log message ***

--- website/websites/u32/version/hipe-version.php
+++ website/websites/u32/version/hipe-version.php
@@ -1,1 +1,1 @@
-<p>Erlang (BEAM) emulator version 5.6.2 [source] [smp:4] [async-threads:0] [hipe] [kernel-poll:false]</p>
+<p>Erlang (BEAM) emulator version 5.6.2 [source] [async-threads:0] [hipe] [kernel-poll:false]</p>
