ecdb414a3380c456977f72a332e40ef4859bcc23
igouy-guest <igouy-guest>
2008-10-22 01:55:42 +0000

    *** empty log message ***

---
+++ bench/mandelbrot/mandelbrot.scala
@@ -0,0 +1,97 @@
+/* The Computer Language Benchmarks Game
+   http://shootout.alioth.debian.org/
+   original contributed by Isaac Gouy
+   made to use single array and parallelized by Stephen Marsh
+*/
+
+import java.io.BufferedOutputStream
+
+object mandelbrot {
+  var size: int = 0
+  var bytesPerRow: int = 0
+  var bitmap: Array[byte] = _
+  var donerows: Array[boolean] = _
+  var nextRow = 0
+  val limitSquared = 4.0
+  val max = 50
+
+  def getNextRow: int = synchronized {
+    notify() // wakes up main thread
+    if (nextRow == size) return -1
+    nextRow += 1
+    return nextRow - 1
+  }
+
+  def main(args: Array[String]) {
+    size = Integer.parseInt(args(0))
+    bytesPerRow = (size+7)/8 // ceiling of (size / 8)
+    bitmap = new Array(bytesPerRow*size)
+    donerows = new Array(size)
+
+    for (i <- 0 until Runtime.getRuntime().availableProcessors())
+      new Thread(new CalcThread()).start()
+
+    // main thread prints rows as they finish
+    println("P4\n" + size + " " + size)
+    val w = new BufferedOutputStream(System.out)
+    var y = 0
+    while (y < size) {
+      while (!donerows(y)) synchronized{wait()}
+      w.write(bitmap, y * bytesPerRow, bytesPerRow)
+      y += 1
+    }
+    w.close
+  }
+
+  class CalcThread extends Runnable {
+    def run () {
+      while (true) {
+	var y = getNextRow
+	if (y == -1) return
+
+	var bits = 0
+	var bitnum = 0
+	var x = 0
+	var aindex = y * bytesPerRow
+
+	while (x < size) {
+	
+        val cr = 2.0 * x / size - 1.5
+        val ci = 2.0 * y / size - 1.0
+	
+        var zr = 0.0; var zi = 0.0
+        var tr = 0.0; var ti = 0.0
+	
+        var j = max
+            do {
+              zi = 2.0 * zr * zi + ci
+              zr = tr - ti + cr
+              ti = zi*zi
+              tr = zr*zr
+	      
+              j = j - 1
+            } while (!(tr + ti > limitSquared) && j > 0)
+	 
+         bits = bits << 1
+         if (!(tr + ti > limitSquared)) bits += 1
+         bitnum += 1
+	 
+         if (x == size - 1) {
+           bits = bits << (8 - bitnum)
+           bitnum = 8
+         }
+	 
+         if (bitnum == 8) {
+           bitmap(aindex) = bits.toByte
+	   aindex += 1
+           bits = 0
+           bitnum = 0
+         }
+	 
+         x += 1
+	}
+	donerows(y) = true
+      }
+    }
+  }
+}
