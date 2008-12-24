2821e5e9187965b7bd11ee2dffd0acfe0f6927a0
igouy-guest <igouy-guest>
2008-12-24 00:12:51 +0000

    *** empty log message ***

--- website/about/python3-about.tpl.php
+++ website/about/python3-about.tpl.php
@@ -1,4 +1,3 @@
 <?=$Version;?>
 <p>A new version of the Python "language that is incompatible with the 2.x line of releases".</p>
 <p>Home Page: <a href="http://www.python.org/download/releases/3.0/">Python 3.0</a></p>
-a></p>
