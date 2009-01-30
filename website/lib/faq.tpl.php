2cddaa86cbc5646b6059f49f47f4d2d3c4cc191e
igouy-guest <igouy-guest>
2009-01-30 18:40:46 +0000

    *** empty log message ***

--- website/lib/faq.tpl.php
+++ website/lib/faq.tpl.php
@@ -517,6 +517,8 @@ correct result, but also <strong>use the same algorithm</strong> to calculate th
 <dl>
 <dd><p>You'll come across a range of uses for the programs and measurements:</p>
 <ul>
+<li>edge cases - <a href="http://ulf.wiger.net/weblog/wp-content/uploads/2009/01/damp09-erlang-multicore.pdf">Erlang Programming
+for Multi-core (pdf)</a> "runs slower the more cores you throw at it" p22</li>
 <li>training a classifier - <a href="http://blog.chrislowis.co.uk/2009/01/04/identify-programming-languages-with-source-classifier.html">Identify Programming Languages with SourceClassifier</a></li>
 <li>more comparisons - <a href="http://www.cs.purdue.edu/homes/sbarakat/cs456/Scripting.pdf">Performance of Scripting Languages (pdf)</a></li>
 <li>performance testing - <a href="http://dromaeo.com/">Mozilla JavaScript performance testing</a></li>
