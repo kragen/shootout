987db57f702356af36c64de2c28ba1507842697b
igouy-guest <igouy-guest>
2009-02-01 01:18:46 +0000

    *** empty log message ***

--- website/about/yarv-about.tpl.php
+++ website/about/yarv-about.tpl.php
@@ -1,4 +1,4 @@
 <p>online book <a href="http://www.rubycentral.com/book/">Programming Ruby: The Pragmatic Programmers Guide</a></p>
 <?=$Version;?>
 <p>Home Page: <a href="http://www.ruby-lang.org/en/community/ruby-core/">Ruby Core</a></p>
-<p>Download: <a href="http://www.ruby-lang.org/en/community/ruby-core/#following-ruby">Using Subversion to Track Ruby Development</a></p>
+<p>Download: <a href="http://www.ruby-lang.org/en/news/2009/01/30/ruby-1-9-1-released/">Ruby 1.9.1 released</a></p>
