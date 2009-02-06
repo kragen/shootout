3ebc9faa27d76720d62d3a52f574e6321fcbd8c8
igouy-guest <igouy-guest>
2009-02-06 00:59:03 +0000

    *** empty log message ***

---
+++ bench/threadring/threadring.lua-3.lua
@@ -0,0 +1,45 @@
+-- The Computer Language Benchmarks Game
+-- http://shootout.alioth.debian.org/
+-- contributed by Sam Roberts
+-- reviewed by Bruno Massa
+
+require"coroutine"
+
+-- first and only argument is number of token passes
+local n         = assert(tonumber(arg[1]))
+
+-- fixed size pool
+local poolsize  = 503
+local threads   = {}
+
+-- cache these to avoid global environment lookups
+local create    = coroutine.create
+local resume    = coroutine.resume
+local yield     = coroutine.yield
+
+local id        = 1
+local token     = 0
+local ok
+
+local body = function(token)
+  while true do
+    token = yield(token + 1)
+  end
+end
+
+-- create all threads
+for id = 1, poolsize do
+  threads[id] = create(body)
+end
+
+-- send the token
+repeat
+  if id == poolsize then
+    id = 1
+  else
+    id = id + 1
+  end
+  ok, token = resume(threads[id], token)
+until token == n
+
+print(id)
