047e66cf8e73e317b6aa9166ff0d5742576e8b14
igouy-guest <igouy-guest>
2009-02-08 16:30:38 +0000

    *** empty log message ***

--- website/about/scorecard-about.tpl.php
+++ website/about/scorecard-about.tpl.php
@@ -3,7 +3,7 @@
 <p>For each benchmark, <em>the best measurement</em> <strong>B</strong> is the lowest non-zero measurement. For each language implementation, the measurement <strong>L</strong> for the language implementation is converted to the <em>ratio</em> <strong>L/B</strong></p>
 <p>The ratio can be adjusted by a measurement multiplier (make CPU time or memory use or
 lines-of-code more or less important) and by a benchmark weight (make particular benchmarks more or less important).</p>
-<p>For each language implementation, we report the <em>Weighted Geometric Mean</em> of those <strong>L/B</strong> ratios.</p>
+<p>For each language implementation, we report the <a href="http://portal.acm.org/citation.cfm?id=5666.5673"><strong>Weighted Geometric Mean</strong></a> of those <strong>L/B</strong> ratios.</p>
 
 
 
