8737809fa2784d1e3621a9b6d284a766f546fe13
igouy-guest <igouy-guest>
2009-02-17 03:23:15 +0000

    *** empty log message ***

--- website/about/boxplot-about.tpl.php
+++ website/about/boxplot-about.tpl.php
@@ -1,14 +1,5 @@
-<p>For each benchmark, <strong>F</strong> is the fastest time measurement; for each language implementation, the fastest time measurement <strong>T</strong> is converted to the <em>ratio</em> <strong><sup>T</sup>/<sub>F</sub></strong> and then to <em>"hemibels"</em> <strong>2 &#215; log<sub>10</sub><sup>T</sup>/<sub>F</sub></strong></p>
+<p>For each benchmark, <strong>F</strong> is the fastest time measurement; for each language implementation, each time measurement <strong>T</strong> is then converted to <strong>log<sub>10</sub><sup>T</sup>/<sub>F</sub></strong></p>
 
 <p><a href="http://www.lcgceurope.com/lcgceurope/data/articlestandard/lcgceurope/132005/152912/article.pdf"><strong>"Visual Presentation of Data by Means of Box Plots" (pdf)</strong></a></p>
 
 <p>Note: only the smallest and largest outliers are shown.</p>
-
-<p>Thanks to M. Edward (Ed) Borasky for "hemibel" enthusiasm.</p>
-
-
-
-
-
-
-
