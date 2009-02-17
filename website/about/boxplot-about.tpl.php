5153e0dced62d7e105391b5cd2c243a6aedecbb9
igouy-guest <igouy-guest>
2009-02-17 03:29:34 +0000

    *** empty log message ***

--- website/about/boxplot-about.tpl.php
+++ website/about/boxplot-about.tpl.php
@@ -1,4 +1,4 @@
-<p>For each benchmark, <strong>F</strong> is the fastest time measurement; for each language implementation, each time measurement <strong>T</strong> is then converted to <strong>log<sub>10</sub><sup>T</sup>/<sub>F</sub></strong></p>
+<p>For each benchmark, <strong>F</strong> is the fastest time measurement; for each language implementation, each time measurement <strong>T</strong> is then converted to the <em>ratio</em> <strong><sup>T</sup>/<sub>F</sub></strong></p>
 
 <p><a href="http://www.lcgceurope.com/lcgceurope/data/articlestandard/lcgceurope/132005/152912/article.pdf"><strong>"Visual Presentation of Data by Means of Box Plots" (pdf)</strong></a></p>
 
