1b453fc3c99339a8bcc256df437a1c61423da152
igouy-guest <igouy-guest>
2009-02-18 21:18:15 +0000

    *** empty log message ***

--- website/lib/headtohead.tpl.php
+++ website/lib/headtohead.tpl.php
@@ -24,74 +24,94 @@ $ShortName2 = $Langs[$SelectedLang2][LANG_NAME];
 <? MkHeadToHeadMenuForm($Tests,$SelectedTest,$Langs,$SelectedLang,$SelectedLang2,"fullcpu"); ?>
 
 <h2><a href="#title" name="title">&nbsp;Are the <?=$LangName;?> programs better?</a></h2>
-<p>For each one of our benchmarks, a dark gray bar shows when it had the better time, a thin black bar shows when it had the better memory use, and a white outline bar shows when it had smaller source code size.</p>
 
-
-<p><img src="chartvs.php?<?='r='.Encode($ratios);?>&amp;<?='m='.Encode($Mark.' n');?>&amp;<?='w='.Encode($SelectedLang.'O'.$SelectedLang2);?>"
+<p><br/><img src="chartvs.php?<?='r='.Encode($ratios);?>&amp;<?='m='.Encode($Mark.' n');?>&amp;<?='w='.Encode($SelectedLang.'O'.$SelectedLang2);?>"
    alt=""
    title=""
    width="480" height="300"
  /></p>
 
 
-<p>How many times <em>faster or smaller</em> are the <strong><?=$LangName;?></strong> programs than the corresponding <?=$LangName2;?> programs?</p>
+<p>How many times <em>worse or better</em> are the <strong><?=$LangName;?></strong> programs than the corresponding <?=$LangName2;?> programs?</p>
 
 
 <table>
 <colgroup span="1" class="txt"></colgroup>
 <colgroup span="4" class="num"></colgroup>
-<tr><th colspan="5"><?=$LangName;?> <a href="#ratio" name="ratio"><em>x&nbsp;times</em>&nbsp;better</a> <span class="num2"><br/>~ <?=$LangName2;?> <em>x&nbsp;times</em>&nbsp;better</span></th></tr>
+<tr><th colspan="5"><a href="#ratio" name="ratio"><span class="num2">~&nbsp;<em>x&nbsp;times</em>&nbsp;worse&nbsp;than&nbsp;<?=$LangName2;?></span></a><br/><?=$LangName;?>&nbsp;<em>x&nbsp;times</em>&nbsp;better</th></tr>
 
 <tr>
-<th>Program &amp; Logs</th>
-<th>&nbsp;Faster&nbsp;</th>
-<th>Smaller: Memory Use</th>
-<th>Smaller: GZip Bytes</th>
-<th>Reduced N</th>
+<th>Program&nbsp;&amp;&nbsp;Logs</th>
+<th>Time</th>
+<th>Memory&nbsp;Use</th>
+<th>Source&nbsp;Size</th>
+<th>Reduced&nbsp;N</th>
 </tr>
 
+
 <?
+
+$noprogram = array();
+$nocomparison = array();
+$failed = array();
+
+foreach($sTests as $row){
+   if ($row[TEST_WEIGHT]<=0){ continue; }
+   $Link = $row[TEST_LINK];
+   $Name = $row[TEST_NAME];
+   if (isset($data[$Link])){
+      $v = $data[$Link];
+      if ($v[N_LINES] < 0){
+         if ($v[N_LINES] == NO_COMPARISON){
+            $nocomparison[] = array($Link,$SelectedLang,$v[N_ID],$Name,'No '.$Langs[$v[N_LANG]][LANG_NAME]);
+
+         } else {
+            $failed[] = array($Link,$SelectedLang,$v[N_ID],$Name,StatusMessage($v[N_LINES]) );
+         }
+      }
+   } else {
+      $noprogram[] = array($Link,$SelectedLang,0,$Name,'No&nbsp;program');
+   }
+}
+
+foreach($noprogram as $tr){
+   printf('<tr><td><a href="benchmark.php?test=%s&amp;lang=%s">%s</a></td><td><span class="message">%s</span></td><td></td><td></td><td></td></tr>',
+      $tr[0],$tr[1],$tr[3],$tr[4]);
+}
+
+foreach($nocomparison as $tr){
+      printf('<tr><td><a href="benchmark.php?test=%s&amp;lang=%s&amp;id=%d">%s</a></td><td>&nbsp;</td><td>&nbsp;</td><td colspan="2"><span class="message">%s</span></td></tr>',
+      $tr[0],$tr[1],$tr[2],$tr[3],$tr[4]);
+}
+
+foreach($failed as $tr){
+      printf('<tr><td><a href="benchmark.php?test=%s&amp;lang=%s&amp;id=%d">%s</a></td><td><span class="message">%s</span></td><td></td><td></td><td></td>',
+      $tr[0],$tr[1],$tr[2],$tr[3],$tr[4]);
+}
+
 foreach($sTests as $Row){
-   if (($Row[TEST_WEIGHT]<=0)){ continue; }
-   printf('<tr>'); echo "\n";
+   if ($Row[TEST_WEIGHT]<=0){ continue; }
+
    $Link = $Row[TEST_LINK];
    $Name = $Row[TEST_NAME];
 
    if (isset($data[$Link])){
       $v = $data[$Link];
 
-      printf('<td><a href="benchmark.php?test=%s&amp;lang=%s&amp;id=%d">%s</a></td>', 
-         $Link, $SelectedLang, $v[N_ID], $Name);
-
       if ($v[N_LINES] >= 0){
-      
-         if ($v[N_N]==0){ $n = '<td></td>'; } 
-         else { $n = '<td><span class="numN">&nbsp;'.number_format($v[N_N]).'</span></td>'; }
+         printf('<tr><td><a href="benchmark.php?test=%s&amp;lang=%s&amp;id=%d">%s</a></td>',
+            $Link, $SelectedLang, $v[N_ID], $Name);
+
+         if ($v[N_N]==0){ $n = '<td></td>';
+         } else { $n = '<td><span class="numN">&nbsp;'.number_format($v[N_N]).'</span></td>'; }
 
          if ($Name=='startup'){ $kb = 1.0; } else { $kb = $v[N_MEMORY]; }
 
-         printf('%s%s%s%s',
-            PF($v[N_FULLCPU]), PF($kb), PF($v[N_GZ]), $n);
-
-      } else {
-         if ($v[N_LINES] == NO_COMPARISON){
-            $message = 'No '.$Langs[$v[N_LANG]][LANG_NAME]; 
-            printf('<td>&nbsp;</td><td>&nbsp;</td><td colspan="2"><span class="message">%s</span></td>', $message);
-         } else {
-            $message = StatusMessage($v[N_LINES]);
-            printf('<td><span class="message">%s</span></td><td></td><td></td><td></td>', $message);
-         }
+         printf('%s%s%s%s</tr>', PF($v[N_FULLCPU]), PF($kb), PF($v[N_GZ]), $n);
       }
-
-   } else {
-      printf('<td><a href="benchmark.php?test=%s&amp;lang=%s">%s</a></td>', 
-         $Link, $SelectedLang, $Name); echo "\n";
-      $message = 'No&nbsp;program';
-      printf('<td><span class="message">%s</span></td><td></td><td></td><td></td>', $message); 
    }
-   echo "</tr>\n";
 }
-?>   
+?>
 </table>
 
 <h3><a href="#about" name="about">&nbsp;about <?=$LangName;?></a></h3>
