42618d00dc70ae6d057029cc6114a9cf38499856
igouy-guest <igouy-guest>
2009-02-26 02:09:49 +0000

    *** empty log message ***

--- website/about/php-about.tpl.php
+++ website/about/php-about.tpl.php
@@ -1,3 +1,4 @@
 <?=$Version;?>
+<p><a href="http://talks.php.net/show/drupal08/7">Overall Performance: PHP is rarely the bottleneck (HTML slides)</a></p>
 <p>Home Page: <a href="http://www.php.net/">http://www.php.net/</a></p>
 <p>Download: <a href="http://www.php.net/downloads.php">http://www.php.net/downloads.php</a></p>
