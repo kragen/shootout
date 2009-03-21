288cfc3fb36a49388842284ee98855921ba19d29
igouy-guest <igouy-guest>
2009-03-21 15:56:05 +0000

    *** empty log message ***

--- website/websites/u64/version/hipe-version.php
+++ website/websites/u64/version/hipe-version.php
@@ -1,1 +1,1 @@
-<p>Erlang (BEAM) emulator version 5.6.5 [source] [64-bit] [async-threads:0] [hipe] [kernel-poll:false]</p>
+<p>Erlang R13A (erts-5.7) [source] [64-bit] [rq:1] [async-threads:0] [hipe] [kernel-poll:false]</p>
