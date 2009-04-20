d9839ff24e6c71aa4af223604f2ddb06dea6a10e
igouy-guest <igouy-guest>
2009-04-20 16:32:57 +0000

    *** empty log message ***

---
+++ website/about/pypy-about.tpl.php
@@ -0,0 +1,4 @@
+<?=$Version;?>
+<p>Online book: <a href="http://www.diveintopython.org/">Dive Into Python</a></p>
+<p>Home Page: <a href="http://codespeak.net/pypy/dist/pypy/doc/index.html">PyPy[index]</a></p>
+<p>Download: <a href="http://codespeak.net/pypy/dist/pypy/doc/getting-started.html#svn-check-out">Svn-check out</a></p>
