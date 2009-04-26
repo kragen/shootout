7e5aee680b27b3417f75d871f2fdb9035325d34e
igouy-guest <igouy-guest>
2009-04-26 18:30:32 +0000

    *** empty log message ***

--- website/websites/u64q/version/hipe-version.php
+++ website/websites/u64q/version/hipe-version.php
@@ -1,1 +1,1 @@
-<p>Erlang R13A (erts-5.7) [source] [64-bit] [smp:4:4] [rq:4] [async-threads:0] [hipe] [kernel-poll:false]</p>
+<p>Erlang R13B (erts-5.7.1) [source] [64-bit] [smp:4:4] [rq:4] [async-threads:0] [hipe] [kernel-poll:false]</p>
