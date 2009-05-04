727771dd6948de8f367f8844fa39485585514ba8
igouy-guest <igouy-guest>
2009-05-04 20:08:32 +0000

    *** empty log message ***

--- website/about/ats-about.tpl.php
+++ website/about/ats-about.tpl.php
@@ -1,4 +1,4 @@
 <?=$Version;?>
 <p>Home Page: <a href="http://www.ats-lang.org/">ATS -- unleashing the potential of types!</a></p>
-<p>Download: <a href="http://sourceforge.net/project/showfiles.php?group_id=205722">anairiats-0.0.1</a></p>
+
 
