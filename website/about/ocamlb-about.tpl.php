0075276b0dc397658796b06425f55520bd14075e
igouy-guest <igouy-guest>
2009-05-23 00:25:33 +0000

    *** empty log message ***

--- website/about/ocamlb-about.tpl.php
+++ website/about/ocamlb-about.tpl.php
@@ -1,3 +1,3 @@
 <?=$Version;?>
-<p>Home Page: <a href="http://camlr.inria.fr/">http://www.ocaml.org/</a></p>
-<p>Download: <a href="http://caml.inria.fr/ocaml/distrib.html">http://caml.inria.fr/ocaml/distrib.html</a></p>
+<p>Home Page: <a href="http://caml.inria.fr/">http://www.ocaml.org/</a></p>
+<p>Download: <a href="http://caml.inria.fr/download.en.html">http://caml.inria.fr/download.en.html</a></p>
