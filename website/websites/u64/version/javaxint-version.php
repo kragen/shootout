6d4e095857483651ab085fa0c40da9a8196925a0
igouy-guest <igouy-guest>
2009-07-20 14:01:12 +0000

    *** empty log message ***

--- website/websites/u64/version/javaxint-version.php
+++ website/websites/u64/version/javaxint-version.php
@@ -1,4 +1,4 @@
-<p>java version "1.6.0_10"<br/>
-Java(TM) SE Runtime Environment (build 1.6.0_10-b33)<br/>
-Java HotSpot(TM) 64-Bit Server VM (build 11.0-b15, interpreted mode)
+<p>java version "1.6.0_14"<br/>
+Java(TM) SE Runtime Environment (build 1.6.0_14-b08)<br/>
+Java HotSpot(TM) 64-Bit Server VM (build 14.0-b16, interpreted mode)
 </p>
