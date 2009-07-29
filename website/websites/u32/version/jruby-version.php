3102d3bb23e3a74d820d2353bc0beb65c8620604
igouy-guest <igouy-guest>
2009-07-29 01:54:07 +0000

    *** empty log message ***

--- website/websites/u32/version/jruby-version.php
+++ website/websites/u32/version/jruby-version.php
@@ -1,1 +1,1 @@
-<p>jruby 1.2.0 (ruby 1.8.6 patchlevel 287) (2009-03-16 rev 9419) [i386-java]</p>
+<p>jruby 1.3.1 (ruby 1.8.6p287) (2009-06-15 2fd6c3d) (Java HotSpot(TM) Server VM 1.6.0_14) [i386-java]</p>
--- website/websites/u64q/version/jruby-version.php
+++ website/websites/u64q/version/jruby-version.php
@@ -1,1 +1,1 @@
-<p>jruby 1.2.0 (ruby 1.8.6 patchlevel 287) (2009-03-16 rev 9419) [amd64-java]</p>
+<p>jruby 1.3.1 (ruby 1.8.6p287) (2009-06-15 2fd6c3d) (Java HotSpot(TM) 64-Bit Server VM 1.6.0_14) [amd64-java]</p>
