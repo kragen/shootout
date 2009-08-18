b0dd29b6120f352a83b0c5a8c13d5f677b174ff6
igouy-guest <igouy-guest>
2009-08-18 16:40:56 +0000

    *** empty log message ***

--- website/websites/u64/config.php
+++ website/websites/u64/config.php
@@ -34,7 +34,7 @@ define('DEV',TRUE);                // show excluded languages, tests, programs, 
 define('HOMEPAGE_ROWS',0);         // show N rows of languages on the homepage
 
 
-define('SITE_NAME', 'u64q');
+define('SITE_NAME', 'u64');
 define('SITE_TITLE','x64 Ubuntu : Intel&#174; Q6600&#174; one core Computer Language Benchmarks Game');
 define('BANNER_TITLE','x64&nbsp;Ubuntu&nbsp;:&nbsp;Intel&#174;&nbsp;Q6600&#174;&nbsp;one&nbsp;core<br/>Computer&nbsp;Language&nbsp;Benchmarks&nbsp;Game&nbsp;');
 define('FAQ_TITLE','Read&nbsp;the&nbsp;FAQ!');
