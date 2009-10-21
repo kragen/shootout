fc3f9719a73b2324ce5699a76dc8947f9bb3fc54
igouy-guest <igouy-guest>
2009-10-21 13:07:21 +0000

    *** empty log message ***

--- website/websites/u64/version/jruby-version.php
+++ website/websites/u64/version/jruby-version.php
@@ -1,1 +1,1 @@
-<p>jruby 1.3.1 (ruby 1.8.6p287) (2009-06-15 2fd6c3d) (Java HotSpot(TM) 64-Bit Server VM 1.6.0_14) [amd64-java]</p>
+<p>jruby 1.4.0RC1 (ruby 1.8.7 patchlevel 174) (2009-09-30 80c263b) (Java HotSpot(TM) 64-Bit Server VM 1.6.0_14) [amd64-java]</p>
