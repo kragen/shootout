ca4eedbd0f3f289a074942a0e1ae09d0f91657d5
igouy-guest <igouy-guest>
2009-10-24 00:35:45 +0000

    *** empty log message ***

--- website/lib/headtohead.tpl.php
+++ website/lib/headtohead.tpl.php
@@ -35,7 +35,7 @@ $ShortName2 = $Langs[$SelectedLang2][LANG_NAME];
 <table>
 <colgroup span="1" class="txt"></colgroup>
 <colgroup span="4" class="num"></colgroup>
-<tr><th colspan="5"><a href="#ratio" name="ratio"><sup><?=$LangName;?></sup>&nbsp;/&nbsp;<sub><?=$LangName2;?></sub></a> <sup>[smaller is better]</sup></th></tr>
+<tr><th colspan="5"><a href="#ratio" name="ratio"><sup><?=$LangName;?></sup>&nbsp;/&nbsp;<sub><?=$LangName2;?></sub></a> <sup>[rounded - smaller is better]</sup></th></tr>
 
 <tr>
 <th>Programs</th>
