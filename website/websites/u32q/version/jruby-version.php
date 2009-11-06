1fae3108e52d559b0b2695fd04decc1fe2d82ad1
igouy-guest <igouy-guest>
2009-11-06 18:01:54 +0000

    *** empty log message ***

--- website/websites/u32q/version/jruby-version.php
+++ website/websites/u32q/version/jruby-version.php
@@ -1,1 +1,2 @@
-<p>jruby 1.3.1 (ruby 1.8.6p287) (2009-06-15 2fd6c3d) (Java HotSpot(TM) Server VM 1.6.0_14) [i386-java]</p>
+<p>jruby 1.4.0 (ruby 1.8.7 patchlevel 174) (2009-11-02 69fbfa3) (Java HotSpot(TM) Client VM 1.6.0_16) [i386-java]
+</p>
