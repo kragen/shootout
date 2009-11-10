416f88fa85318f0a2433d1e0cab2a4cf070f3e78
igouy-guest <igouy-guest>
2009-11-10 17:30:14 +0000

    *** empty log message ***

--- website/about/programs/meteor-javasteady-2-about.tpl.php
+++ website/about/programs/meteor-javasteady-2-about.tpl.php
@@ -1,6 +1,6 @@
 <p>The reported "*Java 6 steady state" program CPU secs and program Elapsed secs are <strong>averages</strong> that approximate steady state performance.</p>
 
-<p>Each program performs the same calculation 20 times, the measurements are made in the same way as all the other time measurements, but before display they are divided by 20 to give averages that minimize the influence of mixed-mode method interpretation and <a href="http://www.ibm.com/developerworks/java/library/j-benchmark1.html#osr"><strong>on-stack-replacement</strong></a>.</p>
+<p>Each program performs the same calculation 66 times, the measurements are made in the same way as all the other time measurements, but before display they are divided by 66 to give averages that minimize the influence of mixed-mode method interpretation and <a href="http://www.ibm.com/developerworks/java/library/j-benchmark1.html#osr"><strong>on-stack-replacement</strong></a>.</p>
 
 <p><a href="faq.php#dynamic"><strong>FAQ: What about Java dynamic compilation?</strong></a></p>
 
