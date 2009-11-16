4f579046f5d9c9973b9c079d29a0ea7a14cff01f
igouy-guest <igouy-guest>
2009-11-16 04:36:01 +0000

    *** empty log message ***

--- website/about/scorecard-about.tpl.php
+++ website/about/scorecard-about.tpl.php
@@ -1,11 +1,10 @@
-<p>For each benchmark, <strong>B</strong> is the best measurement; for each language implementation, the best measurement <strong>L</strong> is converted to the <em>ratio</em> <strong><sup>L</sup>/<sub>B</sub></strong></p>
+<p><br/>Read <a href="http://portal.acm.org/citation.cfm?id=5666.5673"><strong>How not to lie with statistics: the correct way to summarize benchmark results (pdf)</strong></a>.</p>
 
-<p>The ratio can be adjusted by a measurement multiplier (make CPU time or memory use or
-lines-of-code more or less important) and by a benchmark weight (make particular benchmarks more or less important).</p>
+<p>For each benchmark, <strong>B</strong> is the best measurement; for each language implementation, the best measurement <strong>L</strong> is then normalized to <strong><sup>L</sup>/<sub>B</sub></strong></p>
 
-<p>For each language implementation, we report the <a href="http://portal.acm.org/citation.cfm?id=5666.5673"><strong>Weighted Geometric Mean</strong></a> of those <strong><sup>L</sup>/<sub>B</sub></strong> ratios.</p>
+<p><strong>GM</strong> is the Weighted Geometric Mean of those <strong><sup>L</sup>/<sub>B</sub></strong> ratios.
 
-<p>(Language implementations with more than a couple of Timeouts distort the ranking - so they have been excluded. They can still be <a href="benchmark.php?test=all&amp;lang=gpp&amp;lang2=java"><strong>compared directly</strong></a> against another language implementation.)</p>
+<p><strong>missing</strong> : Language implementations with more than a couple of Timeouts distort the ranking - so they have been excluded. They can still be <a href="benchmark.php?test=all&amp;lang=gpp&amp;lang2=java"><strong>compared directly</strong></a> against another language implementation.</p>
 
 
 
