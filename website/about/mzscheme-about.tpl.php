564fd8374ba5b118864dad2ac5ac5659df46c9c4
igouy-guest <igouy-guest>
2009-11-19 04:53:00 +0000

    *** empty log message ***

--- website/about/mzscheme-about.tpl.php
+++ website/about/mzscheme-about.tpl.php
@@ -1,3 +1,4 @@
+<p>online book <a href="http://www.scheme.com/tspl4/">"The Scheme Programming Language, Fourth Edition" by R. Kent Dybvig, 2009.</a></p>
 <?=$Version;?>
 <p>Home Page: <a href="http://www.plt-scheme.org">PLT Scheme</a></p>
 <p>Download: <a href="http://download.plt-scheme.org/mzscheme/">Download MzScheme</a></p>
