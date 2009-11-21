c9166a2a35296f2297e5d1d06ea85629fef4573f
igouy-guest <igouy-guest>
2009-11-21 06:02:19 +0000

    *** empty log message ***

--- website/about/tracemonkey-about.tpl.php
+++ website/about/tracemonkey-about.tpl.php
@@ -1,7 +1,3 @@
-<p>ECMAScript standard <a href="http://www.mozilla.org/js/language/E262-3.pdf">"ECMA-262 Edition 3"</a> 1.2MB pdf</p>
-
-<p><a href="http://developer.mozilla.org/En/SpiderMonkey/Internals/Tracing_JIT">Tracing JIT</a></p>
-
 <?=$Version;?>
 
-<p>Home Page: <a href="https://wiki.mozilla.org/JavaScript:TraceMonkey">mozilla wiki JavaScript:TraceMonkey</a></p>
+<p>Home Page: <a href="https://developer.mozilla.org/En/SpiderMonkey/Introduction_to_the_JavaScript_shell">Introduction to the JavaScript shell</a></p>
