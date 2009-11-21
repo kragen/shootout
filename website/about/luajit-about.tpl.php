0f01f1c4d8c189ed3fd37225a2d95dc8a89ec885
igouy-guest <igouy-guest>
2009-11-21 20:48:28 +0000

    *** empty log message ***

--- website/about/luajit-about.tpl.php
+++ website/about/luajit-about.tpl.php
@@ -1,3 +1,5 @@
+<p><a href="http://lua-users.org/lists/lua-l/2009-11/msg00089.html">LuaJIT 2.0 intellectual property disclosure and research opportunities</a></p>
+
 <?=$Version;?>
 <p>Home Page: <a href="http://luajit.org/">The LuaJIT Project</a></p>
 <p>Download: <a href="http://luajit.org/download.html">LuaJIT Download</a></p>
