e7843bcf123e6ad5959805cb63ee47cc1bf8968f
igouy-guest <igouy-guest>
2009-11-21 06:52:53 +0000

    *** empty log message ***

--- website/about/tracemonkey-about.tpl.php
+++ website/about/tracemonkey-about.tpl.php
@@ -1,3 +1,7 @@
+<p><a href="http://people.mozilla.org/~dmandelin/tracemonkey-pldi-09.pdf">"Trace-based Just-in-Time Type Specialization for Dynamic Languages"</a> pdf</p>
+
 <?=$Version;?>
 
 <p>Home Page: <a href="https://developer.mozilla.org/En/SpiderMonkey/Introduction_to_the_JavaScript_shell">Introduction to the JavaScript shell</a></p>
+
+<p>Download: <a href="https://developer.mozilla.org/En/SpiderMonkey/Build_Documentation">Building SpiderMonkey tip</a></p>
