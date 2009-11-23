96e91786cde6012ac367104d45de30469088b08d
igouy-guest <igouy-guest>
2009-11-23 00:59:09 +0000

    *** empty log message ***

--- website/about/shapes-about.tpl.php
+++ website/about/shapes-about.tpl.php
@@ -2,3 +2,6 @@
 <p>Both axes use logarithmic scaling to show the full range of measurements, although there's at least two orders of magnitude more difference between program Time-used than program Code-used.</p>
 
 <p>Thanks to Guillaume Marceau for demonstrating there was interest in this kind of presentation.</p>
+
+
+<p><br/>There are other ways to analyse and present this data: ask <a href="fastest.php?d=<?=$DataSet?>">Which programming language implementations have the fastest programs?</a> or <a href="fun.php?d=<?=$DataSet?>">Which are best for you?</a> or take the <a href="summarydata.php?d=<?=$DataSet?>">Summary Data</a> and do your own analysis!</p>
