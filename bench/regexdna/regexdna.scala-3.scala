0a41fe53a68ddcd5de8d4b14e934077ace53b148
igouy-guest <igouy-guest>
2009-12-05 22:50:26 +0000

    *** empty log message ***

--- bench/regexdna/regexdna.scala-3.scala
+++ bench/regexdna/regexdna.scala-3.scala
@@ -18,10 +18,12 @@ object regexdna
       var input      = Source fromInputStream(System.in, "US-ASCII") mkString
       val init_len   = input length
 
+
       // strip header & newline
       input         = ">.*\n|\n".r replaceAllIn(input, "")
       val strip_len   = input length
 
+      // counting patterns
       val patterns   = Array(
          "agggtaaa|tttaccct" ,
          "[cgt]gggtaaa|tttaccc[acg]",
@@ -34,40 +36,40 @@ object regexdna
          "agggtaa[cgt]|[acg]ttaccct")
 
       // queue tasks, each task is handled in a separate thread
-      val replace_result   = future(Replace(input))
-      val count_results   = patterns map( pt => future(pt.r.findAllIn(input).toList.length) )
+      val count_results   = patterns map( pt => future(pt.r.findAllIn(input).collect.length) )
+
+      // replace IUB
+      val replace_result   = future(      
+      {
+         val iub = HashMap(
+            'B' -> "(c|g|t)",
+            'D' -> "(a|g|t)",
+            'H' -> "(a|c|t)",
+            'K' -> "(g|t)",
+            'M' -> "(a|c)",
+            'N' -> "(a|c|g|t)",
+            'R' -> "(a|g)",
+            'S' -> "(c|g)",
+            'V' -> "(a|c|g)",
+            'W' -> "(a|t)",
+            'Y' -> "(c|t)"  )
+      
+         val buffer   = new StringBuffer((input.length * 2) >>> 1) // input.len * 1.5
+         val matcher   = Pattern compile "[BDHKMNRSVWY]" matcher input
+
+         while ( matcher find )
+            matcher appendReplacement( buffer, iub(input(matcher start))  )
+
+         matcher appendTail buffer
+         buffer length
+      })
+      
 
       // print results
       for ((pt, cres) <- patterns zip count_results)
-         printf( "%s\t%d\n", pt, cres() )
+         printf( "%s %d\n", pt, cres() )
 
       printf( "\n%d\n%d\n%d\n", init_len, strip_len, replace_result() )
    }
- 
-   @inline
-   private final
-   def Replace(input: String) : Int =
-   {
-      val iub = HashMap(
-         'B' -> "(c|g|t)",
-         'D' -> "(a|g|t)",
-         'H' -> "(a|c|t)",
-         'K' -> "(g|t)",
-         'M' -> "(a|c)",
-         'N' -> "(a|c|g|t)",
-         'R' -> "(a|g)",
-         'S' -> "(c|g)",
-         'V' -> "(a|c|g)",
-         'W' -> "(a|t)",
-         'Y' -> "(c|t)"  )
-
-      val buffer   = new StringBuffer((input.length * 2) >>> 1) // input.len * 1.5
-      val matcher   = Pattern compile "[BDHKMNRSVWY]" matcher input
-
-      while ( matcher find )
-         matcher appendReplacement ( buffer, iub(input(matcher start) )  )
-
-      matcher appendTail buffer
-      buffer length
-   }
 }
+
