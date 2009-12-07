32c03fd8bd6dea1b2ee6379046c9d000638db206
igouy-guest <igouy-guest>
2009-12-07 18:29:30 +0000

    *** empty log message ***

--- website/websites/gp4sandbox/iofile.php
+++ website/websites/gp4sandbox/iofile.php
@@ -1,13 +1,15 @@
+<?php 
+header("HTTP/1.1 301 Moved Permanently");
+header("Location: http://shootout.alioth.debian.org/index.php");
+exit;
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" >
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<meta name="robots" content="noindex,noarchive" />
-<title>The Computer Language Shootout Benchmarks</title>
-<meta http-equiv="refresh" content="3;url=http://shootout.alioth.debian.org/">
+<title>The Computer Language Benchmarks Game</title>
 </head>
 <body>
-<p>Redirecting in 3 seconds</p> 
-<h3><a href="http://shootout.alioth.debian.org/">http://shootout.alioth.debian.org/</a></h3>
+<h3><a href="http://shootout.alioth.debian.org/index.php">The Computer Language Benchmarks Game</a></h3>
 </body>
 </html>
