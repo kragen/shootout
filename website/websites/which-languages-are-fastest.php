f654b748ff7c232a12869d311dd0ecf5a51cd955
igouy-guest <igouy-guest>
2009-12-07 12:42:05 +0000

    *** empty log message ***

--- website/websites/which-languages-are-fastest.php
+++ website/websites/which-languages-are-fastest.php
@@ -1,10 +1,14 @@
+<?php 
+header("HTTP/1.1 302 Found");
+header("Location: http://shootout.alioth.debian.org/u64/which-languages-are-fastest.php");
+exit;
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" >
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <meta name="robots" content="noindex,noarchive" />
 <title>The Computer Language Benchmarks Game</title>
-<meta http-equiv="refresh" content="url=http://shootout.alioth.debian.org/u64/which-languages-are-fastest.php">
 </head>
 <body>
 <h3><a href="http://shootout.alioth.debian.org/u64/which-languages-are-fastest.php">which-languages-are-fastest.php</a></h3>
