ffbd7879be6aaf86eb18cb2e3314a90a3072950b
igouy-guest <igouy-guest>
2009-12-08 17:05:36 +0000

    *** empty log message ***

--- website/about/luajit-about.tpl.php
+++ website/about/luajit-about.tpl.php
@@ -1,3 +1,5 @@
+<p><strong><a href="http://lua-users.org/lists/lua-l/2009-12/msg00254.html">LuaJIT x64 port sponsorship program</a></strong></p>
+
 <p><a href="http://lua-users.org/lists/lua-l/2009-11/msg00089.html">LuaJIT 2.0 intellectual property disclosure and research opportunities</a></p>
 
 <?=$Version;?>
