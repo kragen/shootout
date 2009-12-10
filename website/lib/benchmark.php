577c202967620dd58ef2a7f84358a551bcc0dc21
igouy-guest <igouy-guest>
2009-12-10 00:55:39 +0000

    *** empty log message ***

--- website/lib/benchmark.php
+++ website/lib/benchmark.php
@@ -190,7 +190,7 @@ if ($T=='all'){
       if (! file_exists(ABOUT_PATH.$AboutTemplateName)){ $AboutTemplateName = 'blank-about.tpl.php'; }
       $Body->set('Data', WhiteListSelected(DATA_PATH.'data.csv', $T, $Incl) );
       $metaRobots = '<meta name="robots" content="index,nofollow,noarchive" />';
-      $MetaKeywords = '<meta name="description" content="'Performance measurements and source code for ~30 different programming languages to '.$Tests[$T][TEST_META].'. Measured on '.PLATFORM_NAME.'.'" />';
+      $MetaKeywords = '<meta name="description" content="'Performance measurements and source code in ~30 different programming languages to '.$Tests[$T][TEST_META].'. Measured on '.PLATFORM_NAME.'.'" />';
 
 
    } else {              // Program
