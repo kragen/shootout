ac6e9fb6be9124253212efa8c156465711d71ba7
igouy-guest <igouy-guest>
2009-12-11 20:21:15 +0000

    *** empty log message ***

--- website/lib/page.tpl.php
+++ website/lib/page.tpl.php
@@ -19,7 +19,7 @@ header("Expires: " . gmdate("D, d M Y H:i:s", $s + (8*3600)) . " GMT");
 <?=$MetaKeywords;?>
 
 <title><?=$PageTitle;?></title>
-<link rel="stylesheet" type="text/css" href="<?=IMAGE_PATH;?>benchmark_css_10dec2009.php" />
+<link rel="stylesheet" type="text/css" href="<?=IMAGE_PATH;?>benchmark_css_11dec2009.php" />
 <link rel="shortcut icon" href="./favicon.ico" />
 </head>
 
