8c7f8bee8f451c9c6e0f6b6cc85a518ba97b66fa
igouy-guest <igouy-guest>
2009-12-12 19:14:35 +0000

    *** empty log message ***

--- website/websites/index.php
+++ website/websites/index.php
@@ -67,7 +67,7 @@ We can't - <strong>we measure particular programs</strong>.</p><br/>
 
 <h5><a href="http://shootout.alioth.debian.org/<?=$ChosenSite;?><?=$ChosenUrl;?>"><strong>Which programming languages have the fastest programs?</strong></a></h5><br/>
 
-<p>There are 4 sets of up-to-date measurements. Click one of these <br/>color-code links to see one benchmark for a particular OS/machine -</p><br/>
+<p>There are 4 sets of up-to-date measurements. Click one of these <br/>color-code links to see one benchmark for a particular OS/machine -</p>
 
 <?
 $choices = array(
