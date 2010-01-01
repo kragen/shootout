9e52594272dbf25b55a6823de72b8e5db8015865
igouy-guest <igouy-guest>
2010-01-01 18:26:45 +0000

    *** empty log message ***

--- website/lib/shapes.tpl.php
+++ website/lib/shapes.tpl.php
@@ -113,12 +113,12 @@
             $k = $cols[$col][$row];
 
             if (isset($Langs[$k][LANG_SPECIALURL]) && !empty($Langs[$k][LANG_SPECIALURL])){
-               printf('<a href="%s.php" title="Compare %s size and speed against one other programming language">', $Langs[$k][LANG_SPECIALURL], $Langs[$k][LANG_FULL]);
+               printf('<a href="%s.php" title="Is %s the fastest programming language?">', $Langs[$k][LANG_SPECIALURL], $Langs[$k][LANG_FULL]);
             } else {
-               printf('<a href="benchmark.php?test=all&amp;lang=%s" title="Compare %s size and speed against one other programming language">', $k, $Langs[$k][LANG_FULL]);
+               printf('<a href="benchmark.php?test=all&amp;lang=%s" title="Is %s the fastest programming language?">', $k, $Langs[$k][LANG_FULL]);
             }
 
-            printf('<img src="chartshape.php?w=%s&amp;s=%s&amp;c=%s" alt="source code size versus speed of %s benchmark programs" title="Is %s the faster programming language?" width="150" height="120" />',
+            printf('<img src="chartshape.php?w=%s&amp;s=%s&amp;c=%s" alt="source code size versus speed of %s benchmark programs" title="Compare %s size and speed against one other programming language" width="150" height="120" />',
                Encode($k), Encode($Shapes[$k]), Encode($Centers[$k]), $Langs[$k][LANG_FULL], $Langs[$k][LANG_FULL] );
 
             printf('</a>');
