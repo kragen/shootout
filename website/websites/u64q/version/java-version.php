cdc042161b55c66835d17cb9b56252519f1e40c2
igouy-guest <igouy-guest>
2010-01-28 01:09:28 +0000

    *** empty log message ***

--- website/websites/u64q/version/java-version.php
+++ website/websites/u64q/version/java-version.php
@@ -1,4 +1,4 @@
-<p>java version "1.6.0_13"<br/>
-Java(TM) SE Runtime Environment (build 1.6.0_13-b03)<br/>
-Java HotSpot(TM) 64-Bit Server VM (build 11.3-b02, mixed mode)
+<p>java version "1.6.0_18"<br/>
+Java(TM) SE Runtime Environment (build 1.6.0_18-b07)<br/>
+Java HotSpot(TM) 64-Bit Server VM (build 16.0-b13, mixed mode)
 </p>
--- website/websites/u64q/version/javaxint-version.php
+++ website/websites/u64q/version/javaxint-version.php
@@ -1,4 +1,4 @@
-<p>java version "1.6.0_10"<br/>
-Java(TM) SE Runtime Environment (build 1.6.0_10-b33)<br/>
-Java HotSpot(TM) 64-Bit Server VM (build 11.0-b15, interpreted mode)
+<p>java version "1.6.0_18"<br/>
+Java(TM) SE Runtime Environment (build 1.6.0_18-b07)<br/>
+Java HotSpot(TM) 64-Bit Server VM (build 16.0-b13, interpreted mode)
 </p>
