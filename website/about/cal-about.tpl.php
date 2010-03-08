0108a9eed9cf35d15652da57cd23500027f7df72
igouy-guest <igouy-guest>
2010-03-08 19:16:50 +0000

    *** empty log message ***

--- website/about/cal-about.tpl.php
+++ website/about/cal-about.tpl.php
@@ -1,5 +1,4 @@
 <?=$Version;?>
-<p>Home Page: <a href="http://labs.businessobjects.com/cal/">The Open Quark Framework for Java
-and the CAL Language</a></p>
-<p>Download: <a href="http://labs.businessobjects.com/cal/#Software">Download Summary</a></p>
+<p>Home Page: <a href="http://openquark.org/Open_Quark/Welcome.html">Open Quark Framework featuring the CAL Language</a><br/>The Java framework for lazy functional programming.</p>
+<p>Download: <a href="http://openquark.org/Open_Quark/Download.html">Download</a></p>
 
