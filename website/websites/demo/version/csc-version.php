a2835cadbc059f737ef3efe38a070ec7a505f419
igouy-guest <igouy-guest>
2010-04-01 02:32:36 +0000

    *** empty log message ***

---
+++ website/websites/demo/version/csc-version.php
@@ -0,0 +1,2 @@
+<p>Microsoft (R) Visual C# 2008 Compiler version 3.5.30729.1<br/>
+for Microsoft (R) .NET Framework version 3.5</p>
---
+++ website/websites/demo/version/fsc-version.php
@@ -0,0 +1,1 @@
+<p>Microsoft (R) F# 2.0 Compiler build 1.9.9.9</p>
