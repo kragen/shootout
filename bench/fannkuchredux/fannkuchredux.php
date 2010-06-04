555331b79f0b59d371ed94b34e413c654a2b839b
igouy-guest <igouy-guest>
2010-06-04 07:16:54 +0000

    *** empty log message ***

--- bench/fannkuchredux/fannkuchredux.php
+++ bench/fannkuchredux/fannkuchredux.php
@@ -1,72 +1,53 @@
 <? /* The Computer Language Benchmarks Game
    http://shootout.alioth.debian.org/
    
-   contributed by Isaac Gouy
+   contributed by Isaac Gouy, transliterated from Mike Pall's Lua program 
 */
 
 function Fannkuch($n){
-   $check = 0;
-   $perm = array();
-   $perm1 = array();
-   $count = array();
-   $maxPerm = array();
-   $maxFlipsCount = 0;
-   $m = $n - 1;
-   $permCount = 0;
-   $checksum = 0;
-
-   for ($i=0; $i<$n; $i++) $perm1[$i] = $i;
-   $r = $n;
-
-   while (TRUE) {
-
-      while ($r != 1){ $count[$r-1] = $r; $r--; } 
-       
-      for($i=0; $i<$n; $i++) $perm[$i] = $perm1[$i]; 
-      $flipsCount = 0;
-
-      while ( !(($k=$perm[0]) == 0) ) {
-         $k2 = ($k+1) >> 1;
-         for($i=0; $i<$k2; $i++) {
-            $temp = $perm[$i]; $perm[$i] = $perm[$k-$i]; $perm[$k-$i] = $temp;
-         }
-         $flipsCount++;
+   $p = $q = $s = array(); 
+   $sign = 1; $maxflips = $sum = 0; $m = $n-1;
+   for ($i=0; $i<$n; $i++){ $p[$i] = $i; $q[$i] = $i; $s[$i] = $i; }      
+   do {
+      // Copy and flip.
+      $q0 = $p[0];                                          // Cache 0th element.
+      if ($q0 != 0){
+         for($i=1; $i<$n; $i++) $q[$i] = $p[$i];            // Work on a copy.
+         var $flips = 1;
+         do { 
+            $qq = $q[$q0]; 
+            if ($qq == 0){                                  // ... until 0th element is 0.
+               $sum += $sign*$flips;
+	       if ($flips > $maxflips) $maxflips = $flips;  // New maximum?
+               break; 
+            } 
+ 	    $q[$q0] = $q0; 
+	    if ($q0 >= 3){
+	       $i = 1; $j = $q0 - 1;
+               do { $t = $q[$i]; $q[$i] = $q[$j]; $q[$j] = $t; $i++; $j--; } while ($i < $j); 
+            }
+	    $q0 = $qq; $flips++;
+         } while (true); 
       }
-
-      if ($flipsCount > $maxFlipsCount) {
-         $maxFlipsCount = $flipsCount;
-         for($i=0; $i<$n; $i++) $maxPerm[$i] = $perm1[$i];
-      }
-
-      $checksum += ($permCount % 2 == 0) ? $flipsCount : -$flipsCount;  
- 
-
-      while (TRUE) {        
-         if ($r == $n){
-            echo $checksum, "\n";
-            return $maxFlipsCount;
+      // Permute.
+      if ($sign == 1){
+         $t = $p[1]; $p[1] = $p[0]; $p[0] = $t; $sign = -1; // Rotate 0<-1.
+      } else { 
+         $t = $p[1]; $p[1] = $p[2]; $p[2] = $t; $sign = 1;  // Rotate 0<-1 and 0<-1<-2.
+         for($i=2; $i<$n; $i++){ 
+	    $sx = $s[$i];
+	    if ($sx != 0){ $s[$i] = $sx-1; break; }
+	    if ($i == $m) return array(sum,maxflips);       // Out of permutations.
+	    $s[$i] = $i;
+	    // Rotate 0<-...<-i+1.
+	    $t = $p[0]; for($j=0; $j<=$i; $j++){ $p[$j] = $p[$j+1]; } $p[$i+1] = $t;
          }
-         $perm0 = $perm1[0];
-         $i = 0;
-         while ($i < $r) {
-            $j = $i + 1;
-            $perm1[$i] = $perm1[$j];
-            $i = $j;
-         }
-         $perm1[$r] = $perm0;
-
-         $count[$r] = $count[$r] - 1;
-         if ($count[$r] > 0) break;
-         $r++;
       }
-
-      $permCount++;
-      if ($permCount == 1048576){ $permCount = 0; }
-
-   }
+   } while (true);
 }
 
 $n = $argv[1];
-printf("Pfannkuchen(%d) = %d", $n, Fannkuch($n));
+list($checksum,$pf) = Fannkuch($n);
+printf("(%d)\nPfannkuchen(%d) = %d", $checksum, $n, $pf);
 ?>
 
