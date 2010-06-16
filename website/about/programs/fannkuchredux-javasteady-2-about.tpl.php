793757c5f8618a492794430b4232e53c8bd9d132
igouy-guest <igouy-guest>
2010-06-16 16:24:20 +0000

    *** empty log message ***

--- website/about/programs/fannkuchredux-javasteady-2-about.tpl.php
+++ website/about/programs/fannkuchredux-javasteady-2-about.tpl.php
@@ -5,134 +5,41 @@
 <p><a href="<?=CORE_SITE;?>help.php#java"><strong>FAQ: What about Java?</strong></a></p>
 
 <pre>
-$ java -server -XX:+PrintCompilation -XX:-PrintGC binarytrees_test 20 10
-  1       java.lang.Object::<init> (1 bytes)
-  2       binarytrees_test$TreeNode::bottomUpTree (42 bytes)
-  3       binarytrees_test$TreeNode::<init> (10 bytes)
-  4       binarytrees_test$TreeNode::<init> (20 bytes)
-  5       binarytrees_test$TreeNode::itemCheck (33 bytes)
-stretch tree of depth 21	 check: -1
-  6       binarytrees_test$TreeNode::access$000 (6 bytes)
-  7       binarytrees_test$TreeNode::access$100 (5 bytes)
-  1%      binarytrees_test::program_main @ 108 (240 bytes)
-2097152	 trees of depth 4	 check: -2097152
-524288	 trees of depth 6	 check: -524288
-131072	 trees of depth 8	 check: -131072
-32768	 trees of depth 10	 check: -32768
-8192	 trees of depth 12	 check: -8192
-2048	 trees of depth 14	 check: -2048
-512	 trees of depth 16	 check: -512
-128	 trees of depth 18	 check: -128
-32	 trees of depth 20	 check: -32
-long lived tree of depth 20	 check: -1
-21.318
-  8       binarytrees_test::program_main (240 bytes)
-stretch tree of depth 21	 check: -1
-2097152	 trees of depth 4	 check: -2097152
-524288	 trees of depth 6	 check: -524288
-131072	 trees of depth 8	 check: -131072
-32768	 trees of depth 10	 check: -32768
-8192	 trees of depth 12	 check: -8192
-2048	 trees of depth 14	 check: -2048
-512	 trees of depth 16	 check: -512
-128	 trees of depth 18	 check: -128
-32	 trees of depth 20	 check: -32
-long lived tree of depth 20	 check: -1
-15.977
-stretch tree of depth 21	 check: -1
-2097152	 trees of depth 4	 check: -2097152
-524288	 trees of depth 6	 check: -524288
-131072	 trees of depth 8	 check: -131072
-32768	 trees of depth 10	 check: -32768
-8192	 trees of depth 12	 check: -8192
-2048	 trees of depth 14	 check: -2048
-512	 trees of depth 16	 check: -512
-128	 trees of depth 18	 check: -128
-32	 trees of depth 20	 check: -32
-long lived tree of depth 20	 check: -1
-16.393
-stretch tree of depth 21	 check: -1
-2097152	 trees of depth 4	 check: -2097152
-524288	 trees of depth 6	 check: -524288
-131072	 trees of depth 8	 check: -131072
-32768	 trees of depth 10	 check: -32768
-8192	 trees of depth 12	 check: -8192
-2048	 trees of depth 14	 check: -2048
-512	 trees of depth 16	 check: -512
-128	 trees of depth 18	 check: -128
-32	 trees of depth 20	 check: -32
-long lived tree of depth 20	 check: -1
-16.030
-stretch tree of depth 21	 check: -1
-2097152	 trees of depth 4	 check: -2097152
-524288	 trees of depth 6	 check: -524288
-131072	 trees of depth 8	 check: -131072
-32768	 trees of depth 10	 check: -32768
-8192	 trees of depth 12	 check: -8192
-2048	 trees of depth 14	 check: -2048
-512	 trees of depth 16	 check: -512
-128	 trees of depth 18	 check: -128
-32	 trees of depth 20	 check: -32
-long lived tree of depth 20	 check: -1
-15.889
-stretch tree of depth 21	 check: -1
-2097152	 trees of depth 4	 check: -2097152
-524288	 trees of depth 6	 check: -524288
-131072	 trees of depth 8	 check: -131072
-32768	 trees of depth 10	 check: -32768
-8192	 trees of depth 12	 check: -8192
-2048	 trees of depth 14	 check: -2048
-512	 trees of depth 16	 check: -512
-128	 trees of depth 18	 check: -128
-32	 trees of depth 20	 check: -32
-long lived tree of depth 20	 check: -1
-15.512
-stretch tree of depth 21	 check: -1
-2097152	 trees of depth 4	 check: -2097152
-524288	 trees of depth 6	 check: -524288
-131072	 trees of depth 8	 check: -131072
-32768	 trees of depth 10	 check: -32768
-8192	 trees of depth 12	 check: -8192
-2048	 trees of depth 14	 check: -2048
-512	 trees of depth 16	 check: -512
-128	 trees of depth 18	 check: -128
-32	 trees of depth 20	 check: -32
-long lived tree of depth 20	 check: -1
-15.567
-stretch tree of depth 21	 check: -1
-2097152	 trees of depth 4	 check: -2097152
-524288	 trees of depth 6	 check: -524288
-131072	 trees of depth 8	 check: -131072
-32768	 trees of depth 10	 check: -32768
-8192	 trees of depth 12	 check: -8192
-2048	 trees of depth 14	 check: -2048
-512	 trees of depth 16	 check: -512
-128	 trees of depth 18	 check: -128
-32	 trees of depth 20	 check: -32
-long lived tree of depth 20	 check: -1
-15.107
-stretch tree of depth 21	 check: -1
-2097152	 trees of depth 4	 check: -2097152
-524288	 trees of depth 6	 check: -524288
-131072	 trees of depth 8	 check: -131072
-32768	 trees of depth 10	 check: -32768
-8192	 trees of depth 12	 check: -8192
-2048	 trees of depth 14	 check: -2048
-512	 trees of depth 16	 check: -512
-128	 trees of depth 18	 check: -128
-32	 trees of depth 20	 check: -32
-long lived tree of depth 20	 check: -1
-16.046
-stretch tree of depth 21	 check: -1
-2097152	 trees of depth 4	 check: -2097152
-524288	 trees of depth 6	 check: -524288
-131072	 trees of depth 8	 check: -131072
-32768	 trees of depth 10	 check: -32768
-8192	 trees of depth 12	 check: -8192
-2048	 trees of depth 14	 check: -2048
-512	 trees of depth 16	 check: -512
-128	 trees of depth 18	 check: -128
-32	 trees of depth 20	 check: -32
-long lived tree of depth 20	 check: -1
-15.169
+$ /usr/local/src/jdk1.6.0_18/bin/java -server -XX:+PrintCompilation -XX:-PrintGC fannkuchredux 12
+  1%      fannkuchredux::fannkuch @ 68 (276 bytes)
+  1       java.lang.Math::max (11 bytes)
+  1%  made not entrant  (2)  fannkuchredux::fannkuch @ 68 (276 bytes)
+3968050
+Pfannkuchen(12) = 65
+88.443213617
+  2       fannkuchredux::fannkuch (276 bytes)
+  2%      fannkuchredux::fannkuch @ 91 (276 bytes)
+  3%      fannkuchredux::fannkuch @ 111 (276 bytes)
+3968050
+Pfannkuchen(12) = 65
+91.85444456
+3968050
+Pfannkuchen(12) = 65
+90.611090816
+3968050
+Pfannkuchen(12) = 65
+90.736423516
+3968050
+Pfannkuchen(12) = 65
+90.870581883
+3968050
+Pfannkuchen(12) = 65
+90.861219422
+3968050
+Pfannkuchen(12) = 65
+90.611711336
+3968050
+Pfannkuchen(12) = 65
+90.736936381
+3968050
+Pfannkuchen(12) = 65
+91.041401088
+3968050
+Pfannkuchen(12) = 65
+90.862677571
 </pre>
