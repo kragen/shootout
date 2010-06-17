3fa028eb01739811e97be5eece458f3309dc8e94
igouy-guest <igouy-guest>
2010-06-17 23:42:28 +0000

    *** empty log message ***

---
+++ website/about/racket-about.tpl.php
@@ -0,0 +1,3 @@
+<?=$Version;?>
+<p>Home Page: <a href="http://www.racket-lang.org/">Racket</a></p>
+<p>Download: <a href="http://www.racket-lang.org/download/">Download Racket v5.0 (June 2010)</a></p>
