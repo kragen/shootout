04fb46d5db79a207e00c75b09b110a04df5ef29e
igouy-guest <igouy-guest>
2010-07-07 13:49:32 +0000

    *** empty log message ***

--- website/misc/benchmarking.php
+++ website/misc/benchmarking.php
@@ -21,7 +21,7 @@ means that most programs are small and spend most of their time in I/O and nativ
 <dd>
 <ul>
 <li><p>Generalizing from particular measurements without understanding what limits performance in other situations.</p>
-<p>Page load times for this site are limited by what the other 880 hosted projects are doing with the server. Even if page generation was instantaneous, we'd still average 1.3s just waiting in line until the load changes - "On average, pages in your site take 0.7 seconds to load (updated on Jun 28, 2010). This is faster than 96% of sites."</p>
+<p>Page load times for this site are limited by what the other 880 hosted projects are doing with the server. Even if page generation was instantaneous, we'd still average 1.3s just waiting in line unless the server load changes: the server load changed - "On average, pages in your site take 0.7 seconds to load (updated on Jun 28, 2010). This is faster than 96% of sites."</p>
 </li>
 <li><p>Generalizing from particular measurements without showing the measured programs are somehow representative of other situations.</p>
 <p>"The performance of a benchmark, even if it is derived from a real program, <a href="http://www.ccs.neu.edu/home/will/Twobit/bmcrock.temp.html" title="Benchmarks are a crock"><b>may not help</b></a> to predict the performance of similar programs that have different hot spots."</p>
