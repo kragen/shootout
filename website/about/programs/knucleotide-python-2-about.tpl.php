df21cb0484e139e6cc50e9a67002ad569d0ee9b1
igouy-guest <igouy-guest>
2010-08-14 17:36:38 +0000

    *** empty log message ***

---
+++ website/about/programs/knucleotide-python-2-about.tpl.php
@@ -0,0 +1,2 @@
+<p>Notice that sought sequences (GGT GGTA GGTATT GGTATTTTAATT GGTATTTTAATTTATAGT) newer overlap and create new function find_seq() with built-in function count(). Previous program iterated all data in function sort_seq() - char by char.</p>
+<p>Improved gen_func() - removed "if" statement and used defaultdict() function</p>
