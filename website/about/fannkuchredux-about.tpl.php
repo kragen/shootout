e4bbd99a6fe3092b10936cfc55c9556c70a14b64
igouy-guest <igouy-guest>
2010-09-03 16:26:58 +0000

    *** empty log message ***

--- website/about/fannkuchredux-about.tpl.php
+++ website/about/fannkuchredux-about.tpl.php
@@ -7,15 +7,17 @@
 <li>Take the first element, here 4, and reverse the order of the first 4 elements: {5,1,2,4,3}.</li>
 <li>Repeat this until the first element is a 1, so flipping won't change anything more: {3,4,2,1,5}, {2,4,3,1,5}, {4,2,3,1,5}, {1,3,2,4,5}.</li>
 <li>Count the number of flips, here 5.</li>
+<li>Keep a checksum
+<ul>
+<li>check_sum = check_sum + (if permutation_index is even then flips_count else -flips_count)</li>
+<li>check_sum = check_sum + (toggle_sign_-1_1 * flips_count)</li>
+</ul>
+</li>
 <li>Do this for all n! permutations, and record the maximum number of flips needed for any permutation.</li>
 </ul>
 <p>The conjecture is that this maximum count is approximated by n*log(n) when n goes to infinity.</p><p><i>FANNKUCH</i> is an abbreviation for the German word <i>Pfannkuchen</i>, or pancakes, in analogy to flipping pancakes."</p>
 <br />
 
-<p>Thanks to Oleg Mazurov for insisting on a checksum</p>
-<ul>
-<li>check_sum = check_sum + (if permutation_index is even then flips_count else -flips_count)</li>
-<li>check_sum = check_sum + (toggle_sign_-1_1 * flips_count)</li>
-</ul>
+<p>Thanks to Oleg Mazurov for insisting on a checksum.</p>
 
 <p>The fannkuch benchmark is defined in <a href="http://www.apl.jhu.edu/~hall/text/Papers/Lisp-Benchmarking-and-Fannkuch.ps">Performing Lisp Analysis of the FANNKUCH Benchmark</a>, Kenneth R. Anderson and Duane Rettig (26KB postscript)</p>
