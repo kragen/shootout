c8a4eab08ca9d8620821f1f7a432f127747d2447
igouy-guest <igouy-guest>
2010-09-18 05:30:59 +0000

    *** empty log message ***

--- website/websites/u32q/version/hipe-version.php
+++ website/websites/u32q/version/hipe-version.php
@@ -1,1 +1,1 @@
-<p>Erlang R13B03 (erts-5.7.4) [source] [smp:4:4] [rq:4] [async-threads:0] [hipe] [kernel-poll:false]</p>
+<p>Erlang R14B (erts-5.8.1) [source] [smp:4:4] [rq:4] [async-threads:0] [hipe] [kernel-poll:false]</p>
