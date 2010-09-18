11c834ad6144a7d19d3648deb4b1d420f6d917b9
igouy-guest <igouy-guest>
2010-09-18 15:49:07 +0000

    *** empty log message ***

--- website/websites/u64/version/hipe-version.php
+++ website/websites/u64/version/hipe-version.php
@@ -1,1 +1,1 @@
-<p>Erlang R13B03 (erts-5.7.4) [source] [64-bit] [rq:1] [async-threads:0] [hipe] [kernel-poll:false]</p>
+<p>Erlang R14B (erts-5.8.1) [source] [64-bit] [rq:1] [async-threads:0] [hipe] [kernel-poll:false]</p>
