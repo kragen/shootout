0fd6cede99cdfef4545a2f648308501ba8970e33
igouy-guest <igouy-guest>
2010-10-03 19:56:52 +0000

    *** empty log message ***

---
+++ website/about/cspreview-about.tpl.php
@@ -0,0 +1,5 @@
+<?=$Version;?>
+
+
+<p>Home Page: <a href="http://lists.ximian.com/pipermail/mono-devel-list/2010-September/035858.html">Mono 2.8 Public Preview</a></p>
+<p>Download: <a href="http://mono.ximian.com/monobuild/preview/download-preview/">Mono 2.8 Downloads (Release Candidate 2 [89f9256])</a></p>
