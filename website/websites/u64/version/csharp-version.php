fe71a4e68371a75c1545c4517aa492137395f1fc
igouy-guest <igouy-guest>
2010-10-08 16:02:23 +0000

    *** empty log message ***

--- website/websites/u64/version/csharp-version.php
+++ website/websites/u64/version/csharp-version.php
@@ -1,1 +1,1 @@
-<p>Mono JIT compiler version 2.8 (tarball Wed Oct  6 23:10:10 PDT 2010)<br/>--enable-parallel-mark</p>
+<p>Mono JIT compiler version 2.8 (tarball Wed Oct  6 23:10:10 PDT 2010)</p>
--- website/websites/u64q/version/fsharp-version.php
+++ website/websites/u64q/version/fsharp-version.php
@@ -1,2 +1,2 @@
 <p>Microsoft (R) F# 2.0 Compiler build 2.0.0.0</p>
-<p>Mono JIT compiler version 2.6.4 (tarball Sat Jun  5 18:20:02 PDT 2010)<br/>--with-profile4=yes --enable-parallel-mark</p>
+<p>Mono JIT compiler version 2.8 (tarball Wed Oct  6 23:10:10 PDT 2010)</p>
