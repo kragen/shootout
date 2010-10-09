21e650b41069d40209a6af3f8e73f2eb5ac09c75
igouy-guest <igouy-guest>
2010-10-09 00:55:05 +0000

    *** empty log message ***

--- website/websites/u32/version/ironpy-version.php
+++ website/websites/u32/version/ironpy-version.php
@@ -1,2 +1,2 @@
 <p>PythonContext 2.6.10920.0 on .NET 2.0.0.0</p>
-<p>Mono JIT compiler version 2.6.4 (tarball Sun Jun  6 00:57:49 PDT 2010)<br/>--with-profile4=yes --enable-parallel-mark</p>
+<p>Mono JIT compiler version 2.8 (tarball Wed Oct  6 20:05:56 PDT 2010)</p>
