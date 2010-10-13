db05ca2bb0ef77c85f327b8efc5b542b948e545f
igouy-guest <igouy-guest>
2010-10-13 18:48:29 +0000

    *** empty log message ***

--- website/about/programs/knucleotide-python3-2-about.tpl.php
+++ website/about/programs/knucleotide-python3-2-about.tpl.php
@@ -1,2 +1,3 @@
 <p>Notice that sought sequences (GGT GGTA GGTATT GGTATTTTAATT GGTATTTTAATTTATAGT) newer overlap and create new function find_seq() with built-in function count(). Previous program iterated all data in function sort_seq() - char by char.</p>
 <p>Improved gen_func() - removed "if" statement and used defaultdict() function</p>
+<p><b>BUT</b> skips the sequence generation in the sequence finder step. def find_seq(seq, nucleo): count = seq.count(nucleo) return nucleo, count</p>
