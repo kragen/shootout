754f97a78b64f8b49242ff60dd473ac9ef1d0e6d
igouy-guest <igouy-guest>
2010-11-28 17:29:19 +0000

    *** empty log message ***

--- website/websites/u32/version/java-version.php
+++ website/websites/u32/version/java-version.php
@@ -1,4 +1,4 @@
-<p>java version "1.6.0_18"<br/>
-Java(TM) SE Runtime Environment (build 1.6.0_18-b07)<br/>
-Java HotSpot(TM) Server VM (build 16.0-b13, mixed mode)
+<p>java version "1.6.0_22"<br/>
+Java(TM) SE Runtime Environment (build 1.6.0_22-b04)<br/>
+Java HotSpot(TM) Server VM (build 17.1-b03, mixed mode)
 </p>
