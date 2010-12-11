cfbc6a3f98c5e1781c8ce818e7d34ebe7efc998e
igouy-guest <igouy-guest>
2010-12-11 22:41:50 +0000

    *** empty log message ***

--- website/websites/index.php
+++ website/websites/index.php
@@ -18,7 +18,7 @@ header("Expires: " . gmdate("D, d M Y H:i:s", $s + (31*3600)) . " GMT");
 
 <meta name="robots" content="all" /><meta name="revisit" content="1 days" />
 
-<meta name="description" content="Compare the performance of ~30 programming languages using ~12 flawed benchmarks and ~1100 programs. Contribute faster more elegant programs." />
+<meta name="description" content="Compare the performance of ~30 programming languages using ~12 simple benchmarks and ~1100 programs. Contribute faster more elegant programs." />
 
 <title>Computer Language Benchmarks Game</title>
 <link rel="stylesheet" type="text/css" href="http://shootout.alioth.debian.org/benchmark_css_18aug2010.php" />
