9ea872cc2b6217a26c2e9633e71e70b02438e949
igouy-guest <igouy-guest>
2010-12-23 06:37:47 +0000

    *** empty log message ***

--- website/about/clean-about.tpl.php
+++ website/about/clean-about.tpl.php
@@ -1,5 +1,3 @@
-<p><a href="http://clean.cs.ru.nl/CleanExtra/report20/contents.html">Clean Version 2.0 Language Report (HTML)</a></p>
-<p>online book <a href="http://www.cs.kun.nl/~clean/contents/Clean_Book/clean_book.html">Functional Programming in Clean</a></p>
 <?=$Version;?>
-<p>Home Page: <a href="http://www.cs.kun.nl/~clean/index.html">http://www.cs.kun.nl/~clean/index.html</a></p>
-<p>Download: <a href="http://www.cs.kun.nl/~clean/Download/main/main.htm">http://www.cs.kun.nl/~clean/Download/main/main.htm</a></p>
+<p>Home Page: <a href="http://wiki.clean.cs.ru.nl/Clean">Clean</a></p>
+<p>Download: <a href="http://wiki.clean.cs.ru.nl/Download_Clean">Download Clean</a></p>
