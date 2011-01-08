2a25cf00ea00a421339a919cd173dc55b7b85d35
igouy-guest <igouy-guest>
2011-01-08 18:53:24 +0000

    *** empty log message ***

---
+++ website/websites/beware-hasty-generalization.php
@@ -0,0 +1,7 @@
+<?php
+// Copyright (c) Isaac Gouy 2011
+ob_start('ob_gzhandler');
+require_once('config.php'); 
+$F = 'benchmarking'; $T = 'Beware making hasty generalizations!';
+require_once(LIB_PATH.'miscfile.php');
+?>
