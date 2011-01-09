b670f023ca654dd3d2d3ad3f5ff8e05feaaed871
igouy-guest <igouy-guest>
2011-01-09 01:58:30 +0000

    *** empty log message ***

--- website/about/ats-about.tpl.php
+++ website/about/ats-about.tpl.php
@@ -1,3 +1,4 @@
+<p>online book <a href="http://www.ats-lang.org/DOCUMENTATION/proginats/HTML/book1.html">Programming in ATS</a></p>
 <?=$Version;?>
 <p>Home Page: <a href="http://www.ats-lang.org/">ATS -- unleashing the potential of types!</a></p>
 
