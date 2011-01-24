4522176a85670c8c33f162cf9b0675ac383633dd
igouy-guest <igouy-guest>
2011-01-24 23:50:14 +0000

    *** empty log message ***

--- website/lib/median.tpl.php
+++ website/lib/median.tpl.php
@@ -7,7 +7,7 @@
    unset($selected);
 ?>
 
-<h2>Which programming language implementations have the fastest benchmark programs?</h2>
+<p>Which programming language implementations have the fastest benchmark programs?</p>
 <p>Median x86 Ubuntu (normalized) elapsed time measurements:</p>
 <ul><?
 foreach($score as $k => $v){
