8d8a0e51820db9e86ddb6886919099db9549ab11
igouy-guest <igouy-guest>
2011-01-26 21:30:22 +0000

    *** empty log message ***

--- website/websites/index.php
+++ website/websites/index.php
@@ -190,7 +190,7 @@ foreach($tests as $t){
 
 
 
-<p class="imgfooter">&nbsp; <a href="http://shootout.alioth.debian.org/dont-jump-to-conclusions.php">Conclusions</a> &nbsp; <a href="http://shootout.alioth.debian.org/fastest-programming-language.php">Fastest</a> &nbsp; <a href="http://shootout.alioth.debian.org/license.php">License</a> &nbsp; <a href="http://shootout.alioth.debian.org/help.php">Help</a> &nbsp;</p>
+<p class="imgfooter">&nbsp; <a href="http://shootout.alioth.debian.org/mobile/index.php"  title="HandheldFriendly pages">Mobile</a> &nbsp; <a href="http://shootout.alioth.debian.org/dont-jump-to-conclusions.php">Conclusions</a> &nbsp; <a href="http://shootout.alioth.debian.org/fastest-programming-language.php">Fastest</a> &nbsp; <a href="http://shootout.alioth.debian.org/license.php">License</a> &nbsp; <a href="http://shootout.alioth.debian.org/help.php">Help</a> &nbsp;</p>
 
 </div>
 
