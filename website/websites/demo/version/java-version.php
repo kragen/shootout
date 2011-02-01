ad188cc4291b0fe770b7887fa802eb7b8b57fc6e
igouy-guest <igouy-guest>
2011-02-01 02:28:04 +0000

    *** empty log message ***

--- website/websites/demo/version/java-version.php
+++ website/websites/demo/version/java-version.php
@@ -1,4 +1,4 @@
 <p>java version "1.6.0_23"<br/>
 Java(TM) SE Runtime Environment (build 1.6.0_23-b05)<br/>
-Java HotSpot(TM) Client VM (build 19.0-b09, mixed mode, sharing)
+Java HotSpot(TM) Server VM (build 19.0-b09, mixed mode, sharing)
 </p>
