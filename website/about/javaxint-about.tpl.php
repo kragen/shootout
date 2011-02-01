64616f8805e59c24bad5adbc8a67835fe428be37
igouy-guest <igouy-guest>
2011-02-01 02:18:05 +0000

    *** empty log message ***

--- website/about/javaxint-about.tpl.php
+++ website/about/javaxint-about.tpl.php
@@ -1,3 +1,3 @@
 <?=$Version;?>
-<p>Home Page: <a href="http://www.oracle.com/technetwork/java/javase/overview/index.html/">Java SE at a Glance</a></p>
+<p>Home Page: <a href="http://www.oracle.com/technetwork/java/javase/overview/index.html">Java SE at a Glance</a></p>
 <p>Download: <a href="http://www.oracle.com/technetwork/java/javase/downloads/index.html">Java SE Downloads</a></p>
