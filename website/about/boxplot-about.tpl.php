fe23e0cdca6835a06a210dbc9d77af6cd9418758
igouy-guest <igouy-guest>
2011-02-10 19:30:10 +0000

    *** empty log message ***

--- website/about/boxplot-about.tpl.php
+++ website/about/boxplot-about.tpl.php
@@ -2,6 +2,6 @@
 
 <p>Read <a href="http://www.lcgceurope.com/lcgceurope/data/articlestandard/lcgceurope/132005/152912/article.pdf"><b>"Visual Presentation of Data by Means of Box Plots" (pdf)</b></a></p>
 
-<p>For each benchmark, <b>F</b> is the fastest time measurement; for each language implementation, each time measurement <b>T</b> is then normalized to <b>T &#247; F</b></p>
+<p>For each benchmark, <b>F</b> is the fastest time measurement.<br/>For each language implementation, each time measurement <b>T</b> is then normalized to <b>T &#247; F</b></p>
 
 <p><br/>There are other ways to analyse and present this data: look for patterns in <a href="code-used-time-used-shapes.php">Code-used Time-used Shapes</a> or ask <a href="which-language-is-best.php">Which programming language is best?</a> or take the <a href="summarydata.php">Summary Data</a> and do your own analysis!</p>
--- website/about/scorecard-about.tpl.php
+++ website/about/scorecard-about.tpl.php
@@ -1,8 +1,8 @@
 <p>Read <a href="http://portal.acm.org/citation.cfm?id=5666.5673"><strong>"How not to lie with statistics: the correct way to summarize benchmark results" (pdf)</strong></a>.</p>
 
-<p>For each benchmark, <strong>B</strong> is the best measurement; for each language implementation, the best measurement <strong>L</strong> is then normalized to <strong><sup>L</sup>/<sub>B</sub></strong></p>
+<p>For each benchmark, <strong>B</strong> is the best measurement.<br/>For each language implementation, the best measurement <strong>L</strong> is then normalized to <strong>L &#247; B</strong></p>
 
-<p><strong>GM</strong> is the Weighted Geometric Mean of those <strong><sup>L</sup>/<sub>B</sub></strong> ratios.</p>
+<p><strong>GM</strong> is the Weighted Geometric Mean of those <strong>L &#247; B</strong> ratios.</p>
 
 <p><strong>missing</strong> : Language implementations with more than a couple of Timeouts (at the largest workload) distort the ranking - so they have been excluded. They can still be <a href="benchmark.php?test=all&amp;lang=gpp&amp;lang2=java">compared directly</a> against another language implementation.</p>
 
