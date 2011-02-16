9c0d1085630bb7340e37c1c0207d877e8c32f751
igouy-guest <igouy-guest>
2011-02-16 19:02:38 +0000

    *** empty log message ***

--- website/lib/lib_whitelist.php
+++ website/lib/lib_whitelist.php
@@ -132,8 +132,8 @@ function ValidSort($H,$valid=FALSE){
       if (isset($H['so']) && strlen($H['so']) && strlen($H['so']) <= $bounds){
          $X = base64_decode( rawurldecode($H['so']) );
          $X = @gzuncompress($X,$bounds); // returns FALSE on error
-         if ($X && ereg("^[ a-z ]+$",$X){
-            //&& (($X == 'elapsed') || ($X == 'gz') || ($X == 'kb') || ($X == 'fullcpu')) ){ 
+         if ($X && ereg("^[ a-z ]+$",$X) && 
+            (($X == "elapsed") || ($X == "gz") || ($X == "kb") || ($X == "fullcpu")) ){ 
                $sort = $X; 
                $valid = TRUE; 
          }
