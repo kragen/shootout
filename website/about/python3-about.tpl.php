669a3c5ff0ef9914fe20d3c5d9984c8395ea4663
igouy-guest <igouy-guest>
2011-02-21 18:41:36 +0000

    *** empty log message ***

--- website/about/python3-about.tpl.php
+++ website/about/python3-about.tpl.php
@@ -1,3 +1,3 @@
 <?=$Version;?>
 <p>A new version of the Python "language that is incompatible with the 2.x line of releases".</p>
-<p>Home Page: <a href="http://www.python.org/download/releases/3.1.2/">Python 3.1.2</a></p>
+<p>Home Page: <a href="http://www.python.org/download/releases/3.2/">Python 3.2 was released on February 20th, 2011.</a></p>
