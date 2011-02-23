47994527bdc448ee9ad998d9f85465040e4a376c
igouy-guest <igouy-guest>
2011-02-23 20:39:41 +0000

    Mono JIT compiler version 2.10 F#

--- website/websites/u32/version/fsharp-version.php
+++ website/websites/u32/version/fsharp-version.php
@@ -1,2 +1,2 @@
 <p>Microsoft (R) F# 2.0 Compiler build 2.0.0.0</p>
-<p>Mono JIT compiler version 2.8 (tarball Wed Oct  6 20:05:56 PDT 2010)</p>
+<p>Mono JIT compiler version 2.10 (tarball Tue Feb 22 12:09:58 PST 2011)</p>
