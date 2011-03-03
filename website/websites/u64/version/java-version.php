231e9c77b841d8300b5da5ff03c8241cf074f9e9
igouy-guest <igouy-guest>
2011-03-03 03:08:47 +0000

    Java(TM) 1.6.0_24

--- website/websites/u64/version/java-version.php
+++ website/websites/u64/version/java-version.php
@@ -1,4 +1,4 @@
-<p>java version "1.6.0_22"<br/>
-Java(TM) SE Runtime Environment (build 1.6.0_22-b04)<br/>
-Java HotSpot(TM) 64-Bit Server VM (build 17.1-b03, mixed mode)
+<p>java version "1.6.0_24"<br/>
+Java(TM) SE Runtime Environment (build 1.6.0_24-b07)<br/>
+Java HotSpot(TM) 64-Bit Server VM (build 19.1-b02, mixed mode)
 </p>
