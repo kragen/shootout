b1ea9e1caa376f3a7f0fe4c10cf259b2803832ac
igouy-guest <igouy-guest>
2011-03-17 15:55:20 +0000

    Mono 2.10.1

--- website/websites/u64/version/csharp-version.php
+++ website/websites/u64/version/csharp-version.php
@@ -1,1 +1,1 @@
-<p>Mono JIT compiler version 2.10 (tarball Tue Feb 22 17:22:25 PST 2011)</p>
+<p>Mono JIT compiler version 2.10.1 (tarball Tue Mar 15 18:33:21 PDT 2011)</p>
--- website/websites/u64q/version/fsharp-version.php
+++ website/websites/u64q/version/fsharp-version.php
@@ -1,2 +1,2 @@
 <p>Microsoft (R) F# 2.0 Compiler build 2.0.0.0</p>
-<p>Mono JIT compiler version 2.10 (tarball Tue Feb 22 17:22:25 PST 2011)</p>
+<p>Mono JIT compiler version 2.10.1 (tarball Tue Mar 15 18:33:21 PDT 2011)</p>
