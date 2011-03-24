ddbec339589771e9591606df158a81e70dbe3fc9
igouy-guest <igouy-guest>
2011-03-24 05:47:53 +0000

    *** empty log message ***

---
+++ website/about/clang-about.tpl.php
@@ -0,0 +1,2 @@
+<?=$Version;?>
+<p>Home Page: <a href="http://clang.llvm.org/">clang: a C language family frontend for LLVM</a></p>
---
+++ website/about/llvmgcc-about.tpl.php
@@ -0,0 +1,2 @@
+<?=$Version;?>
+<p>Home Page: <a href="http://llvm.org/cmds/llvmgcc.html">llvm-gcc - LLVM C front-end</a></p>
