bdc693762e8a3f7d9a4e57dcbda9be7faf3c5bad
igouy-guest <igouy-guest>
2011-04-07 15:39:22 +0000

    *** empty log message ***

--- website/about/python3-about.tpl.php
+++ website/about/python3-about.tpl.php
@@ -1,3 +1,3 @@
 <?=$Version;?>
-<p>A new version of the Python "language that is incompatible with the 2.x line of releases".</p>
+<p>"3.x is the newest branch of Python and the intended future of the language."</p>
 <p>Home Page: <a href="http://www.python.org/download/releases/3.2/">Python 3.2 was released on February 20th, 2011.</a></p>
