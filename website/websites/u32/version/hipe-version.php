354a427423a71fd4646ecfbae2a06892e7f5b120
igouy-guest <igouy-guest>
2011-04-14 13:36:11 +0000

    Erlang R14B02

--- website/websites/u32/version/hipe-version.php
+++ website/websites/u32/version/hipe-version.php
@@ -1,1 +1,1 @@
-<p>Erlang R14B (erts-5.8.1) [source] [rq:1] [async-threads:0] [hipe] [kernel-poll:false]</p>
+<p>Erlang R14B02 (erts-5.8.3) [source] [rq:1] [async-threads:0] [hipe] [kernel-poll:false]</p>
--- website/websites/u32q/version/hipe-version.php
+++ website/websites/u32q/version/hipe-version.php
@@ -1,1 +1,1 @@
-<p>Erlang R14B (erts-5.8.1) [source] [smp:4:4] [rq:4] [async-threads:0] [hipe] [kernel-poll:false]</p>
+<p>Erlang R14B02 (erts-5.8.3) [source] [smp:4:4] [rq:4] [async-threads:0] [hipe] [kernel-poll:false]</p>
