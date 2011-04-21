40f7587740fad8dd3820b26c31950d9d8b6a3400
igouy-guest <igouy-guest>
2011-04-21 19:15:19 +0000

    *** empty log message ***

---
+++ website/about/programs/nbody-cint-about.tpl.php
@@ -0,0 +1,2 @@
+<p>Here CINT is only being used as a <b>C interpreter</b> but a CINT script can call compiled classes/functions and compiled code can make callbacks to CINT interpreted functions.</p>
+
