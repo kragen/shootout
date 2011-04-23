498427aff2bbe929a0c80ad6f108b2b816df3062
igouy-guest <igouy-guest>
2011-04-23 14:10:01 +0000

    *** empty log message ***

--- website/websites/mobile/luajit.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Copyright (c) Isaac Gouy 2011
-ob_start('ob_gzhandler');
-require_once('config.php'); 
-$T = 'all'; $L = 'lua'; $metaRobots = '';
-require_once(LIB_PATH.'comparetimes.php'); 
-?>
--- website/websites/u64q/luajit.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Copyright (c) Isaac Gouy 2009-2010
-ob_start('ob_gzhandler');
-require_once('config.php'); 
-$T = 'all'; $L = 'lua'; $metaRobots = '';
-require_once(LIB_PATH.'compare.php'); 
-?>
