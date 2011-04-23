cf68c5bad0380e7c540e95931dc8d184611d5e62
igouy-guest <igouy-guest>
2011-04-23 15:17:19 +0000

    *** empty log message ***

--- website/about/cint-about.tpl.php
+++ website/about/cint-about.tpl.php
@@ -1,4 +1,4 @@
-<p>"CINT is an interpreter for C and C++ code. It is useful e.g. for situations where rapid development is more important than execution time. Using an interpreter the compile and link cycle is dramatically reduced facilitating rapid development. CINT makes C/C++ programming enjoyable even for part-time programmers."</p>
+<p>"CINT is <b>an interpreter for C and C++ code</b>. It is useful e.g. for situations where rapid development is more important than execution time. Using an interpreter the compile and link cycle is dramatically reduced facilitating rapid development. CINT makes C/C++ programming enjoyable even for part-time programmers."</p>
 <?=$Version;?>
 <p>Home Page: <a href="http://root.cern.ch/drupal/content/cint">What is CINT?</a></p>
 
