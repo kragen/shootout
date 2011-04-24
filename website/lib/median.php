3d08ff049494e683a9640c2398572a6f8ef88ddc
igouy-guest <igouy-guest>
2011-04-24 19:32:44 +0000

    *** empty log message ***

--- website/lib/median.php
+++ website/lib/median.php
@@ -78,7 +78,7 @@ function FullRatios($FileName,$Tests,$Langs,$Incl,$Excl,$SLangs,$HasHeading=TRUE
 
    $ratios = array();
    foreach($data as $k => $test){
-      if (sizeof($test)/sizeof($Tests) > 0.5){
+      if (sizeof($test)/sizeof($Tests) > 0.4){
          $s = array();
          foreach($test as $t => $row){
             // wait until now to filter so sizeof($test) is consistent with FullWeightedData
