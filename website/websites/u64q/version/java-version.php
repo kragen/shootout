a01464cac2aa20a870dd151b5b96602937982a50
igouy-guest <igouy-guest>
2011-05-12 20:50:11 +0000

    Java 1.6.0_25 Scala 2.9.0.final

--- website/websites/u64q/version/java-version.php
+++ website/websites/u64q/version/java-version.php
@@ -1,4 +1,4 @@
-<p>java version "1.6.0_24"<br/>
-Java(TM) SE Runtime Environment (build 1.6.0_24-b07)<br/>
-Java HotSpot(TM) 64-Bit Server VM (build 19.1-b02, mixed mode)
+<p>java version "1.6.0_25"<br/>
+Java(TM) SE Runtime Environment (build 1.6.0_25-b06)<br/>
+Java HotSpot(TM) 64-Bit Server VM (build 20.0-b11, mixed mode)
 </p>
