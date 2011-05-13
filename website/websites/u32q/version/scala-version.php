dee6061e0a9150c310cb69e4baec09f9f509bb50
igouy-guest <igouy-guest>
2011-05-13 14:29:09 +0000

    Java 1.6.0_25 Scala 2.9.0.final

--- website/websites/u32q/version/scala-version.php
+++ website/websites/u32q/version/scala-version.php
@@ -1,1 +1,5 @@
-<p>Scala compiler version 2.8.1.final -- Copyright 2002-2010, LAMP/EPFL</p>
+<p>java version "1.6.0_25"<br/>
+Java(TM) SE Runtime Environment (build 1.6.0_25-b06)<br/>
+Java HotSpot(TM) Server VM (build 20.0-b11, mixed mode)
+</p>
+<p>Scala compiler version 2.9.0.final -- Copyright 2002-2011, LAMP/EPFL</p>
