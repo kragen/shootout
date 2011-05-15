ba1db1565f3506015bb44885b9c30d600020f023
igouy-guest <igouy-guest>
2011-05-15 00:42:18 +0000

    Java 1.6.0_25 JRuby 1.6.0

--- website/websites/u64q/version/jruby-version.php
+++ website/websites/u64q/version/jruby-version.php
@@ -1,1 +1,1 @@
-<p>jruby 1.4.0 (ruby 1.8.7 patchlevel 174) (2009-11-02 69fbfa3) (Java HotSpot(TM) 64-Bit Server VM 1.6.0_14) [amd64-java]</p>
+<p>jruby 1.6.0 (ruby 1.8.7 patchlevel 330) (2011-03-15 f3b6154) (Java HotSpot(TM) 64-Bit Server VM 1.6.0_25) [linux-amd64-java]</p>
